8439f4a9f86af2c432acb77a166892bea29f1bc0
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-11 09:02:34 +0300

    new commit

--- 4-bubble-sort.js
+++ 4-bubble-sort.js
@@ -4,4 +4,7 @@ let count = 0
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
+            if (array[j + 1] < array[j]) {
+                let tmp = array[j]
+                array[j] = array[j+1]
             
