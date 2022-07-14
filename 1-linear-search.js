c8f8ea2c24942e1d6be752740ff327135f635b3e
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-14 23:15:01 +0300

    new commit

--- 1-linear-search.js
+++ 1-linear-search.js
@@ -20,4 +20,7 @@ export default class BloomFilter {
     /**
      * @param {number} size - the size of the storage.
      */
+    constructor(size = 100) {
+      // Bloom filter size directly affects the likelihood of false positives.
+      // The bigger the size the lower the likelihood of false positives.
    
