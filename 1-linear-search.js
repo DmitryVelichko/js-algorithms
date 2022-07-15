190ed8fd1c330a60c5f47fa3b8958ed7183cb91d
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-15 23:55:56 +0300

    new commit

--- 1-linear-search.js
+++ 1-linear-search.js
@@ -34,3 +34,19 @@ export default class Heap {
     this.heapContainer = [];
     this.compare = new Comparator(comparatorFunction);
   }
+
+  /**
+   * @param {number} parentIndex
+   * @return {number}
+   */
+  getLeftChildIndex(parentIndex) {
+    return (2 * parentIndex) + 1;
+  }
+
+  /**
+   * @param {number} parentIndex
+   * @return {number}
+   */
+  getRightChildIndex(parentIndex) {
+    return (2 * parentIndex) + 2;
+  }
