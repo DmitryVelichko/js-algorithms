1a6e5241c21048ca6b412dee54e670a50b60cfbb
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-17 11:31:41 +0300

    new commit

--- 2.1-binary-search-rec.js
+++ 2.1-binary-search-rec.js
@@ -2,22 +2,20 @@
 
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-const binarySearch = (arr, value) => {
+const binarySearchRec = (arr, value) => {
   let start = 0;
   let end = arr.length - 1;
 
   while (start <= end) {
     let middle = Math.floor((start + end) / 2);
-    
-    if (value === arr[middle] ) return middle;
-
-    if (value < arr[middle]) {
-      end = middle - 1;
-    } else {
+    if (arr[middle] === value) return middle;
+    if (arr[middle] < value) {
       start = middle + 1;
+    } else {
+      end = middle - 1;
     }
   }
   return -1;
 };
-console.log(binarySearch(arr, 11));
+console.log(binarySearchRec(arr, 11));
 
