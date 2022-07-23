5fcfb95ad37d12a6016b58e88b626669b5b2105b
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-23 23:38:42 +0300

    new commit

--- 7-merge-sort.js
+++ 7-merge-sort.js
@@ -6,4 +6,6 @@ const arr = [-5, 23, 7, 5, 3, -12, -29, 21, 54, 35, 0];
 function merge(left, right) {
     let arr = []
     // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
    
