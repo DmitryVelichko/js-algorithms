53b6985b0f1015923ab0f20d27c0f2653e2c543a
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-26 23:12:02 +0300

    new commit

--- 7-merge-sort.js
+++ 7-merge-sort.js
@@ -4,3 +4,6 @@ function mergesort(arr) {
     }
     const mid = Math.floor(arr.length / 2)
     const leftArr = arr.slice(0, mid)
+    const rightArr = arr.slice(mid)
+    return merge(mergesort(leftArr), mergesort(rightArr))
+  }
