8e655523d7a7da2e48dfde273496ad4936738d7a
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-26 23:12:52 +0300

    new commit

--- 7-merge-sort.js
+++ 7-merge-sort.js
@@ -1,3 +1,25 @@
+// Merge Sort, Сортировка слиянием O(n * log n)
+// 
+
+const arr = [-5, 23, 7, 5, 3, -12, -29, 21, 54, 35, 0];
+
+function merge(left, right) {
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
+}
+
 function mergesort(arr) {
     if (arr.length < 2) {
       return arr
