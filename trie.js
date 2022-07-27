aa9d9c04439263fbd935ea46ebf3ca1b4aad574d
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-27 22:56:20 +0300

    new commit

--- trie.js
+++ trie.js
@@ -93,3 +93,18 @@ export default class Trie {
    * @param {string} word
    * @return {TrieNode}
    */
+  getLastCharacterNode(word) {
+    const characters = Array.from(word);
+    let currentNode = this.head;
+
+    for (let charIndex = 0; charIndex < characters.length; charIndex += 1) {
+      if (!currentNode.hasChild(characters[charIndex])) {
+        return null;
+      }
+
+      currentNode = currentNode.getChild(characters[charIndex]);
+    }
+
+    return currentNode;
+  }
+}
