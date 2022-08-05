3f2f6ee62210b130f994cce2fa8b2b56578a1d47
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-05 22:49:06 +0300

    new commit

--- jump-search.js
+++ jump-search.js
@@ -5,3 +5,9 @@ import Comparator from '../../../utils/comparator/Comparator';
  *
  * @param {*[]} sortedArray
  * @param {*} seekElement
+ * @param {function(a, b)} [comparatorCallback]
+ * @return {number}
+ */
+export default function jumpSearch(sortedArray, seekElement, comparatorCallback) {
+  const comparator = new Comparator(comparatorCallback);
+  const arraySize = sortedArray.length;
