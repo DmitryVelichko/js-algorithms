f17160f4ba1f90539d109acf6fe4828d7ae066f1
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-08 23:26:10 +0300

    new commit

--- jump-search.js
+++ jump-search.js
@@ -2,4 +2,7 @@ function mergesort(arr) {
   if (arr.length < 2) {
     return arr
   }
+  const mid = Math.floor(arr.length / 2)
+  const leftArr = arr.slice(0, mid)
+  const rightArr = arr.slice(mid)
  
