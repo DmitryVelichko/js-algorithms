dc0810a1f860c056841583e69a1ca4c9c31dc2a3
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-22 14:00:24 +0300

    new commit

--- algo.js
+++ algo.js
@@ -15,3 +15,15 @@ export default class Queue {
   isEmpty() {
     return !this.linkedList.head;
   }
+
+  /**
+   * Read the element at the front of the queue without removing it.
+   * @return {*}
+   */
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.linkedList.head.value;
+  }
