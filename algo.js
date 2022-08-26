9d1364a9fbc9483bd0401dbb98fe48a5b9da7223
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-26 10:48:05 +0300

    new commit

--- algo.js
+++ algo.js
@@ -13,3 +13,11 @@
     const rangeDelta = sortedArray[rightIndex] - sortedArray[leftIndex];
     const indexDelta = rightIndex - leftIndex;
     const valueDelta = seekElement - sortedArray[leftIndex];
+
+    // If valueDelta is less then zero it means that there is no seek element
+    // exists in array since the lowest element from the range is already higher
+    // then seek element.
+    if (valueDelta < 0) {
+      return -1;
+    }
+
