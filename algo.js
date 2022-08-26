d072e99eb5ad20592ce615301482ca4175314a98
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-26 10:47:58 +0300

    new commit

--- algo.js
+++ algo.js
@@ -5,3 +5,11 @@
  * @param {*} seekElement
  * @return {number}
  */
+ export default function interpolationSearch(sortedArray, seekElement) {
+  let leftIndex = 0;
+  let rightIndex = sortedArray.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    const rangeDelta = sortedArray[rightIndex] - sortedArray[leftIndex];
+    const indexDelta = rightIndex - leftIndex;
+    const valueDelta = seekElement - sortedArray[leftIndex];
