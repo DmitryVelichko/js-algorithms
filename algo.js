a5d2b4de0ce9431e963b2869c9d4ae714ab6d49d
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-27 23:33:04 +0300

    new commit

--- algo.js
+++ algo.js
@@ -9,3 +9,16 @@ export default class MergeSort extends Sort {
     if (originalArray.length <= 1) {
       return originalArray;
     }
+
+    // Split array on two halves.
+    const middleIndex = Math.floor(originalArray.length / 2);
+    const leftArray = originalArray.slice(0, middleIndex);
+    const rightArray = originalArray.slice(middleIndex, originalArray.length);
+
+    // Sort two halves of split array
+    const leftSortedArray = this.sort(leftArray);
+    const rightSortedArray = this.sort(rightArray);
+
+    // Merge two sorted arrays into one.
+    return this.mergeSortedArrays(leftSortedArray, rightSortedArray);
+  }
