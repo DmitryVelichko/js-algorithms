b3ff082b007f036f92663857c439ac13aaa1bfb3
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-30 18:34:51 +0300

    new commit

--- algo.js
+++ algo.js
@@ -11,3 +11,17 @@ export default function prim(graph) {
   if (graph.isDirected) {
     throw new Error('Prim\'s algorithms works only for undirected graphs');
   }
+
+  // Init new graph that will contain minimum spanning tree of original graph.
+  const minimumSpanningTree = new Graph();
+
+  // This priority queue will contain all the edges that are starting from
+  // visited nodes and they will be ranked by edge weight - so that on each step
+  // we would always pick the edge with minimal edge weight.
+  const edgesQueue = new PriorityQueue();
+
+  // Set of vertices that has been already visited.
+  const visitedVertices = {};
+
+  // Vertex from which we will start graph traversal.
+  const startVertex = graph.getAllVertices()[0];
