bdc10903e396a56c94473633d15de00564699bf6
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-30 18:34:44 +0300

    new commit

--- algo.js
+++ algo.js
@@ -5,3 +5,9 @@ import PriorityQueue from '../../../data-structures/priority-queue/PriorityQueue
  * @param {Graph} graph
  * @return {Graph}
  */
+export default function prim(graph) {
+  // It should fire error if graph is directed since the algorithm works only
+  // for undirected graphs.
+  if (graph.isDirected) {
+    throw new Error('Prim\'s algorithms works only for undirected graphs');
+  }
