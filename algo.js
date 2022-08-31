010744f55786a37a24623cc288810e843ab633ff
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-31 08:51:56 +0300

    new commit

--- algo.js
+++ algo.js
@@ -5,3 +5,10 @@ export default class BubbleSort extends Sort {
     // Flag that holds info about whether the swap has occur or not.
     let swapped = false;
     // Clone original array to prevent its modification.
+    const array = [...originalArray];
+
+    for (let i = 1; i < array.length; i += 1) {
+      swapped = false;
+
+      // Call visiting callback.
+      this.callbacks.visitingCallback(array[i]);
