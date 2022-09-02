5f3b6dec36543d2d350a4ff7383be9435c7560b6
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-02 22:14:53 +0300

    new commit

--- algo.js
+++ algo.js
@@ -6,3 +6,17 @@ export default class CountingSort extends Sort {
    * @param {number} [smallestElement]
    * @param {number} [biggestElement]
    */
+  sort(originalArray, smallestElement = undefined, biggestElement = undefined) {
+    // Init biggest and smallest elements in array in order to build number bucket array later.
+    let detectedSmallestElement = smallestElement || 0;
+    let detectedBiggestElement = biggestElement || 0;
+
+    if (smallestElement === undefined || biggestElement === undefined) {
+      originalArray.forEach((element) => {
+        // Visit element.
+        this.callbacks.visitingCallback(element);
+
+        // Detect biggest element.
+        if (this.comparator.greaterThan(element, detectedBiggestElement)) {
+          detectedBiggestElement = element;
+        }
