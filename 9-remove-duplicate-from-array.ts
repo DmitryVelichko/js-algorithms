a3326587f09315a2e9a1eaf34844a60a06471314
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-03 21:22:30 +0300

    new commit

--- 9-remove-duplicate-from-array.ts
+++ 9-remove-duplicate-from-array.ts
@@ -13,7 +13,11 @@ function removeDuplicates2(nums: number[]): number {
  
     let k: number = 1;
     for(let i = 1; i < nums.length; i++) {
-       
+        if(nums[i] !== nums[i-1]) {
+           
+            nums[k] = nums[i];
+             k++;
+        }
     }
     return k;
     
