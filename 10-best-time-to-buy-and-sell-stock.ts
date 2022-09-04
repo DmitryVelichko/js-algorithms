34984465a6fd300bcf87cf3f63e0276f16f03420
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-04 10:40:33 +0300

    new commit

--- 10-best-time-to-buy-and-sell-stock.ts
+++ 10-best-time-to-buy-and-sell-stock.ts
@@ -10,4 +10,9 @@ var maxProfit = function(prices) {
     if (prices === null || prices.length === 0) {
         return 0;
     }
+    let profit = 0;
+    let buyingPrice = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > buyingPrice) {
+            profit += prices[i] - buyingPrice;
 
