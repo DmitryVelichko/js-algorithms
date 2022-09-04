d00c0833c03b3446a9acff56e92b9fe7f703ea17
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-04 10:56:29 +0300

    new commit

--- 10-best-time-to-buy-and-sell-stock.ts
+++ 10-best-time-to-buy-and-sell-stock.ts
@@ -1,27 +1,18 @@
-/*
-Solution
+function maxProfit(prices) {
 
-Profit from Buying - Selling - Re-buying Selling
-and
-Profit from Buying - Wait for Best price - Selling
-are the same. That's why the logic behind this solution.
-*/
-var maxProfit = function(prices) {
     if (prices === null || prices.length === 0) {
         return 0;
     }
     let profit = 0;
-    let buyingPrice = prices[0];
+ 
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] > buyingPrice) {
-            profit += prices[i] - buyingPrice;
-            buyingPrice = prices[i]; // Re-buying 
-        } else if (prices[i] < buyingPrice) {
-            buyingPrice = prices[i]; // Better price
+        if (prices[i] > prices[i-1]) {
+            profit += prices[i] - prices[i-1];
+            
         }
     }
     return profit;
+};
     // T.C: O(N)
     // S.C: O(1)
-};
 
