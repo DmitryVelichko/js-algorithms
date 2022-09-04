b759818dcfb9794bfe25fb39d4f6baeb9630c252
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-04 10:40:21 +0300

    new commit

---
+++ 10-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,9 @@
+/*
+Solution
+
+Profit from Buying - Selling - Re-buying Selling
+and
+Profit from Buying - Wait for Best price - Selling
+are the same. That's why the logic behind this solution.
+*/
+
