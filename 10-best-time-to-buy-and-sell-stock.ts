4d8e41fb0ab9263e3dc2f6439f764b4cdc685bd0
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-04 10:40:27 +0300

    new commit

--- 10-best-time-to-buy-and-sell-stock.ts
+++ 10-best-time-to-buy-and-sell-stock.ts
@@ -6,4 +6,8 @@ and
 Profit from Buying - Wait for Best price - Selling
 are the same. That's why the logic behind this solution.
 */
+var maxProfit = function(prices) {
+    if (prices === null || prices.length === 0) {
+        return 0;
+    }
 
