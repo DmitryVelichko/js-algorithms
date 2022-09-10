13b65fa5848e4530cbe9c3c67df0b0ff6d7269fc
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-10 11:31:10 +0300

    new commit

--- 13-fizz-buzz.js
+++ 13-fizz-buzz.js
@@ -12,3 +12,7 @@ var fizzBuzz = function (n) {
   arr = [];
   for (let i = 1; i <= n; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
