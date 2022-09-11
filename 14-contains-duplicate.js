2a560fc2106a5229e27e964fa53098c0369be7db
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-11 10:35:29 +0300

    new commit

--- 14-contains-duplicate.js
+++ 14-contains-duplicate.js
@@ -10,4 +10,10 @@
 var containsDuplicate = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     let a = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === a) {
+        return true;
+      }
+    }
+  }
 
