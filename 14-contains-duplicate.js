95c0c3910dfe9e745e5d02838c32df3470a3e36d
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-11 11:58:24 +0300

    new commit

--- 14-contains-duplicate.js
+++ 14-contains-duplicate.js
@@ -7,47 +7,61 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// Time - O(n), space - O(n)
 var containsDuplicate = function (nums) {
+  let memory = {};
   for (let i = 0; i < nums.length; i++) {
-    let a = nums[i];
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] === a) {
-        return true;
-      }
+    if (memory[nums[i]] === undefined) {
+      memory[nums[i]] = true;
+    } else {
+      return true;
     }
   }
   return false;
 };
 
-console.log(containsDuplicate([2, 14, 18, 22, 22]) + ' TRUE');
-console.log(containsDuplicate([1, 2, 3, 4, 5]) + ' FALSE');
+// var containsDuplicate = function (nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     let a = nums[i];
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] === a) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// };
 
-function hashSolution(nums) {
-  let testObj = {};
-  for (var i = 0; i < nums.length; i++) {
-    let aNum = nums[i];
-    if (testObj[aNum]) {
-      return true;
-    } else {
-      testObj[aNum] = true;
-    }
-  }
+// console.log(containsDuplicate([2, 14, 18, 22, 22]) + ' TRUE');
+// console.log(containsDuplicate([1, 2, 3, 4, 5]) + ' FALSE');
 
-  return false;
-}
+// function hashSolution(nums) {
+//   let testObj = {};
+//   for (var i = 0; i < nums.length; i++) {
+//     let aNum = nums[i];
+//     if (testObj[aNum]) {
+//       return true;
+//     } else {
+//       testObj[aNum] = true;
+//     }
+//   }
 
-function setSizeSolution(nums) {
-  let testSet = new Set(nums);
-  return testSet.size !== nums.length;
-}
-function setHasSolution(nums) {
-  let set = new Set();
-  for (i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      return true;
-    }
+//   return false;
+// }
 
-    set.add(nums[i]);
-  }
-  return false;
-}
+// function setSizeSolution(nums) {
+//   let testSet = new Set(nums);
+//   return testSet.size !== nums.length;
+// }
+// function setHasSolution(nums) {
+//   let set = new Set();
+//   for (i = 0; i < nums.length; i++) {
+//     if (set.has(nums[i])) {
+//       return true;
+//     }
+
+//     set.add(nums[i]);
+//   }
+//   return false;
+// }
