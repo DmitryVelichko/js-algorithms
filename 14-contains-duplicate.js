51c691351cf131570a651e1eba741c4cbf23b404
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-11 10:35:18 +0300

    new commit

--- 14-contains-duplicate.js
+++ 14-contains-duplicate.js
@@ -3,4 +3,8 @@
 // Solution
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 
