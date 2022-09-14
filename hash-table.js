41b651fc5f2b00051161ef9fa3b9519c3a234a9b
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-14 09:30:56 +0300

    new commit

--- hash-table.js
+++ hash-table.js
@@ -24,3 +24,41 @@ export default class HashTable {
    * @param {string} key
    * @return {number}
    */
+  hash(key) {
+    // For simplicity reasons we will just use character codes sum of all characters of the key
+    // to calculate the hash.
+    //
+    // But you may also use more sophisticated approaches like polynomial string hash to reduce the
+    // number of collisions:
+    //
+    // hash = charCodeAt(0) * PRIME^(n-1) + charCodeAt(1) * PRIME^(n-2) + ... + charCodeAt(n-1)
+    //
+    // where charCodeAt(i) is the i-th character code of the key, n is the length of the key and
+    // PRIME is just any prime number like 31.
+    const hash = Array.from(key).reduce(
+      (hashAccumulator, keySymbol) => (hashAccumulator + keySymbol.charCodeAt(0)),
+      0,
+    );
+
+    // Reduce hash number so it would fit hash table size.
+    return hash % this.buckets.length;
+  }
+
+  /**
+   * @param {string} key
+   * @param {*} value
+   */
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.keys[key] = keyHash;
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.find({ callback: (nodeValue) => nodeValue.key === key });
+
+    if (!node) {
+      // Insert new node.
+      bucketLinkedList.append({ key, value });
+    } else {
+      // Update value of existing node.
+      node.value.value = value;
+    }
+  }
