4de8342c0bf4083d0cab60971a382ab09906954e
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-06 23:08:56 +0400

    add algo

--- a.js
+++ a.js
@@ -8,3 +8,9 @@
  * @param {LinkedListNode} node
  * @param {traversalCallback} callback
  */
+function reverseTraversalRecursive(node, callback) {
+  if (node) {
+    reverseTraversalRecursive(node.next, callback);
+    callback(node.value);
+  }
+}
