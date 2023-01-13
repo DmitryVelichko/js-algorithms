acff3520a2ac99f82ee9441eb803ba3fe47bb37c
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-13 23:31:05 +0800

    add algo

--- a.js
+++ a.js
@@ -3,3 +3,11 @@
  * @param {GraphVertex} startVertex
  * @return {{distances, previousVertices}}
  */
+export default function bellmanFord(graph, startVertex) {
+  const distances = {};
+  const previousVertices = {};
+
+  // Init all distances with infinity assuming that currently we can't reach
+  // any of the vertices except start one.
+  distances[startVertex.getKey()] = 0;
+  graph.getAllVertices().forEach((vertex) => {
