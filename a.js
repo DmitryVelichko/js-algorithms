5bb08a9ca944d947e66d982ea8f610408c8a377a
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-17 23:40:31 +0800

    add algo

--- a.js
+++ a.js
@@ -25,3 +25,22 @@ export default class Queue {
 
     return this.linkedList.head.value;
   }
+
+  /**
+   * Add a new element to the end of the queue (the tail of the linked list).
+   * This element will be processed after all elements ahead of it.
+   * @param {*} value
+   */
+  enqueue(value) {
+    this.linkedList.append(value);
+  }
+
+  /**
+   * Remove the element at the front of the queue (the head of the linked list).
+   * If the queue is empty, return null.
+   * @return {*}
+   */
+  dequeue() {
+    const removedHead = this.linkedList.deleteHead();
+    return removedHead ? removedHead.value : null;
+  }
