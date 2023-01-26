7e6f73a16343b66ec855056cd885abbd3dfd4f7d
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-26 11:40:04 +0800

    add algo

--- rotate-image.js
+++ rotate-image.js
@@ -1,4 +1,7 @@
 let rotate = function(matrix) {
     for(let r = 0; r < matrix.length; r++) {
         for(let c = r; c < matrix[0].length; c++) {
+            [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]]
+        }
+    }
  
