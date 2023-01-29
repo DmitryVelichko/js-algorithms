0e641f2619807c09a14a4dcf301391e27da13553
Dmitry Velichko <soleil-2005@mail.ru>
2023-01-29 14:46:58 +0800

    add algo

--- intersection-of-2-arrays.js
+++ intersection-of-2-arrays.js
@@ -2,4 +2,13 @@ let intersect = function (nums1, nums2) {
   let obj = {};
   let result = [];
 
-  
+  for (let i of nums1) {
+    // make a hashmap of the first array of nums
+    // {'1': 2, '2': 2}
+    if (!obj[i]) {
+      obj[i] = 1;
+    } else {
+      obj[i]++;
+    }
+  }
+
