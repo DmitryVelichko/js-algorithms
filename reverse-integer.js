7f065a0fc20bc5eb82bf78b8f73699b13ac9b76a
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-02 00:26:49 +0800

    add algo

--- reverse-integer.js
+++ reverse-integer.js
@@ -22,3 +22,17 @@ let reverse = function (x) {
     right--;
   }
 
+  let num = arr.reduce((accum, digit) => accum * 10 + digit, 0);
+  if (x < 0) {
+    num = num * -1;
+  }
+
+  if (num < (-2) ** 31 || num > 2 ** 31 - 1) {
+    return 0;
+  }
+
+
+  return num;
+};
+
+console.log(reverse(x));
