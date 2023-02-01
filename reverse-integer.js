04de47958c8bb128769655d63fd164c00da9d22f
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-02 00:26:43 +0800

    add algo

--- reverse-integer.js
+++ reverse-integer.js
@@ -7,4 +7,18 @@ let reverse = function (x) {
     temp = Math.abs(x);
   }
 
+  let str = temp.toString().split('');
+  let arr = str.map(Number);
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    left++;
+    right--;
+  }
 
