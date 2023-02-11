6b12ff01ecf2a6c293fa76d6eace4a86e535a319
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-11 14:16:56 +0800

    add algo

--- implement-strStr.js
+++ implement-strStr.js
@@ -1,3 +1,7 @@
 let strStr = function(string, substring) {
     if(substring === '') return 0
-  
+    for(let i=0;i<=string.length - substring.length; i++){
+        let j = 0;
+        for(; j < substring.length; j++) {
+            if(substring[j] !== string[i + j]) break
+    
