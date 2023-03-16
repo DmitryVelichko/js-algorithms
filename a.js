8e8e82ce8a0077baab3d1a66243d9cdf3b3a8d95
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-16 18:43:00 +0800

    Add algo

--- a.js
+++ a.js
@@ -31,4 +31,14 @@ var isValid = function(s) {
         if(hashMap[ch]) {
             //ch is an opening bracket
             stack.push(hashMap[ch])
-    
+        } else if(stack.length > 0 && stack[stack.length - 1] === ch) {
+            // ch is a closing bracket and top of stack matches
+            stack.pop()
+        } else{
+            // ch is a closing bracket and top of the stack doesn't match
+            return false
+        }
+    }
+     
+     return stack.length === 0
+ };
