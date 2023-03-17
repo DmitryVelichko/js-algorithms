acc27d789801d4dff980a3422096506ee2b3081e
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-17 12:01:52 +0800

    Add algo

---
+++ pascals-triangle.js
@@ -0,0 +1,7 @@
+// https://www.youtube.com/watch?v=y24nMGNkkPo&ab_channel=shirinsetayesh
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
