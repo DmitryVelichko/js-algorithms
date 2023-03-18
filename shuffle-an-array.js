0c4170b990ce500c7394eabeb6558b463656ad77
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-18 12:04:50 +0800

    Add algo

--- shuffle-an-array.js
+++ shuffle-an-array.js
@@ -38,3 +38,9 @@ Solution.prototype.shuffle = function() {
     return shuffled
 };
 
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
