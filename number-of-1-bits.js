265688240082f7998954321cacdf8853db9d94f4
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-20 19:58:30 +0800

    Add algo

--- number-of-1-bits.js
+++ number-of-1-bits.js
@@ -23,3 +23,22 @@
 // Output: 1
 // Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
 // Example 3:
+
+// Input: n = 11111111111111111111111111111101
+// Output: 31
+// Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+ 
+
+// Constraints:
+
+// The input must be a binary string of length 32.
+ 
+
+// Follow up: If this function is called many times, how would you optimize it?
+// JavaScript	
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+
