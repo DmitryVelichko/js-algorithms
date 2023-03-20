634c26b90daea5b8b807307fc7f7cececbaf5260
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-20 19:58:11 +0800

    Add algo

---
+++ number-of-1-bits.js
@@ -0,0 +1,10 @@
+// https://www.youtube.com/watch?v=KksDoeDaTbk&ab_channel=AlgoJS
+// https://www.youtube.com/watch?v=egUyaWtsQc0&list=PLdo5W4Nhv31a8UcMN9-35ghv8qyFWD9_S&index=19
+
+// Number of 1 Bits
+
+// Solution
+// Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+// Note:
+
