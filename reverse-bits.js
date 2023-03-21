613bd0197dc1808133b59408bdd762df1978e385
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-21 08:59:51 +0800

    Add algo

--- reverse-bits.js
+++ reverse-bits.js
@@ -7,3 +7,17 @@
 // In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
 
 
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let result = 0;
+    
+    for(let i=0; i< 32; i++) {
+        
+        let lastBit = n & 1;
+       
+        let revBit = lastBit << (31-i)
+        
+        result = result | revBit
