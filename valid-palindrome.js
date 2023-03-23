fb5132ff3353b57a2a1746b2f5c25e2cff5ac07c
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-23 11:26:00 +0800

    Add algo

--- valid-palindrome.js
+++ valid-palindrome.js
@@ -1,40 +1,6 @@
-let isPalindrome = function (s) {
-  s = s.replace(/[^a-z0-9]/gi, '')
-  let leftIndex = 0;
-  let rightIndex = s.length - 1;
+//  Palindrome Number
 
-  while(leftIndex < rightIndex) {
-    if(s[leftIndex].toLowerCase() !== s[rightIndex].toLowerCase()) {
-      return false
-    }
-    leftIndex++
-    rightIndex--
-  }
-  return true
-}
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
 
-console.log(isPalindrome('racecar'));
-
-// let isPalindrome = function (s) {
-//   s = s
-//     .split(' ')
-//     .filter((e) => e.trim().length)
-//     .join('');
-//   s = s.replace(/,/g, '');
-//   s = s.toLowerCase();
-//   s = s.replace(/:/g, '');
-
-//   s = s.replace('.', "")
-//   s = s.replace(/[^a-zA-Z0-9 ]/g, "")
-//   console.log(s);
-//   let result = '';
-//   for (let i of s) {
-//     result = i + result;
-//   }
-
-//   if (s === result || s === '') {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
