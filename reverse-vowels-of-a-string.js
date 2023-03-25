838429b6e6501814433a38f7c67e8be904a088d2
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-25 09:40:51 +0800

    Add algo

--- reverse-vowels-of-a-string.js
+++ reverse-vowels-of-a-string.js
@@ -23,3 +23,21 @@ var reverseVowels = function(s) {
         while(left < right && !(s[left] in vowels)) {
             left += 1;
         }
+        while(left < right && !(s[right] in vowels)) {
+            right -= 1;
+        }
+
+        swap(characters, left, right);
+        left +=1;
+        right -=1;
+    }
+
+    return characters.join('')
+
+}
+
+function swap (arr, i, j) {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
