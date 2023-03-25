53d3dbe2d87e8660a36da29714dc5d517d777954
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-25 09:40:39 +0800

    Add algo

--- reverse-vowels-of-a-string.js
+++ reverse-vowels-of-a-string.js
@@ -4,3 +4,14 @@
 
 // The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowels = {} // {a: true, A:true, ...}
+    for (const char of 'aeiouAEIOU') {
+        vowels[char] = true
+    }
+
+    const characters = s.split('');
