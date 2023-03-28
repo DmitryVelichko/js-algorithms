4abed185ade7f9b6154bcccc118730699366cb91
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-28 15:07:39 +0800

    Add algo

--- majority-element.js
+++ majority-element.js
@@ -19,3 +19,9 @@
  * @param {number[]} nums
  * @return {number}
  */
+var majorityElement = function(nums) {
+    const ht = {}
+
+    for(const num of nums) {
+        ht[num] = ht[num] + 1 || 1
+    }
