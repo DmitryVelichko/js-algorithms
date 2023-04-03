89be37ad3f8cce21fd23d21f8e97dad186a03fe3
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-03 09:25:20 +0800

    Add algo

--- design-hashmap.js
+++ design-hashmap.js
@@ -18,3 +18,16 @@ var MyHashMap = function() {
  * @param {number} value
  * @return {void}
  */
+MyHashMap.prototype.put = function(key, value) {
+    this.hashMap[key] = value
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+   let value = this.hashMap[key]
+   return value !== undefined ? value : -1
+};
+
