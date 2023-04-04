feba2365f4e151bcca3d03376fa6da0f6af9bbba
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-04 23:25:14 +0800

    Add algo

--- 167-two-sum-2.js
+++ 167-two-sum-2.js
@@ -42,3 +42,12 @@ var twoSum = function(numbers, target) {
             lower +=1
         } else if(sum > target) {
             upper -=1
+        } else {
+            return [lower + 1, upper + 1]
+        }
+
+        
+    }
+};
+
+console.log(twoSum([2,7,11,15], 9))
