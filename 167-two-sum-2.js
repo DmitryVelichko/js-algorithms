5b2c2e597440261f7c32fe2df8b999c83a7d6f0d
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-04 23:25:09 +0800

    Add algo

--- 167-two-sum-2.js
+++ 167-two-sum-2.js
@@ -35,3 +35,10 @@
 var twoSum = function(numbers, target) {
     let lower = 0;
     let upper = numbers.length - 1;
+
+    while(upper > lower) {
+        let sum = numbers[lower] + numbers[upper]
+        if(sum < target) {
+            lower +=1
+        } else if(sum > target) {
+            upper -=1
