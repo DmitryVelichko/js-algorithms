97fccf50b501c36dc9c4a9dbd66f5b7b48956f1b
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-06 23:05:09 +0800

    Add algo

--- 442-find-all-duplicates-in-array.js
+++ 442-find-all-duplicates-in-array.js
@@ -16,3 +16,13 @@
 
 // Input: nums = [1]
 // Output: []
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+    let result = []
+    for (let i = 0; i < nums.length; i++) {
+        let value = Math.abs(nums[i])
+        let index = value - 1;
