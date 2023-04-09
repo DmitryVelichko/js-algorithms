32e183d1a5248178a9d65e2a26d9fb07de21607c
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-09 11:06:40 +0800

    Add algo

--- 2-add-2-numbers.js
+++ 2-add-2-numbers.js
@@ -46,3 +46,17 @@ var addTwoNumbers = function (l1, l2) {
             val1 = l1.val;
             l1 = l1.next
         }
+
+        if (l2) {
+            val2 = l2.val;
+            l2 = l2.next;
+        }
+        const sum = val1 + val2 + carry
+        carry = Math.floor(sum / 10) // sum > 9 ? 1 : 0
+        const digit = sum % 10
+        const currentNode = new ListNode(digit)
+        previousNode.next = currentNode
+        previousNode = currentNode;
+    }
+    return headNode.next;
+};
