40894c5d2ee500ab7dfe26c0fb3cce8536a18f57
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-12 12:08:11 +0800

    Add algo

--- 27-remove-elements.js
+++ 27-remove-elements.js
@@ -15,3 +15,22 @@
 // int val = ...; // Value to remove
 // int[] expectedNums = [...]; // The expected answer with correct length.
 //                             // It is sorted with no values equaling val.
+
+// int k = removeElement(nums, val); // Calls your implementation
+
+// assert k == expectedNums.length;
+// sort(nums, 0, k); // Sort the first k elements of nums
+// for (int i = 0; i < actualLength; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+
+// Example 1:
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
