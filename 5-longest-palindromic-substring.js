262758ee3231a32c3532480e6299349392640857
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-14 10:40:16 +0800

    Add algo

--- 5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring.js
@@ -12,3 +12,16 @@
 // Input: s = "cbbd"
 // Output: "bb"
 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+/**
+ * @param {string} s
+ * @return {string}
+*/
+const longestPalindrome = function (str) {
+    if (str.length < 1 || str === null) return '';
+    let longest = ''
