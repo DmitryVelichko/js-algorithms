64ca0f0cf966a6845a06146e2207e2b487045366
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-22 13:09:38 +0800

    Add algo

--- 876-Middle-of-the-Linked-List.js
+++ 876-Middle-of-the-Linked-List.js
@@ -30,3 +30,18 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next
+    }
+    return slow
+};
