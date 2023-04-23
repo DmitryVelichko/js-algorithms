8d9ee6d72d59cd3db6509b89d3d85d2b94e0c338
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-23 10:25:07 +0800

    Add algo

--- 142-linked-list-cycle-II.js
+++ 142-linked-list-cycle-II.js
@@ -39,3 +39,21 @@ var detectCycle = function (head) {
 
     let fast = head;
     let slow = head;
+
+    let pointer = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast === slow) break
+    }
+
+    if (fast !== slow) return null
+
+    while (pointer !== slow) {
+        pointer = pointer.next;
+        slow = slow.next;
+    }
+
+    return slow;
+};
