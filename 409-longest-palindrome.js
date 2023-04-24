176b1a824d4f08fe27f662c178081517ee0cc00a
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-24 11:55:14 +0800

    Add algo

---
+++ 409-longest-palindrome.js
@@ -0,0 +1,4 @@
+// 409. Longest Palindrome
+// Easy
+
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
