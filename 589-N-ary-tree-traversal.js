682040a9da1a7da9350bab016ca19685e1d834bf
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-25 22:03:29 +0800

    Add algo

--- 589-N-ary-tree-traversal.js
+++ 589-N-ary-tree-traversal.js
@@ -31,3 +31,17 @@
  *    this.children = children;
  * };
  */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root, res = []) {
+    if(!root) return []
+    res.push(root.val)
+
+    for(let i = 0; i<root.children.length; i++) {
+        preorder(root.children[i], res)
+    }
+    return res
+};
