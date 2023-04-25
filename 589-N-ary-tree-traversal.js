2ac3e8db322cd6221dc8216059c1bf3aba5ba98c
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-25 22:03:23 +0800

    Add algo

--- 589-N-ary-tree-traversal.js
+++ 589-N-ary-tree-traversal.js
@@ -12,3 +12,22 @@
 // Output: [1,3,5,6,2,4]
 // Example 2:
 
+
+
+// Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// 0 <= Node.val <= 104
+// The height of the n-ary tree is less than or equal to 1000.
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
