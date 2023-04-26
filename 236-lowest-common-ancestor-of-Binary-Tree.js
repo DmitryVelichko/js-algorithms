bdd351abc190e7385d5256b8af707b3d298a1aa1
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-26 10:34:28 +0800

    Add algo

--- 236-lowest-common-ancestor-of-Binary-Tree.js
+++ 236-lowest-common-ancestor-of-Binary-Tree.js
@@ -27,3 +27,15 @@ var lowestCommonAncestor = function (root, p, q) {
 
         let left = dfs(node.left)
         let right = dfs(node.right)
+
+        let cur = node === p || node === q
+
+        if (left + right + cur >= 2) result = node
+
+        return left || right || cur;
+
+    }
+
+    dfs(root)
+    return result
+};
