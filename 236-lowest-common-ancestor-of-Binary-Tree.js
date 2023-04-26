ccbe81155316d8bcb69db7d63ad4c562e44621db
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-26 10:34:24 +0800

    Add algo

--- 236-lowest-common-ancestor-of-Binary-Tree.js
+++ 236-lowest-common-ancestor-of-Binary-Tree.js
@@ -12,3 +12,18 @@
  *     this.left = this.right = null;
  * }
  */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    let result = null
+
+    const dfs = (node) => {
+        if (node === null) return false;
+
+        let left = dfs(node.left)
+        let right = dfs(node.right)
