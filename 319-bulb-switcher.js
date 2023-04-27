686860b25ce62baf7478532ca3cae176a354af3e
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-27 09:23:31 +0800

    Add algo

--- 319-bulb-switcher.js
+++ 319-bulb-switcher.js
@@ -8,3 +8,36 @@
 // Return the number of bulbs that are on after n rounds.
 
  
+
+// Example 1:
+
+
+// Input: n = 3
+// Output: 1
+// Explanation: At first, the three bulbs are [off, off, off].
+// After the first round, the three bulbs are [on, on, on].
+// After the second round, the three bulbs are [on, off, on].
+// After the third round, the three bulbs are [on, off, off]. 
+// So you should return 1 because there is only one bulb is on.
+// Example 2:
+
+// Input: n = 0
+// Output: 0
+// Example 3:
+
+// Input: n = 1
+// Output: 1
+ 
+
+// Constraints:
+
+// 0 <= n <= 109
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+    
+    return Math.floor(Math.sqrt(n))
+};
