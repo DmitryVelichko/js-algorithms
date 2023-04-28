0a5677fa81f9a013a10ddf364ea9f100dba33c95
Dmitry Velichko <soleil-2005@mail.ru>
2023-04-28 11:18:01 +0800

    Add algo

--- 200-number-of-islands.js
+++ 200-number-of-islands.js
@@ -50,3 +50,13 @@ var numIslands = function (grid) {
     }
     return countIslands
 };
+
+
+const teraform = (rowIn, colIn, grid) => {
+    if (grid[rowIn] === undefined || grid[rowIn][colIn] === undefined || grid[rowIn][colIn] === '0') return
+    grid[rowIn][colIn] = '0'
+    teraform(rowIn + 1, colIn, grid)
+    teraform(rowIn - 1, colIn, grid)
+    teraform(rowIn, colIn + 1, grid)
+    teraform(rowIn, colIn - 1, grid)
+}
