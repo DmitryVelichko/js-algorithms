2514a9eeafe5992c81ee25f085eb38853c7b2bac
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-02 09:49:25 +0800

    Add algo

--- 1822-Sign-of-the-Product-of-an-Array.js
+++ 1822-Sign-of-the-Product-of-an-Array.js
@@ -16,3 +16,35 @@
 // Output: 1
 // Explanation: The product of all values in the array is 144, and signFunc(144) = 1
 // Example 2:
+
+// Input: nums = [1,5,0,2,-3]
+// Output: 0
+// Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+// Example 3:
+
+// Input: nums = [-1,1,-1,1,-1]
+// Output: -1
+// Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -100 <= nums[i] <= 100
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let ans = 1
+    for(let num of nums) {
+        if(num === 0) return 0
+        if(num < 0) ans = -ans
+    }
+
+    return ans
+};
+
+let a = arraySign([-1,-2,-3,-4,3,2,1])
+
