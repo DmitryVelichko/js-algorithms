239031a4736d4f78cd9c18147a7fbe856e462d9a
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-03 12:04:02 +0800

    Add algo

--- 2215-Find-the-Difference-of-Two-Arrays.js
+++ 2215-Find-the-Difference-of-Two-Arrays.js
@@ -27,3 +27,24 @@
 
 // 1 <= nums1.length, nums2.length <= 1000
 // -1000 <= nums1[i], nums2[i] <= 1000
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1)
+    let set2 = new Set(nums2)
+    
+    for(let num of nums1) {
+        if(set2.has(num)) {
+            set1.delete(num)
+            set2.delete(num)
+        }
+    }
+    
+    return  [[...set1], [...set2]]
+}
+
+findDifference([1,2,3],[2,4,6]) // [[1,3],[4,6]]
