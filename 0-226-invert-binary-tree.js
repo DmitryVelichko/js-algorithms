d4cef552fa1c6b04cad4d787d155c126faa784d9
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-05 10:00:18 +0800

    Add algo

--- 0-226-invert-binary-tree.js
+++ 0-226-invert-binary-tree.js
@@ -8,3 +8,20 @@
 // Input: root = [4,2,7,1,3,6,9]
 // Output: [4,7,2,9,6,3,1]
 // Example 2:
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// Recursive Approach:
