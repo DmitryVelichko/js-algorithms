6b71bd009eb5b5014833f64787fa875bb1328a20
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-05 10:00:09 +0800

    Add algo

---
+++ 0-226-invert-binary-tree.js
@@ -0,0 +1,10 @@
+// 226. Invert Binary Tree
+// Easy
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+// Example 2:
