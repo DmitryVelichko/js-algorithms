4f9ef43a036e7c3f447dd66767083b780cccdef6
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-06 10:36:33 +0800

    Add algo

---
+++ 0-94-binary-tree-inorder-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    
+    if(root === null) return []
+    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
+    
+};
+
+// function inorderTraversal(root) {
+//     const stack = [];
+//     const res = [];
+  
+//     while (root || stack.length) {
+//       if (root) {
+//         stack.push(root);
+//         root = root.left;
+//       } else {
+//         root = stack.pop();
+//         res.push(root.val);
+//         root = root.right;
+//       }
+//     }
+  
+//     return res;
+//   }
