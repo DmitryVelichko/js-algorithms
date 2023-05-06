676a9f892c45f720e13bb8577c30df0962007c85
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-06 10:25:29 +0800

    Add algo

--- 94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal.js
@@ -6,3 +6,13 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// var inorderTraversal = function(root) {
+    
+//     if(root === null) return []
+//     return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
+    
+// };
