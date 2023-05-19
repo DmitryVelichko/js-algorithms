bdce9833ba5253fae3d1d489bb5220fd1d6f851a
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-19 16:00:00 +0800

    Add algo

--- 0-704-binary-search.js
+++ 0-704-binary-search.js
@@ -32,4 +32,23 @@
  * @return {number}
  */
 
+// Time: O(log n), space: O(1)
+var search = function(nums, target) {
 
+    let start = 0;
+    let end = nums.length - 1
+    
+    while(start<=end){
+        let middle = Math.floor((start + end)/2)
+    
+        if(nums[middle] === target) {
+            return middle
+        } else if(nums[middle] > target) {
+            end = middle - 1
+        } else { 
+            start = middle + 1
+        }
+    }
+    return -1
+    
+};
