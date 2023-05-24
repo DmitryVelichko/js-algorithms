7e0cf96af262c43ca02396c22e150a905ee9e0b4
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-24 10:10:02 +0800

    Add algo

--- v0-104-max-depth-of-binary-tree.js
+++ v0-104-max-depth-of-binary-tree.js
@@ -10,3 +10,19 @@
 // Example 1:
 
 
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
