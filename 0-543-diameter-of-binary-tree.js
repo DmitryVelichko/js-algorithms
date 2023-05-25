418011239cb69b5508eb54836436cd3de30219b4
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-25 21:34:11 +0800

    Add algo

--- 0-543-diameter-of-binary-tree.js
+++ 0-543-diameter-of-binary-tree.js
@@ -32,3 +32,18 @@
     if(root === null) return 0
     let max = 0;
 
+    function dfs(node) {
+        if(node === null) return 0
+        
+        let left = dfs(node.left)
+        let right = dfs(node.right)
+
+       // сохраняем max number of edges (in subtrees)
+        max = Math.max(left + right, max)
+        //считаем максимум edges + 1
+        return Math.max(left, right) + 1
+    }
+
+    dfs(root)
+    return max
+};
