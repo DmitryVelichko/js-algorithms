8a0960cc117838f2261d949f12eb8f2d2432be1e
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-27 08:59:54 +0800

    Add algo

--- 2. Stack/20-valid-parenteses.js
+++ 2. Stack/20-valid-parenteses.js
@@ -21,7 +21,12 @@
 
 //Time: O(n), Space: O(n), if the string is '(((((((', the stack
 // will end up having the size equal to the string.
-
+// 0 Задача на стек
+// 1 создаем пустой стек stack = []
+// 2 в цикле for of проходим по стринге s
+// 3 дальше 3 проверки if/else if если символ в строке совпадает с открытой скобкой, пушим в стек закрытую, делаем это для всех скобок
+// 4 4-ой проверкой else if проверяем, равен ли символ stack.pop(), если нет, возвращаем false
+// 5 Вне цикла возвращаем stack.length === 0
 var isValid = function (s) {
     const stack = []
 
