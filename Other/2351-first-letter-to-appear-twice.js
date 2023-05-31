e8b5b1d736753a6fac37fd78002affedb81cf6ef
Dmitry Velichko <soleil-2005@mail.ru>
2023-05-31 10:17:19 +0800

    Add change buttons

---
+++ Other/2351-first-letter-to-appear-twice.js
@@ -0,0 +1,10 @@
+// 2351. First Letter to Appear Twice
+// Easy
+// 772
+// 41
+// Companies
+// Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+// Note:
+
+// A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
