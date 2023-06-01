8efbfcdfb2ae145f8e787a14fa1a30cb46341d8d
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-01 10:31:36 +0800

    Add algo

--- 0. Arrays & Hashing/238-product-of-array-except-self.js
+++ 0. Arrays & Hashing/238-product-of-array-except-self.js
@@ -29,3 +29,16 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
+ */
+
+
+// Задача на Arrays and Hashing
+// T: O(n), S:O(1) result array не считается по условиям задачи
+var productExceptSelf2 = function (nums) {
+    const res = [];
+    let start = 1;
+    let start2 = 1
+
+    for (let i = 0; i < nums.length; i++) {
+        res.push(start)
+        start = start * nums[i]
