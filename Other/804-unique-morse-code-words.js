f917f26d3f3eb7ff729e53bb5bda612847a632a3
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-02 13:01:49 +0800

    Add algo

--- Other/804-unique-morse-code-words.js
+++ Other/804-unique-morse-code-words.js
@@ -56,8 +56,29 @@ var uniqueMorseRepresentations = function (words) {
     }
 
     return new Set(words.map(word => word.split('').map(letter => alphabet[letter]).join(''))).size
+
 };
 
 let a = uniqueMorseRepresentations(["gin", "zen", "gig", "msg"])
 
 console.log(a)
+
+const morseCode = { a: ".-", b: "-...", c: "-.-.", d: "-..", e: ".", f: "..-.", g: "--.", h: "....", i: "..", j: ".---", k: "-.-", l: ".-..", m: "--", n: "-.", o: "---", p: ".--.", q: "--.-", r: ".-.", s: "...", t: "-", u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--", z: "--.." }
+
+function uniqueMorseRepresentations(words) {
+    const set = new Set();
+
+    for (let i = 0; i < words.length; i++) {
+        let encryptedWord = "";
+
+        for (let l = 0; l < words[i].length; l++) {
+            const character = words[i][l];
+
+            encryptedWord += morseCode[character];
+        }
+
+        set.add(encryptedWord);
+    }
+
+    return set.size;
+}
