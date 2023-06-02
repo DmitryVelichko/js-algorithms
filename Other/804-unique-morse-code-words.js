d01921b117e7587fc11b9c323b85c684050c885a
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-02 12:41:52 +0800

    Add algo

---
+++ Other/804-unique-morse-code-words.js
@@ -0,0 +1,10 @@
+// 804. Unique Morse Code Words
+// Easy
+
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+// 'a' maps to ".-",
+// 'b' maps to "-...",
+// 'c' maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
