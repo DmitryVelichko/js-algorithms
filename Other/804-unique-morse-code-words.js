014dc939c6bd976b5d51b9d1db6f4bd39508ffba
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-02 12:42:24 +0800

    Add algo

--- Other/804-unique-morse-code-words.js
+++ Other/804-unique-morse-code-words.js
@@ -54,3 +54,10 @@ var uniqueMorseRepresentations = function (words) {
         a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--',
         n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
     }
+
+    return new Set(words.map(word => word.split('').map(letter => alphabet[letter]).join(''))).size
+};
+
+let a = uniqueMorseRepresentations(["gin", "zen", "gig", "msg"])
+
+console.log(a)
