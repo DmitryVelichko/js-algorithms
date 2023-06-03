774fb3489ccdc2696b2166af341edd1403e8c65b
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-03 14:15:43 +0800

    Add algo

---
+++ Other/1002-find-common-characters.js
@@ -0,0 +1,5 @@
+// 1002. Find Common Characters
+// Easy
+
+// Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
