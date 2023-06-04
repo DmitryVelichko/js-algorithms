b496e79b2ad767c58ed8e183239d77affde162d9
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-04 12:18:00 +0800

    Add algo

--- Other/9-remove-duplicate-from-array.js
+++ Other/9-remove-duplicate-from-array.js
@@ -6,3 +6,13 @@ let removeDuplicates0 = function(nums) {
     let k = 1;
     for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] !== nums[i+1]) {
+           nums[k] = nums[i + 1]
+            k++;
+        }
+    }
+    return k;
+};
+
+console.log(removeDuplicates0(nums))
+
+
