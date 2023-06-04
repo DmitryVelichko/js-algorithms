0aa8ff683c328ac66fd2592118b5730a1628b7be
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-04 12:17:54 +0800

    Add algo

---
+++ Other/9-remove-duplicate-from-array.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+
+let nums = [1,1,2,2,2,3,3,4,4]
+
+let removeDuplicates0 = function(nums) {
+    let k = 1;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i+1]) {
