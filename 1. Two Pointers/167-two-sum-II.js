fb0745a7a539c3f7ebb9f58f2e55a427249a864c
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-05 08:46:31 +0800

    Add algo

--- 1. Two Pointers/167-two-sum-II.js
+++ 1. Two Pointers/167-two-sum-II.js
@@ -48,3 +48,15 @@
 var twoSum2 = function (numbers, target) {
     let lower = 0;
     let upper = numbers.length - 1;
+
+    while (upper > lower) {
+        let sum = numbers[lower] + numbers[upper]
+        if (sum < target) {
+            lower += 1
+        } else if (sum > target) {
+            upper -= 1
+        } else {
+            return [lower + 1, upper + 1]
+        }
+    }
+};
