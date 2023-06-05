cfe6c6b0a0c9a5cf335e731c4083bdea20fa2f52
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-05 08:46:16 +0800

    Add algo

--- 1. Two Pointers/167-two-sum-II.js
+++ 1. Two Pointers/167-two-sum-II.js
@@ -20,3 +20,16 @@
 // Output: [1,3]
 // Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
 // Example 3:
+
+// Input: numbers = [-1,0], target = -1
+// Output: [1,2]
+// Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+
+// Constraints:
+
+// 2 <= numbers.length <= 3 * 104
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
