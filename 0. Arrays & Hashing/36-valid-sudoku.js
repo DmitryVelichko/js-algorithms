fd68187dfffcddb63b7533c1a4fa09b657db1f36
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-08 11:21:49 +0800

    Add algo

--- 0. Arrays & Hashing/36-valid-sudoku.js
+++ 0. Arrays & Hashing/36-valid-sudoku.js
@@ -53,3 +53,12 @@
 //  T: O(1), S: O(1), так как размер доски фиксирован 9х9
 var isValidSudoku2 = function (board) {
     const set = new Set()
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            const cell = board[i][j]
+            if (cell === '.') continue
+            const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3)
+            const row = `row: ${i}, value: ${cell}`
+            const col = `col: ${j}, value: ${cell}`
+            const box = `box: ${boxNum}, value: ${cell}`
