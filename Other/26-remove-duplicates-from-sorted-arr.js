bdc2b086a5acdbe5ca4de083135799e5915e8e38
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-09 10:43:28 +0800

    Add algo

--- Other/26-remove-duplicates-from-sorted-arr.js
+++ Other/26-remove-duplicates-from-sorted-arr.js
@@ -57,3 +57,11 @@ var removeDuplicates = function(nums) {
     }
     return ++i;
 };
+// var removeDuplicates = function(nums) {
+//    let index = 1
+//    for(let i=0; i<nums.length-1; i++){
+//        if(nums[i] !== nums[i+1]) nums[index] = nums[i+1]
+//        index++
+//    }
+//    return index
+// };
