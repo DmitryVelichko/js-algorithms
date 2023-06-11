ec95bb1f066d4ba0ee7013d2243e1d1631298a77
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-11 09:59:27 +0800

    Add algo

--- 2. Stack/22-generate-parenthesis.js
+++ 2. Stack/22-generate-parenthesis.js
@@ -60,4 +60,19 @@ function generateParenthesis(n) {
   //       return;
   //     }
   
- 
+  //     if (openN < n) {
+  //       stack.push('(');
+  //       backtrack(openN + 1, closedN);
+  //       stack.pop();
+  //     }
+  //     if (closedN < openN) {
+  //       stack.push(')');
+  //       backtrack(openN, closedN + 1);
+  //       stack.pop();
+  //     }
+  //   }
+  
+  //   backtrack(0, 0);
+  //   return res;
+  // }
+  
