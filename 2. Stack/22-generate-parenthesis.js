33f79c6c1e18d3e3723dd88d247a87a39cfe4c8f
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-11 09:58:50 +0800

    Add algo

---
+++ 2. Stack/22-generate-parenthesis.js
@@ -0,0 +1,9 @@
+// 22. Generate Parentheses
+// Medium
+
+// Companies
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+
+// Example 1:
+
