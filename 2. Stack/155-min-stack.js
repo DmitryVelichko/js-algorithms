b844a86b54e20d5204e08c849cf28cec36a7f5f9
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-17 09:44:32 +0800

    Add algo

--- 2. Stack/155-min-stack.js
+++ 2. Stack/155-min-stack.js
@@ -32,3 +32,21 @@
 // minStack.pop();
 // minStack.top();    // return 0
 // minStack.getMin(); // return -2
+ 
+
+// Constraints:
+
+// -231 <= val <= 231 - 1
+// Methods pop, top and getMin operations will always be called on non-empty stacks.
+// At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+var MinStack = function () {
+    this.stack = []
+    this.minStack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+// T: O(1)
