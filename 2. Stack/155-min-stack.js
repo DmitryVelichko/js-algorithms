d4a4cc6a47584314f31fe51e89b8246731203ad7
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-17 09:44:38 +0800

    Add algo

--- 2. Stack/155-min-stack.js
+++ 2. Stack/155-min-stack.js
@@ -50,3 +50,23 @@ var MinStack = function () {
  * @return {void}
  */
 // T: O(1)
+MinStack.prototype.push = function (val) {
+    if (this.stack.length === 0) {
+        this.minStack.push(val)
+        this.stack.push(val)
+    } else {
+        this.minStack.push(Math.min(val, this.minStack[this.minStack.length - 1]))
+        this.stack.push(val)
+    }
+};
+
+/**
+ * @return {void}
+ */
+// T: O(1)
+MinStack.prototype.pop = function () {
+    if (!this.stack.length) return null
+    this.minStack.pop()
+    return this.stack.pop()
+
+};
