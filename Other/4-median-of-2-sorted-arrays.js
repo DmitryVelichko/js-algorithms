8bf2b87d766b73f31ee9c0c66acef94473ec8922
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-20 09:42:10 +0800

    Add algo

--- Other/4-median-of-2-sorted-arrays.js
+++ Other/4-median-of-2-sorted-arrays.js
@@ -42,3 +42,22 @@ var findMedianSortedArrays = function(nums1, nums2) {
     while(low <= high) {
         const partitionX = (high + low) >> 1
         const partitionY = ((x + y + 1) >> 1) - partitionX
+        
+        const maxX = partitionX == 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1]
+        const maxY = partitionY == 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1]
+        
+        const minX = partitionX == nums1.length ? Number.POSITIVE_INFINITY : nums1[partitionX]
+        const minY = partitionY == nums2.length ? Number.POSITIVE_INFINITY : nums2[partitionY ]
+        
+        if(maxX <= minY && maxY <= minX) {
+            const lowMax = Math.max(maxX, maxY)
+            if( (x + y) % 2 == 1)
+                return lowMax
+            return (lowMax + Math.min(minX, minY)) / 2
+        } else if(maxX < minY) {
+            low = partitionX + 1
+        } else 
+            high = partitionX - 1
+    }
+    
+};
