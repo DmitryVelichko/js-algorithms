e138913dafc321dd27bbc9d19ff450d043f14107
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-22 16:18:02 +0800

    Add algo

---
+++ Other/1700-Number-Of-Students.js
@@ -0,0 +1,7 @@
+// 1700. Number of Students Unable to Eat Lunch
+// Easy
+// 1.4K
+// 84
+// Companies
+// The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+
