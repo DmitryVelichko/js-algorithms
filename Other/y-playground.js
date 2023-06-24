1905ba3313283be36f296db0cb1e90ab12ecf6d1
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-24 23:33:38 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -20,3 +20,20 @@
 // The distinct triplets are [-1,0,1] and [-1,-1,2].
 // Notice that the order of the output and the order of the triplets does not matter.
 // Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
+// T: O(n^2), S: O(1)
