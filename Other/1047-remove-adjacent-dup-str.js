461c3d884816a2450888e025f786d6243902b4dd
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-27 10:02:09 +0800

    Add algo

--- Other/1047-remove-adjacent-dup-str.js
+++ Other/1047-remove-adjacent-dup-str.js
@@ -18,3 +18,16 @@
 // For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 // Example 2:
 
+// Input: s = "azxxzy"
+// Output: "ay"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
