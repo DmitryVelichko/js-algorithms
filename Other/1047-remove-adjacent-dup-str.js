f1c53348ea547cf153ab579cbc46bcbcd6eb72bd
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-27 10:02:01 +0800

    Add algo

--- Other/1047-remove-adjacent-dup-str.js
+++ Other/1047-remove-adjacent-dup-str.js
@@ -9,3 +9,12 @@
 // Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
 
  
+
+// Example 1:
+
+// Input: s = "abbaca"
+// Output: "ca"
+// Explanation: 
+// For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+// Example 2:
+
