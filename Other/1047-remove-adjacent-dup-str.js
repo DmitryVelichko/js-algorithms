4a4256e94f8766807912585c12e47dc367d22aff
Dmitry Velichko <soleil-2005@mail.ru>
2023-06-27 10:02:14 +0800

    Add algo

--- Other/1047-remove-adjacent-dup-str.js
+++ Other/1047-remove-adjacent-dup-str.js
@@ -31,3 +31,11 @@
  * @param {string} s
  * @return {string}
  */
+var removeDuplicates = function(s) {
+    const stack = []
+ 
+    for(let char of s) {
+        char === stack[stack.length - 1] ? stack.pop() : stack.push(char)
+    }
+    return stack.join('')
+ }
