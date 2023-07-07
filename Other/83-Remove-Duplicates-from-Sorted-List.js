8031ec867f7fde354b02de8c6cc1f1ecd719f606
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-07 08:50:02 +0800

    Add algo

--- Other/83-Remove-Duplicates-from-Sorted-List.js
+++ Other/83-Remove-Duplicates-from-Sorted-List.js
@@ -8,3 +8,14 @@
 // Input: head = [1,1,2]
 // Output: [1,2]
 // Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
