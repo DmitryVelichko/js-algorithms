6de3dff93204e01147b5eef8972b5897e19f63f3
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-07 08:50:18 +0800

    Add algo

--- Other/83-Remove-Duplicates-from-Sorted-List.js
+++ Other/83-Remove-Duplicates-from-Sorted-List.js
@@ -39,3 +39,10 @@ var deleteDuplicates = function (head) {
     while (current) {
         if (current.next !== null && current.val == current.next.val) {
             current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
