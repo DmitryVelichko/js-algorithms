711bce29bbb6b2b64120549d1b8e471de9fe11f7
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-09 09:59:00 +0800

    Add algo

--- Other/1290-Convert-Binary-Number-in-Linked-List-to-Integer.js
+++ Other/1290-Convert-Binary-Number-in-Linked-List-to-Integer.js
@@ -16,3 +16,18 @@
 
 // Input: head = [0]
 // Output: 0
+ 
+
+// Constraints:
+
+// The Linked List is not empty.
+// Number of nodes will not exceed 30.
+// Each node's value is either 0 or 1.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
