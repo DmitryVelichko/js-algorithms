4b13ed7883817ec5035591b97cb048df6942e60b
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-09 10:02:32 +0800

    Add algo

--- JS Interview Questions/2619-Array-Prototype-Last.js
+++ JS Interview Questions/2619-Array-Prototype-Last.js
@@ -21,3 +21,14 @@
 // Constraints:
 
 // 0 <= arr.length <= 1000
+
+Array.prototype.last = function() {
+    if(this.length === 0) return -1
+    else return this[this.length-1]
+   
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
