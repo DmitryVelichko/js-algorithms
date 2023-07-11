73bbee04c490f0e1667659e5f8ce916b3c884874
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-11 09:38:42 +0800

    Add algo

--- Other/2621-sleep.js
+++ Other/2621-sleep.js
@@ -10,3 +10,11 @@
 // Explanation: It should return a promise that resolves after 100ms.
 // let t = Date.now();
 // sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+// Example 2:
+
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+ 
