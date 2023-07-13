6fca00ae30ae16dacd2f36cbad31f3fc1eabd534
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-13 11:23:02 +0800

    Add algo

--- Other/2627-debounce.js
+++ Other/2627-debounce.js
@@ -74,3 +74,10 @@
  * @param {number} t milliseconds
  * @return {Function}
  */
+var debounce = function(fn, t) {
+    let timer
+    return function(...args) {
+       clearTimeout(timer)
+       timer = setTimeout(() => fn(...args), t)
+    }
+};
