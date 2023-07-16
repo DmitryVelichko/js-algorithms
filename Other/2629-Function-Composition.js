6d06c16d1ec8bb9087dc85b6007dfabcf85b7b61
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-16 12:16:14 +0800

    Add algo

--- Other/2629-Function-Composition.js
+++ Other/2629-Function-Composition.js
@@ -50,3 +50,10 @@
  * @param {Function[]} functions
  * @return {Function}
  */
+var compose = function (functions) {
+    if (functions.length === 0) {
+        return function (x) { return x }
+    }
+    return function (x) {
+        let result = x
+        for (let i = functions.length - 1; i >= 0; i--) {
