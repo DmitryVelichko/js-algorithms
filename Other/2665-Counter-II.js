9954e69399779487c0f1267f09e6a616a3f1e8b9
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-17 14:56:58 +0800

    Add algo

--- Other/2665-Counter-II.js
+++ Other/2665-Counter-II.js
@@ -20,3 +20,13 @@
 // counter.reset(); // 5
 // counter.decrement(); // 4
 // Example 2:
+
+// Input: init = 0, calls = ["increment","increment","decrement","reset","reset"]
+// Output: [1,2,1,0,0]
+// Explanation:
+// const counter = createCounter(0);
+// counter.increment(); // 1
+// counter.increment(); // 2
+// counter.decrement(); // 1
+// counter.reset(); // 0
+// counter.reset(); // 0
