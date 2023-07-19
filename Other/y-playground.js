efc48230ffe90ca41290c45efd2bf9bd441e0ff1
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-19 23:03:07 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -44,3 +44,12 @@ MinStack.prototype.top = function () {
 MinStack.prototype.getMin = function () {
     return this.minStack[this.minStack.length - 1]
 };
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
