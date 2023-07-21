40367762d22923be2e414e1839ccb918a6219fd1
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-21 09:58:01 +0800

    Add algo

--- Other/2625-Flatten-Deeply-Nested-Array.js
+++ Other/2625-Flatten-Deeply-Nested-Array.js
@@ -42,3 +42,19 @@
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 // Explanation
+// The maximum depth of any subarray is 1. Thus, all of them are flattened.
+ 
+
+// Constraints:
+
+// 0 <= count of numbers in arr <= 105
+// 0 <= count of subarrays in arr <= 105
+// maxDepth <= 1000
+// -1000 <= each number <= 1000
+// 0 <= n <= 1000
+
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
