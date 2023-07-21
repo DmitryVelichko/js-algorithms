aad8ebd40da2407d64bcaa658352f717b08ad384
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-21 09:57:43 +0800

    Add algo

--- Other/2625-Flatten-Deeply-Nested-Array.js
+++ Other/2625-Flatten-Deeply-Nested-Array.js
@@ -8,3 +8,15 @@
 // A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
 
 // A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array. This flattening operation should only be done if the current depth of nesting is less than n. The depth of the elements in the first array are considered to be 0.
+
+// Please solve it without the built-in Array.flat method.
+
+ 
+
+// Example 1:
+
+// Input
+// arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+// n = 0
+// Output
+// [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
