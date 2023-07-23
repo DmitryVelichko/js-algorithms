c43e6593472491b0d50b2a51f3142af428cbaa63
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-23 11:34:30 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -39,3 +39,11 @@ var generate = function(numRows) {
         row[0] = 1
         row[row.length - 1] = 1
         
+        for(let j=1; j<row.length-1; j++) {
+            let rowAbove = pascalsTriangle[i-1]
+            row[j] = rowAbove[j] + rowAbove[j-1]
+        }
+        pascalsTriangle[i] = row
+    }
+    return pascalsTriangle
+};
