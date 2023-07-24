e790b7cbce7322ff97c86634a13d864c32e3d0ec
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-24 23:08:30 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -1,49 +1,9 @@
-// https://www.youtube.com/watch?v=y24nMGNkkPo&ab_channel=shirinsetayesh
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
-// Given an integer numRows, return the first numRows of Pascal's triangle.
-
-// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
-
+ 
 
 // Example 1:
 
-// Input: numRows = 5
-// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
 // Example 2:
-
-// Input: numRows = 1
-// Output: [[1]]
- 
-
-// Constraints:
-
-// 1 <= numRows <= 30
-
-/**
-2
- * @param {number} numRows
-3
- * @return {number[][]}
-4
- */
-
-/**
- * @param {number} numRows
- * @return {number[][]}
- */
-var generate = function(numRows) {
-    let pascalsTriangle = new Array(numRows);
-    
-    for(let i=0; i<numRows; i++) {
-        let row = new Array(i+1)
-        row[0] = 1
-        row[row.length - 1] = 1
-        
-        for(let j=1; j<row.length-1; j++) {
-            let rowAbove = pascalsTriangle[i-1]
-            row[j] = rowAbove[j] + rowAbove[j-1]
-        }
-        pascalsTriangle[i] = row
-    }
-    return pascalsTriangle
-};
