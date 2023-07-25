97a567c902332fbeae971a18d41adeaeb14661fd
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-25 09:57:30 +0800

    Add algo

--- Other/2635-apply-transform-over-each.js
+++ Other/2635-apply-transform-over-each.js
@@ -37,3 +37,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
+var map = function(arr, fn) {
+    let res = []
+     for(let i = 0; i<arr.length; i++){
+      res.push(fn(arr[i], i));
+     
+     }
+     return res;
+ };
