6e1b29e55c262e5347e0232efc2e6e2315739bf7
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-27 21:36:40 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -3,3 +3,10 @@
  * @return {number[]}
  */
 
+ // Задача на Arrays and Hashing
+ // 
+ // T: O(n), S:O(1) result array не считается по условиям задачи
+ var productExceptSelf = function(nums) {
+    const res = [];
+    let start = 1;
+    let start2 = 1
