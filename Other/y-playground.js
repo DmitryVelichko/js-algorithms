f0e86a2393ceb2ebda53fdeb27815d0577d1b57e
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-28 11:10:51 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -33,3 +33,18 @@
 // Output: [-2,0,1,2]
 // Explanation:
 // Falsey values such as 0 should be filtered out
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            res.push(arr[i])
+        }
+    }
+    return res
+};
