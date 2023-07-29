21db6cbfaeba8ebe52916dc3bcd6cd401bfbad4f
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-29 11:20:42 +0800

    Add algo

--- Other/2637-Promise-Time-Limit.js
+++ Other/2637-Promise-Time-Limit.js
@@ -6,3 +6,26 @@
 
 // If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
 // If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
+ 
+
+// Example 1:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 50
+// Output: {"rejected":"Time Limit Exceeded","time":50}
+// Explanation:
+// const limited = timeLimit(fn, t)
+// const start = performance.now()
+// let result;
+// try {
+//    const res = await limited(...inputs)
+//    result = {"resolved": res, "time": Math.floor(performance.now() - start)};
+// } catch (err) {
+//    result = {"rejected": err, "time": Math.floor(performance.now() - start)};
+// }
+// console.log(result) // Output
