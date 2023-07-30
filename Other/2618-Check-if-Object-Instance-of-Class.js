05e8de1908b545a04ad3e06dfd023a15e3feee35
Dmitry Velichko <soleil-2005@mail.ru>
2023-07-30 21:43:49 +0800

    Add algo

--- Other/2618-Check-if-Object-Instance-of-Class.js
+++ Other/2618-Check-if-Object-Instance-of-Class.js
@@ -38,3 +38,15 @@
  * @param {any} classFunction
  * @return {boolean}
  */
+var checkIfInstanceOf = function(obj, classFunction) {
+    const prototype = classFunction?.prototype;
+    while (obj !== null && obj !== undefined) {
+        obj = Object.getPrototypeOf(obj);
+        if (obj === prototype) return true;
+    }
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
