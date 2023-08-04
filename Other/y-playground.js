3b84b955aa274da25fe3140b536884feff1e9728
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-04 21:42:26 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -1,23 +1,11 @@
-/**
- * @param {string} val
- * @return {Object}
- */
-var expect = function(val) {
-    let val1 = val
-      return {  toBe(value) {
-          if (value === val1)  return {"value": true}
-          else return {'error': 'Not Equal'}
-      
-      },
-  
-        notToBe(value) {
-          if (value !== val1)  return {"value": true}
-          else return {'error': 'Not Equal'}
-      }
-      }
-  };
-  
-  /**
-   * expect(5).toBe(5); // true
-   * expect(5).notToBe(5); // throws "Equal"
-   */
+// 5 Longest Palindromic Substring
+
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
