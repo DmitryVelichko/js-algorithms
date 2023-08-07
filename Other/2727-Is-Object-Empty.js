ac4454fbb2e88c5c9adf0bff73603014a59bf70b
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-07 16:33:11 +0800

    Add algo

--- Other/2727-Is-Object-Empty.js
+++ Other/2727-Is-Object-Empty.js
@@ -6,3 +6,16 @@
 // An empty object contains no key-value pairs.
 // An empty array contains no elements.
 // You may assume the object or array is the output of JSON.parse.
+
+ 
+// Example 1:
+
+// Input: obj = {"x": 5, "y": 42}
+// Output: false
+// Explanation: The object has 2 key-value pairs so it is not empty.
+// Example 2:
+
+// Input: obj = {}
+// Output: true
+// Explanation: The object doesn't have any key-value pairs so it is empty.
+// Example 3:
