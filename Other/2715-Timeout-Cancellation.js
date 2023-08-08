50f1d19e11bbcdadc57f356029c23dccbba5f188
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-08 10:37:15 +0800

    Add algo

--- Other/2715-Timeout-Cancellation.js
+++ Other/2715-Timeout-Cancellation.js
@@ -5,3 +5,20 @@
 
 // After a delay of t, fn should be called with args passed as parameters unless cancelFn was invoked before the delay of t milliseconds elapses, specifically at cancelT ms. In that case, fn should never be called.
 
+ 
+// Example 1:
+
+// Input: fn = (x) => x * 5, args = [2], t = 20, cancelT = 50
+// Output: [{"time": 20, "returned": 10}]
+// Explanation: 
+// const cancel = cancellable((x) => x * 5, [2], 20); // fn(2) called at t=20ms
+// setTimeout(cancel, 50);
+
+// The cancellation was scheduled to occur after a delay of cancelT (50ms), which happened after the execution of fn(2) at 20ms.
+// Example 2:
+
+// Input: fn = (x) => x**2, args = [2], t = 100, cancelT = 50 
+// Output: []
+// Explanation: 
+// const cancel = cancellable((x) => x**2, [2], 100); // fn(2) not called
+// setTimeout(cancel, 50);
