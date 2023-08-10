c2de6118d23b0e7f2776ca38b70c0c06db8ab60f
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-10 11:06:09 +0800

    Add algo

--- Other/2703-return-length-of-args-passed.js
+++ Other/2703-return-length-of-args-passed.js
@@ -13,3 +13,13 @@
 
 // One value was passed to the function so it should return 1.
 // Example 2:
+
+// Input: argsArr = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+ 
+
+// Constraints:
