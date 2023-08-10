135bcafa9ca3057c647233c8ee688acc722fedc6
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-10 11:06:02 +0800

    Add algo

--- Other/2703-return-length-of-args-passed.js
+++ Other/2703-return-length-of-args-passed.js
@@ -4,3 +4,12 @@
 // Write a function argumentsLength that returns the count of arguments passed to it.
  
 
+// Example 1:
+
+// Input: argsArr = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
