49bc329b879d2f0a0f2590a03f902a76c81b8903
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-11 22:33:45 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -37,3 +37,11 @@ var expect = function (val) {
             else throw new Error("Not Equal")
 
         },
+
+        notToBe(value) {
+            if (value !== val) return true
+            else throw new Error('Equal')
+        }
+    }
+};
+
