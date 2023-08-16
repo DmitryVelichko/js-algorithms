0db7b7782732e71b170cb50acfa805e1756e1deb
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-16 23:05:41 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -57,3 +57,58 @@ var mergeTwoLists = function(l1, l2) {
     let dummy = new ListNode(-Infinity)
     let prev = dummy
 
+    while(l1 && l2) {
+        if(l1.val <= l2.val) {
+            prev.next = l1
+            prev = l1
+            l1 = l1.next
+        } else {
+            prev.next = l2
+            prev = l2
+            l2 = l2.next
+        }
+    }
+    if(!l1) prev.next = l2
+    if(!l2) prev.next = l1
+    return dummy.next
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // T: O(n), S: O(1)
+var mergeTwoLists2 = function(l1, l2) {
+    const dummy = new ListNode(-Infinity)
+    let prev = dummy
+    while(l1&&l2) {
+        if(l1.val<=l2.val) {
+            prev.next = l1
+            prev = l1
+            l1 = l1.next
+        } else {
+            prev.next = l2
+            prev = l2
+            l2 = l2.next
+        }
+    }
+    if(!l1) prev.next = l2
+    if(!l2) prev.next = l1
+    return dummy.next
+};
