b1beab55d051289e5d9448a658caa55ecc49238a
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-21 23:54:31 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -7,3 +7,15 @@
 
 // After a delay of t, fn should be called with args passed as parameters unless cancelFn was invoked before the delay of t milliseconds elapses, specifically at cancelT ms. In that case, fn should never be called.
 
+ 
+
+// Example 1:
+
+// Input: fn = (x) => x * 5, args = [2], t = 20, cancelT = 50
+// Output: [{"time": 20, "returned": 10}]
+// Explanation: 
+// const cancel = cancellable((x) => x * 5, [2], 20); // fn(2) called at t=20ms
+// setTimeout(cancel, 50);
+
+// The cancellation was scheduled to occur after a delay of cancelT (50ms), which happened after the execution of fn(2) at 20ms.
+// Example 2:
