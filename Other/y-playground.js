ee273d5d68c9f4006a8d5167fcd6ed5ef823e68c
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-24 23:55:28 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -23,3 +23,13 @@
 
 // 0 <= arr.length <= 1000
 
+Array.prototype.last = function() {
+    if(this.length === 0) return -1
+    else return this[this.length-1]
+   
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
