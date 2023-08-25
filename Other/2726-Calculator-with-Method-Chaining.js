43b5e1ff9b5c5c3b18e0dfa167f229506e747620
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-25 09:26:53 +0800

    Add algo

--- Other/2726-Calculator-with-Method-Chaining.js
+++ Other/2726-Calculator-with-Method-Chaining.js
@@ -25,3 +25,21 @@
 // new Calculator(10).add(5).subtract(7).getResult() // 10 + 5 - 7 = 8
 // Example 2:
 
+// Input: 
+// actions = ["Calculator", "multiply", "power", "getResult"], 
+// values = [2, 5, 2]
+// Output: 100
+// Explanation: 
+// new Calculator(2).multiply(5).power(2).getResult() // (2 * 5) ^ 2 = 100
+// Example 3:
+
+// Input: 
+// actions = ["Calculator", "divide", "getResult"], 
+// values = [20, 0]
+// Output: "Division by zero is not allowed"
+// Explanation: 
+// new Calculator(20).divide(0).getResult() // 20 / 0 
+
+// The error should be thrown because we cannot divide by zero.
+ 
+
