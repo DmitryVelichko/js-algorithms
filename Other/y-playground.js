de23b952f57811dd3311ce036ea806c40cab560a
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-29 14:00:40 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -1,35 +1,6 @@
-// 2619. Array Prototype Last
-// Easy
+// 2627. Debounce
+// Medium
 
-// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+// Given a function fn and a time in milliseconds t, return a debounced version of that function.
 
-// You may assume the array is the output of JSON.parse.
-
- 
-
-// Example 1:
-
-// Input: nums = [null, {}, 3]
-// Output: 3
-// Explanation: Calling nums.last() should return the last element: 3.
-// Example 2:
-
-// Input: nums = []
-// Output: -1
-// Explanation: Because there are no elements, return -1.
- 
-
-// Constraints:
-
-// 0 <= arr.length <= 1000
-
-Array.prototype.last = function() {
-    if(this.length === 0) return -1
-    else return this[this.length-1]
-   
-};
-
-/**
- * const arr = [1, 2, 3];
- * arr.last(); // 3
- */
+// A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. The debounced function should also receive the passed parameters.
