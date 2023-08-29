78b1b70c2e1eaf02e6f660375d1007ccf40e5843
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-29 14:01:14 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -83,3 +83,9 @@ var debounce = function(fn, t) {
     }
 };
 
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
