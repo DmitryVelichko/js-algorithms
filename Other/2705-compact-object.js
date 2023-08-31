1bbcd8682aa03de847ab0f26d87a26a86c2a4bf7
Dmitry Velichko <soleil-2005@mail.ru>
2023-08-31 09:48:46 +0800

    Add algo

--- Other/2705-compact-object.js
+++ Other/2705-compact-object.js
@@ -33,3 +33,8 @@
  * @param {Object|Array} obj
  * @return {Object|Array}
  */
+var compactObject = function(obj) {
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+
