64fdc1694da384ee71caa9106215707d63cd498e
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-06 23:59:47 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -28,3 +28,24 @@
 // (6) + nums[3] = 10
 // The final answer is 10.
 // Example 2:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr * curr; }
+// init = 100
+// Output: 130
+// Explanation:
+// initially, the value is init=100.
+// (100) + nums[0]^2 = 101
+// (101) + nums[1]^2 = 105
+// (105) + nums[2]^2 = 114
+// (114) + nums[3]^2 = 130
+// The final answer is 130.
+// Example 3:
+
+// Input: 
+// nums = []
+// fn = function sum(accum, curr) { return 0; }
+// init = 25
+// Output: 25
+// Explanation: For empty arrays, the answer is always init.
