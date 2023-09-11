953afe645816eaab083e2cabd337a562bed44b6e
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-11 23:39:53 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -54,3 +54,18 @@ var createCounter = function (init) {
     function decrement() {
         return --counter
     }
+
+    function reset() {
+
+        return counter = init
+    }
+
+    return { increment, decrement, reset }
+
+}
+/**
+* const counter = createCounter(5)
+* counter.increment(); // 6
+* counter.reset(); // 5
+* counter.decrement(); // 4
+*/
