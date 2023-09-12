b11485a642f59164a90d4882c8d2ce30fdb956b0
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-12 12:38:19 +0800

    Add algo

--- Other/2630-Memoize-II.js
+++ Other/2630-Memoize-II.js
@@ -8,3 +8,18 @@
 
 // fn can be any function and there are no constraints on what type of values it accepts. Inputs are considered identical if they are === to each other.
 
+
+
+// Example 1:
+
+// Input: 
+// getInputs = () => [[2,2],[2,2],[1,2]]
+// fn = function (a, b) { return a + b; }
+// Output: [{"val":4,"calls":1},{"val":4,"calls":1},{"val":3,"calls":2}]
+// Explanation:
+// const inputs = getInputs();
+// const memoized = memoize(fn);
+// for (const arr of inputs) {
+//   memoized(...arr);
+// }
+
