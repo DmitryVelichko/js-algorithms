4e9ccdf5c09326c4e1a3d373d166528dc6fe7083
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-14 11:04:27 +0800

    Add algo

--- Other/2631-group-by.js
+++ Other/2631-group-by.js
@@ -10,3 +10,22 @@
 // The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
 
 // Please solve it without lodash's _.groupBy function.
+
+ 
+
+// Example 1:
+
+// Input: 
+// array = [
+//   {"id":"1"},
+//   {"id":"1"},
+//   {"id":"2"}
+// ], 
+// fn = function (item) { 
+//   return item.id; 
+// }
+// Output: 
+// { 
+//   "1": [{"id": "1"}, {"id": "1"}],   
+//   "2": [{"id": "2"}] 
+// }
