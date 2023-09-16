a73b078261e76eacaf571620a5a4d2f1f8fe2d89
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-16 12:15:56 +0800

    Add algo

---
+++ Other/2722-join-two-arrays-by-id.js
@@ -0,0 +1,5 @@
+// 2722. Join Two Arrays by ID
+// Medium
+
+// Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. joinedArray is an array formed by merging arr1 and arr2 based on their id key. The length of joinedArray should be the length of unique values of id. The returned array should be sorted in ascending order based on the id key.
+
