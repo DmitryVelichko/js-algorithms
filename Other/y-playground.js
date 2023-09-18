2252b93f089e41ac73e8343fe73f1e411730daf5
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-18 23:09:13 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -37,3 +37,20 @@
 
 // arr is a valid JSON array
 // 2 <= JSON.stringify(arr).length <= 105
+// 1 <= size <= arr.length + 1
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    let res = []
+
+    while(arr.length) {
+        res.push(arr.splice(0, size))
+    }
+
+    return res
+};
+
