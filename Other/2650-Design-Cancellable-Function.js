870e47ee8b2fb7e2bac6363e2195f2206852f4b1
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-20 21:07:25 +0800

    Add algo

--- Other/2650-Design-Cancellable-Function.js
+++ Other/2650-Design-Cancellable-Function.js
@@ -127,3 +127,17 @@ var cancellable = function(generator) {
     // Every Promise rejection has to be caught.
     cancelPromise.catch(()=>{});
 
+    const promise = (async () => {
+        let next = generator.next();
+        while (!next.done) {
+            try {
+                next = generator.next(await Promise.race([next.value, cancelPromise]));
+            } catch (e) {
+                next = generator.throw(e);
+            }
+        }
+        return next.value;
+    })();
+
+    return [cancel, promise];
+};
