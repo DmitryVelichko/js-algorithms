1959946d52fc18616d0b70ff411abd3792fd5e6e
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-20 21:07:29 +0800

    Add algo

--- Other/2650-Design-Cancellable-Function.js
+++ Other/2650-Design-Cancellable-Function.js
@@ -141,3 +141,14 @@ var cancellable = function(generator) {
 
     return [cancel, promise];
 };
+
+/**
+ * function* tasks() {
+ *   const val = yield new Promise(resolve => resolve(2 + 2));
+ *   yield new Promise(resolve => setTimeout(resolve, 100));
+ *   return val + 1;
+ * }
+ * const [cancel, promise] = cancellable(tasks());
+ * setTimeout(cancel, 50);
+ * promise.catch(console.log); // logs "Cancelled" at t=50ms
+ */
