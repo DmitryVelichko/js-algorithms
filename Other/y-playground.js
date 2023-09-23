be0f04b55050660a7e304105dd77f854652a796a
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-23 12:19:37 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -16,3 +16,25 @@
 // Please solve it without using lodash's _.debounce() function.
 
  
+
+// Example 1:
+
+// Input: 
+// t = 50
+// calls = [
+//   {"t": 50, inputs: [1]},
+//   {"t": 75, inputs: [2]}
+// ]
+// Output: [{"t": 125, inputs: [2]}]
+// Explanation:
+// let start = Date.now();
+// function log(...inputs) { 
+//   console.log([Date.now() - start, inputs ])
+// }
+// const dlog = debounce(log, 50);
+// setTimeout(() => dlog(1), 50);
+// setTimeout(() => dlog(2), 75);
+
+// The 1st call is cancelled by the 2nd call because the 2nd call occurred before 100ms
+// The 2nd call is delayed by 50ms and executed at 125ms. The inputs were (2).
+// Example 2:
