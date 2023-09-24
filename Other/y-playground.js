300de36c211d3c1fd618a5573de6f340a26de051
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-24 19:27:06 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -15,3 +15,19 @@
 
  
 
+// Example 1:
+
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+// Output: [20,30]
+// Explanation:
+// const newArray = filter(arr, fn); // [20, 30]
+// The function filters out values that are not greater than 10
+// Example 2:
+
+// Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
+// Output: [1]
+// Explanation:
+// fn can also accept the index of each element
+// In this case, the function removes elements not at index 0
+// Example 3:
+
