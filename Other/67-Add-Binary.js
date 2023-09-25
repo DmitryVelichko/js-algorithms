a4eb4bb43d44a21c32e393e84b26e5701d11d4f4
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-25 09:41:23 +0800

    Add algo

--- Other/67-Add-Binary.js
+++ Other/67-Add-Binary.js
@@ -23,3 +23,8 @@
 // a and b consist only of '0' or '1' characters.
 // Each string does not contain leading zeros except for the zero itself.
 
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
