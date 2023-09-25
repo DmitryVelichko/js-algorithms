8e5d07f7d4c7601919aaff37dc6629ad5cc2d16a
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-25 09:41:28 +0800

    Add algo

--- Other/67-Add-Binary.js
+++ Other/67-Add-Binary.js
@@ -28,3 +28,9 @@
  * @param {string} b
  * @return {string}
  */
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+  };
