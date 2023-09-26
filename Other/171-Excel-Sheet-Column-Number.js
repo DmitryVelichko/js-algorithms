2d564a0e3818d9b3962dc0c38ba6fd593c36360b
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-26 13:45:18 +0800

    Add algo

--- Other/171-Excel-Sheet-Column-Number.js
+++ Other/171-Excel-Sheet-Column-Number.js
@@ -5,3 +5,22 @@
 
 // For example:
 
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28 
+// ...
+ 
+
+// Example 1:
+
+// Input: columnTitle = "A"
+// Output: 1
+// Example 2:
+
+// Input: columnTitle = "AB"
+// Output: 28
+// Example 3:
