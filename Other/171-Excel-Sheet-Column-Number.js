02cdc9ddfb798821f02a2988335c5bbe83b965bc
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-26 13:45:38 +0800

    Add algo

--- Other/171-Excel-Sheet-Column-Number.js
+++ Other/171-Excel-Sheet-Column-Number.js
@@ -40,3 +40,10 @@
  * @param {string} columnTitle
  * @return {number}
  */
+var titleToNumber = function(s) {
+    let total = 0
+    for (let i = 0; i < s.length; i++) {
+      total = total * 26 + s.charCodeAt(i) - "A".charCodeAt(0) + 1
+    }
+    return total
+  };
