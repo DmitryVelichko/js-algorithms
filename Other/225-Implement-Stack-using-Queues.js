f7283c694218943e4e0f8db0b60bf4ae089bae21
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-27 10:10:40 +0800

    Add algo

--- Other/225-Implement-Stack-using-Queues.js
+++ Other/225-Implement-Stack-using-Queues.js
@@ -42,3 +42,23 @@
 // Follow-up: Can you implement the stack using only one queue?
 
 
+var MyStack = function() {
+    this.q1 = []
+    this.q2 = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    while (this.q1.length !== 0) {
+        this.q2.push(this.q1.shift());
+    }
+    
+    this.q1.push(x);
+    while (this.q2.length !== 0) {
+        this.q1.push(this.q2.shift());
+    }
+};
+
