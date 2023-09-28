030b6dd6ae365aeb8b0c12e9c05b8a6417e1cd0d
Dmitry Velichko <soleil-2005@mail.ru>
2023-09-28 23:34:09 +0800

    Add algo

--- Other/111-Minimum-Depth-of-Binary-Tree.js
+++ Other/111-Minimum-Depth-of-Binary-Tree.js
@@ -19,3 +19,17 @@
 // Input: root = [2,null,3,null,4,null,5,null,6]
 // Output: 5
  
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 105].
+// -1000 <= Node.val <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
