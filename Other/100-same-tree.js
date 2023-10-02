f37f944ba55ae929eac67eedff80cf4694a701aa
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-02 10:11:18 +0800

    Add algo

--- Other/100-same-tree.js
+++ Other/100-same-tree.js
@@ -42,3 +42,9 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+var isSameTree = function(p, q) {
+    if(!p && !q) return true
+    if(!p || !q || p.val !== q.val) return false
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
