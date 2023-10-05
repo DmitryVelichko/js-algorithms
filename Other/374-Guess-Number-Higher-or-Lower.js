e63a79f42d0bb056727ec90f353f432c5773c512
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-05 08:22:22 +0800

    Add algo

--- Other/374-Guess-Number-Higher-or-Lower.js
+++ Other/374-Guess-Number-Higher-or-Lower.js
@@ -55,3 +55,9 @@ var guessNumber = function(n) {
     while(l  <= r ){
         let mid = Math.floor((l+r)/2);
         let res = guess(mid);
+        if(res == 0) return mid;
+        else if (res == 1) l = mid+1;
+        else r = mid-1;
+    }
+    return l;    
+};
