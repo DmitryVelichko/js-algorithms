cd14133d548b373016fbe864156debc8db5ea114
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-08 07:45:52 +0800

    Add algo

--- Other/338-counting-bits.js
+++ Other/338-counting-bits.js
@@ -39,3 +39,10 @@
  * @param {number} n
  * @return {number[]}
  */
+function countBits(num) {
+    let bits = [];
+    for (let i = 0; i <= num; i++)
+        // remove 0 from bits
+        bits.push(Number(i).toString(2).replace(/0/g, '').length);
+    return bits;
+}
