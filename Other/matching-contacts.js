7b9fef4ab01abed425b7a7a373184572df75eab7
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-07 23:50:20 +0800

    Add algo

--- Other/matching-contacts.js
+++ Other/matching-contacts.js
@@ -4,3 +4,14 @@ const book = [
     {name: 'bbb', phones: [444,555,222]},
     {name: 'ccc', phones: [333]},
     {name: 'ddd', phones: [777]}
+]
+
+// aaa, bbb: 222
+// aaa, ccc: 333
+
+
+function findContactsByPhone(book) {
+    const phoneMap = {}; // Объект для хранения номеров телефонов и связанных с ними имен
+
+    // Проходимся по книге контактов и заполняем phoneMap
+    book.forEach(contact => {
