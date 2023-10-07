e8339ade9971a7e1b98d20713c252f078e9c2f35
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-08 07:45:47 +0800

    Add algo

--- Other/338-counting-bits.js
+++ Other/338-counting-bits.js
@@ -25,3 +25,17 @@
 // 5 --> 101
  
 
+// Constraints:
+
+// 0 <= n <= 105
+ 
+
+// Follow up:
+
+// It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+// Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
