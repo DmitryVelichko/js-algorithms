3051976ed0098902ec8888dc74510476ec63ae00
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-09 23:27:13 +0800

    Add algo

--- Other/y-playground.js
+++ Other/y-playground.js
@@ -32,6 +32,23 @@
 // Time: O(n), Space: O(1), Two Pointers
 
 var reverseString = function(s) {
+    let l = 0;
+    let r = s.length - 1
+
+    while(l<r) {
+        let hold = s[l]
+        s[l] = s[r]
+        s[r] = hold
+        l++
+        r--
+    }
+
+};
+
+
+
+
+
 
 
 
