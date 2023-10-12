f48b57601cd84173bf2825408a7574ba73b7aba6
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-12 23:22:54 +0800

    Add algo

--- Other/414-Third-Maximum-Number.js
+++ Other/414-Third-Maximum-Number.js
@@ -43,3 +43,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+var thirdMax = function(nums) {
+    if(nums.length<3) return Math.max(...nums);
+    let u = new Set(nums)
+    if(u.size<3) return Math.max(...u);
+    
