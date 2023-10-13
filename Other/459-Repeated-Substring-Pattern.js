322551f24cacccaa6998c9365f57c4397c1f75fa
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-13 14:29:23 +0800

    Add algo

--- Other/459-Repeated-Substring-Pattern.js
+++ Other/459-Repeated-Substring-Pattern.js
@@ -9,3 +9,13 @@
 // Output: true
 // Explanation: It is the substring "ab" twice.
 // Example 2:
+
+// Input: s = "aba"
+// Output: false
+// Example 3:
+
+// Input: s = "abcabcabcabc"
+// Output: true
+// Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ 
+
