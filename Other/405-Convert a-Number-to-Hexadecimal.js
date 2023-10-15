01f218e42ef437e5fac8b1cdf4eeee46da4cf3e4
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-15 09:01:46 +0800

    Add algo

---
+++ Other/405-Convert a-Number-to-Hexadecimal.js
@@ -0,0 +1,7 @@
+// 405. Convert a Number to Hexadecimal
+// Easy
+// 1.3K
+// 204
+// Companies
+// Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
+
