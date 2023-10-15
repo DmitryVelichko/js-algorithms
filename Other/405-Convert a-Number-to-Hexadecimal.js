35e4d74f93ddb5e164a02430d3f29a6ad2d8980d
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-15 09:02:05 +0800

    Add algo

--- Other/405-Convert a-Number-to-Hexadecimal.js
+++ Other/405-Convert a-Number-to-Hexadecimal.js
@@ -29,3 +29,13 @@
  * @param {number} num
  * @return {string}
  */
+var toHex = function(num) {
+    var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    
+    if (num == 0) return "0";
+    
+    if (num < 0) {
+        num += Math.pow(2,32);
+    }
+    var res = "";
+    
