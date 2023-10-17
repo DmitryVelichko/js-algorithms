c1cfd367de919732ea65146fee16bca69a8a2869
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-17 08:54:29 +0800

    Add algo

--- Other/415-add-strings.js
+++ Other/415-add-strings.js
@@ -40,3 +40,8 @@ var addStrings = function(num1, num2) {
     let sum = "";
     let carry = 0;
     
+    while( i >= 0 || j >= 0 || carry > 0){
+        const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
+        const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
+        const digitSum = digit1 + digit2 + carry;
+        
