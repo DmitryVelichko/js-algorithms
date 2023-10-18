b7a54d352eae37c7a78e06b599c0dda4d09de6f9
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-18 09:41:53 +0800

    Add algo

--- Other/434-Number-of-Segments-in-a-String.js
+++ Other/434-Number-of-Segments-in-a-String.js
@@ -1,3 +1,14 @@
 // 434. Number of Segments in a String
 // Easy
 
+// Given a string s, return the number of segments in the string.
+
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+// Example 1:
+
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+// Example 2:
+
