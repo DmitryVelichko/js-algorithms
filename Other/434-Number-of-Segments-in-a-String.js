4a1ae1b64b9e5d7ff3836c6697e789a524caa42d
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-18 09:41:59 +0800

    Add algo

--- Other/434-Number-of-Segments-in-a-String.js
+++ Other/434-Number-of-Segments-in-a-String.js
@@ -12,3 +12,12 @@
 // Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
 // Example 2:
 
+// Input: s = "Hello"
+// Output: 1
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 300
+// s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+// The only space character in s is ' '.
