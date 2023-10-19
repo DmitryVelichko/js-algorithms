c3fc65d40fc6dce3aaf3e0c4326d4dee0a128f9f
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-19 09:02:32 +0800

    Add algo

--- Other/404-Sum-of-Left-Leaves.js
+++ Other/404-Sum-of-Left-Leaves.js
@@ -24,3 +24,10 @@
 // -1000 <= Node.val <= 1000
 
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
