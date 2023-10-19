a278fe5d67cee7089ac25db85ce500176968affd
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-19 09:02:43 +0800

    Add algo

--- Other/404-Sum-of-Left-Leaves.js
+++ Other/404-Sum-of-Left-Leaves.js
@@ -35,3 +35,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
+var sumOfLeftLeaves = function(root, left = false) {
+    if(root === null) return 0
+    if(root.left === null && root.right === null) return left ? root.val : 0
+    return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false)
+};
