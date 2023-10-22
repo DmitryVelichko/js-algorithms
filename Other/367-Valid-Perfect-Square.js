0469d5e7829d2ad289c99396e62e7a57ea32f2e1
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-22 09:44:43 +0800

    Add algo

--- Other/367-Valid-Perfect-Square.js
+++ Other/367-Valid-Perfect-Square.js
@@ -39,3 +39,9 @@ var isPerfectSquare = function(num) {
     }
     let left = 1;
     let right = num;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(mid*mid === num){
+            return true;
+        }else if(mid*mid > num){
+            right = mid - 1;
