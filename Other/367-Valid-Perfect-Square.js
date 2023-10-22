793f5e8196527cde2fd92f78bda72e0c0112f17a
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-22 09:44:29 +0800

    Add algo

--- Other/367-Valid-Perfect-Square.js
+++ Other/367-Valid-Perfect-Square.js
@@ -19,3 +19,14 @@
 // Input: num = 14
 // Output: false
 // Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+ 
+
+// Constraints:
+
+// 1 <= num <= 231 - 1
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// Binary search
