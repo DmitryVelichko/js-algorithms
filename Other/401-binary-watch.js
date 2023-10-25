daf6882ee0ed237e285c008e9ad28d6cc7d9540e
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-25 11:19:12 +0800

    Add algo

--- Other/401-binary-watch.js
+++ Other/401-binary-watch.js
@@ -25,3 +25,14 @@
 // Input: turnedOn = 9
 // Output: []
  
+
+// Constraints:
+
+// 0 <= turnedOn <= 10
+
+var readBinaryWatch = function(num) {
+    const times = [];
+    for (let h = 0; h < 12; h++) {
+      for (let m = 0; m < 60; m++) {
+        const hOnes = h ? h.toString(2).match(/1/g).length : 0;
+        const mOnes = m ? m.toString(2).match(/1/g).length : 0;
