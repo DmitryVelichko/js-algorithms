f6935ccb14f74bd61b7d613bb8c18de0654dde0d
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-26 11:44:04 +0800

    Add algo

--- Other/463-island-perimeter.js
+++ Other/463-island-perimeter.js
@@ -52,3 +52,10 @@ var islandPerimeter = function(grid) {
             // abstract the number of adjacent island
             if (row > 0 && grid[row - 1][col]) perimeter--;
             if (col > 0 && grid[row][col - 1]) perimeter--;
+            if (row < rows - 1 && grid[row + 1][col]) perimeter--;
+            if (col < cols - 1 && grid[row][col + 1]) perimeter--;
+        }
+    }
+    
+    return perimeter;
+};
