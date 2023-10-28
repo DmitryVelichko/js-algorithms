4cff3c39132a556e63e32d0d08ea7ed311c3d54e
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-28 11:18:49 +0800

    Add algo

--- Other/507-perfect-number.js
+++ Other/507-perfect-number.js
@@ -36,3 +36,6 @@ var checkPerfectNumber = function(num) {
 
     let sum = 1; // 1 всегда является делителем
 
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            sum += i;
