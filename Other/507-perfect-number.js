67d9b146dc3df6cd0227d53ab6b6b0e90f286dc5
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-28 11:18:53 +0800

    Add algo

--- Other/507-perfect-number.js
+++ Other/507-perfect-number.js
@@ -39,3 +39,11 @@ var checkPerfectNumber = function(num) {
     for (let i = 2; i * i <= num; i++) {
         if (num % i === 0) {
             sum += i;
+            if (i * i !== num) {
+                sum += num / i;
+            }
+        }
+    }
+
+    return sum === num;
+};
