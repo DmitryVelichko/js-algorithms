10cda945a0e9e8780e749473fdb423892be1f89c
Dmitry Velichko <soleil-2005@mail.ru>
2023-10-31 09:39:57 +0800

    Add algo

--- Other/495-teemo-attacking.js
+++ Other/495-teemo-attacking.js
@@ -11,3 +11,11 @@
 
 // Example 1:
 
+// Input: timeSeries = [1,4], duration = 2
+// Output: 4
+// Explanation: Teemo's attacks on Ashe go as follows:
+// - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+// - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+// Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+// Example 2:
+
