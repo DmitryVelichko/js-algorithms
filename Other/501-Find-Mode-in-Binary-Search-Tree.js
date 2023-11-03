9332f2f6de1626e103523af3d7fa515572084bd3
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-03 11:58:56 +0800

    Add algo

---
+++ Other/501-Find-Mode-in-Binary-Search-Tree.js
@@ -0,0 +1,9 @@
+// 501. Find Mode in Binary Search Tree
+// Easy
+
+// Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
+
+// If the tree has more than one mode, return them in any order.
+
+// Assume a BST is defined as follows:
+
