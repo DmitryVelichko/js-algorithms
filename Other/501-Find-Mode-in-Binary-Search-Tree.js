1fc500dafb1e889ab34877254e5b34fcee2d1212
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-03 11:59:03 +0800

    Add algo

--- Other/501-Find-Mode-in-Binary-Search-Tree.js
+++ Other/501-Find-Mode-in-Binary-Search-Tree.js
@@ -7,3 +7,18 @@
 
 // Assume a BST is defined as follows:
 
+// The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+// The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+// Both the left and right subtrees must also be binary search trees.
+
+
+// Example 1:
+
+
+// Input: root = [1,null,2,2]
+// Output: [2]
+// Example 2:
+
+// Input: root = [0]
+// Output: [0]
+
