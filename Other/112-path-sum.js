e04c2707c20615dc9052b773c57aba1fbdd16390
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-05 11:36:17 +0800

    Add algo

--- Other/112-path-sum.js
+++ Other/112-path-sum.js
@@ -43,3 +43,11 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+
