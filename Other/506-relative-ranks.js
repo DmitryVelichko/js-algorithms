170bd951e72de8e0bf927ba5b8ebb9c18d556ecb
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-06 11:13:23 +0800

    Add algo

---
+++ Other/506-relative-ranks.js
@@ -0,0 +1,4 @@
+// 506. Relative Ranks
+// Easy
+
+// You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
