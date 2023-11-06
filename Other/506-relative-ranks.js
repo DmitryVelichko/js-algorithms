e9521ab0de2ccc211eab64258d51648b6b79a698
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-06 11:13:57 +0800

    Add algo

--- Other/506-relative-ranks.js
+++ Other/506-relative-ranks.js
@@ -45,3 +45,7 @@ var findRelativeRanks = function(nums) {
     return nums.map((num) => {
        if (num === ranks[0]) return 'Gold Medal';
        else if (num === ranks[1]) return 'Silver Medal';
+       else if (num === ranks[2]) return 'Bronze Medal';
+       else return (ranks.indexOf(num) + 1).toString();
+    });
+}
