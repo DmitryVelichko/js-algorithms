d90ee1f78b11b9fd9ff2134ee41d3ae4c3807baf
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-07 12:11:47 +0800

    Add algo

--- Other/520-detect-capital.js
+++ Other/520-detect-capital.js
@@ -9,3 +9,14 @@
 // Given a string word, return true if the usage of capitals in it is right.
 
 // Example 1:
+
+// Input: word = "USA"
+// Output: true
+// Example 2:
+
+// Input: word = "FlaG"
+// Output: false
+ 
+
+// Constraints:
+
