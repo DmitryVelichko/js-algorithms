8bdfddf19e2c3d00af12cb4140e2ec099d543377
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-08 10:46:32 +0800

    Add algo

--- Other/521. Longest Uncommon Subsequence I.js
+++ Other/521. Longest Uncommon Subsequence I.js
@@ -19,3 +19,14 @@
 // Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
 // Note that "cdc" is also a longest uncommon subsequence.
 // Example 2:
+
+// Input: a = "aaa", b = "bbb"
+// Output: 3
+// Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+// Example 3:
+
+// Input: a = "aaa", b = "aaa"
+// Output: -1
+// Explanation: Every subsequence of string a is also a subsequence of string b. Similarly, every subsequence of string b is also a subsequence of string a.
+ 
+
