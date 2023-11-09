a7701dbe571af825a191892522d6778eeff6bd7f
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-09 11:08:50 +0800

    Add algo

---
+++ Other/783. Minimum Distance Between BST Nodes.js
@@ -0,0 +1,6 @@
+// 783. Minimum Distance Between BST Nodes
+// Easy
+// 3.4K
+// 408
+// Companies
+// Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
