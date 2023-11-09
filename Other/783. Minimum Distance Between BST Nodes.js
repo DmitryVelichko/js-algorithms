ea91f2733320ee9a23d3e9b6daceb9f386c655b3
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-09 11:09:08 +0800

    Add algo

--- Other/783. Minimum Distance Between BST Nodes.js
+++ Other/783. Minimum Distance Between BST Nodes.js
@@ -27,3 +27,15 @@
 
 // Note: This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
