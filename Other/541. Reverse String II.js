a31e4242834856fea2d24f7d771ca879662be14c
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-10 10:10:42 +0800

    Add algo

--- Other/541. Reverse String II.js
+++ Other/541. Reverse String II.js
@@ -34,3 +34,10 @@ var reverseStr = function(s, k) {
     const split = s.split('');
     
     // reverse the segment and put it back
+    for (let i = 0; i < s.length; i += 2*k) {
+        const reverse = split.splice(i, k).reverse();
+        split.splice(i, 0, ...reverse);
+    }
+    
+    return split.join('');
+};
