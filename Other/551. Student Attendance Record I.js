83859730aea76179b2d6eb18f7ce40e8724c767d
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-11 11:27:08 +0800

    Add Algo

--- Other/551. Student Attendance Record I.js
+++ Other/551. Student Attendance Record I.js
@@ -10,3 +10,16 @@
 // 'P': Present.
 // The student is eligible for an attendance award if they meet both of the following criteria:
 
+// The student was absent ('A') for strictly fewer than 2 days total.
+// The student was never late ('L') for 3 or more consecutive days.
+// Return true if the student is eligible for an attendance award, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "PPALLP"
+// Output: true
+// Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+// Example 2:
+
