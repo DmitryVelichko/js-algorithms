49fc24b4c0405c44b97950fe83d7f3df6938b003
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-11 11:27:00 +0800

    Add Algo

--- Other/551. Student Attendance Record I.js
+++ Other/551. Student Attendance Record I.js
@@ -3,3 +3,10 @@
 // 671
 // 41
 // Companies
+// You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+
+// 'A': Absent.
+// 'L': Late.
+// 'P': Present.
+// The student is eligible for an attendance award if they meet both of the following criteria:
+
