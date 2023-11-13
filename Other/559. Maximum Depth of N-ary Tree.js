26558cc38558ab51bfea28150261f949c599d38c
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-13 12:08:24 +0800

    Add algo

--- Other/559. Maximum Depth of N-ary Tree.js
+++ Other/559. Maximum Depth of N-ary Tree.js
@@ -15,3 +15,19 @@
 
 
 
+// Input: root = [1,null,3,2,4,null,5,6]
+// Output: 3
+// Example 2:
+
+
+
+// Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// Output: 5
+ 
+
+// Constraints:
+
+// The total number of nodes is in the range [0, 104].
+// The depth of the n-ary tree is less than or equal to 1000.
+
+
