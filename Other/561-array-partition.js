fc0216f815b9c23700bbf94b7ed5c61f8e2a45fb
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-14 09:26:31 +0800

    Add algo

--- Other/561-array-partition.js
+++ Other/561-array-partition.js
@@ -29,3 +29,13 @@
 // nums.length == 2 * n
 // -104 <= nums[i] <= 104
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    // sort and sum up the minimum of the pairs
+    return nums
+        .sort((a, b) => a - b)
+        .reduce((sum, cur, i) => (i % 2 === 0) ? sum += cur : sum, 0);
+};
