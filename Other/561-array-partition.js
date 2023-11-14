31d263bec66012b5feb034827775c4588d102a0d
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-14 09:26:09 +0800

    Add algo

---
+++ Other/561-array-partition.js
@@ -0,0 +1,6 @@
+// 561. Array Partition
+// Easy
+// 1.9K
+// 236
+// Companies
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
