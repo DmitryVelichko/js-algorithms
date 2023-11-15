df2ddd240e5510462f3e42af487e426a3d88e1b7
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-15 10:43:00 +0800

    Add algo

--- Other/563. Binary Tree Tilt.js
+++ Other/563. Binary Tree Tilt.js
@@ -22,3 +22,16 @@
 // Example 2:
 
 
+// Input: root = [4,2,9,3,5,null,7]
+// Output: 15
+// Explanation: 
+// Tilt of node 3 : |0-0| = 0 (no children)
+// Tilt of node 5 : |0-0| = 0 (no children)
+// Tilt of node 7 : |0-0| = 0 (no children)
+// Tilt of node 2 : |3-5| = 2 (left subtree is just left child, so sum is 3; right subtree is just right child, so sum is 5)
+// Tilt of node 9 : |0-7| = 7 (no left child, so sum is 0; right subtree is just right child, so sum is 7)
+// Tilt of node 4 : |(3+5+2)-(9+7)| = |10-16| = 6 (left subtree values are 3, 5, and 2, which sums to 10; right subtree values are 9 and 7, which sums to 16)
+// Sum of every tilt : 0 + 0 + 0 + 2 + 7 + 6 = 15
+// Example 3:
+
+
