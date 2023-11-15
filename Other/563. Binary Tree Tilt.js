279f05863794fae76cafd259b29baca2a77918a5
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-15 10:43:20 +0800

    Add algo

--- Other/563. Binary Tree Tilt.js
+++ Other/563. Binary Tree Tilt.js
@@ -61,3 +61,11 @@ var findTilt = function(root) {
     dfs(root, tilt)
     return tilt.val
 
+    function dfs(root, tilt) {
+        if(!root) return 0
+        let left = dfs(root.left, tilt)
+        let right = dfs(root.right, tilt)
+        tilt.val += Math.abs(left - right)
+        return root.val + left + right
+    }
+};
