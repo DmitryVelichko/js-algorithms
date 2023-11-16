501f511aa34ce149552843eb51da86c488c91a9d
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-16 18:07:37 +0800

    Add algo

---
+++ Other/566-reshape-the-matrix.js
@@ -0,0 +1,6 @@
+// 566. Reshape the Matrix
+// Easy
+// 3.4K
+// 394
+// Companies
+// In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
