99eab1588edc0c7e7191c9f8c50b8b7ff6262473
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-17 09:17:04 +0800

    Add algo

---
+++ Other/572-subtree-of-another-tree.js
@@ -0,0 +1,6 @@
+// 572. Subtree of Another Tree
+// Easy
+// 7.8K
+// 456
+// Companies
+// Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
