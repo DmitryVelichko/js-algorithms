d7edd77c88babdfc706a94077ad397ad31aa116d
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-19 05:30:09 +0800

    Add algo

--- Other/575. Distribute Candies.js
+++ Other/575. Distribute Candies.js
@@ -11,3 +11,15 @@
 
  
 
+// Example 1:
+
+// Input: candyType = [1,1,2,2,3,3]
+// Output: 3
+// Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+// Example 2:
+
+// Input: candyType = [1,1,2,3]
+// Output: 2
+// Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+// Example 3:
+
