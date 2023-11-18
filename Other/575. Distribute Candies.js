b53c6e8a1df58809d8fc11d336fdc0a09c2a1769
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-19 05:29:56 +0800

    Add algo

---
+++ Other/575. Distribute Candies.js
@@ -0,0 +1,6 @@
+// 575. Distribute Candies
+// Easy
+// 1.4K
+// 1.3K
+// Companies
+// Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
