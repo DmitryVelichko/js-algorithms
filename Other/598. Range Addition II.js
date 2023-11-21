4de8625d941176b57c3c84be0265c39156f1399e
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-21 08:22:59 +0800

    Add algo

--- Other/598. Range Addition II.js
+++ Other/598. Range Addition II.js
@@ -16,3 +16,14 @@
 // Output: 4
 // Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
 // Example 2:
+
+// Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+// Output: 4
+// Example 3:
+
+// Input: m = 3, n = 3, ops = []
+// Output: 9
+ 
+
+// Constraints:
+
