64e9c479cf8fb0fca3fa8e9a9dede3db43d7e58b
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-25 11:54:15 +0800

    Add algo

--- Other/606. Construct String from Binary Tree.js
+++ Other/606. Construct String from Binary Tree.js
@@ -40,3 +40,9 @@
  * @param {TreeNode} root
  * @return {string}
  */
+var tree2str = function(root) {
+    if (!root) return '';
+    const left = tree2str(root.left);
+    const right = tree2str(root.right);
+    return root.val + (left || right ? `(${left})` : '') + (right ? `(${right})` : '');
+};
