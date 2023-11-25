91619bd43666ccce88873404e47ed03608716c9c
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-25 11:53:50 +0800

    Add algo

---
+++ Other/606. Construct String from Binary Tree.js
@@ -0,0 +1,7 @@
+// 606. Construct String from Binary Tree
+// Easy
+// 2.7K
+// 3.2K
+// Companies
+// Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
