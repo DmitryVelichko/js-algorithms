015ea882aa97a4f19fcc6f4b96e9a6e74025590c
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-27 09:11:23 +0800

    Add algo

--- Other/617. Merge Two Binary Trees.js
+++ Other/617. Merge Two Binary Trees.js
@@ -52,3 +52,13 @@ var mergeTrees = function(t1, t2) {
     if (t2 === null) {
         return t1;
     }
+    // if both nodes exist, sum the values
+    t1.val += t2.val;
+    
+    // do the same thing for left and right branch
+    t1.left = mergeTrees(t1.left, t2.left);
+    t1.right = mergeTrees(t1.right, t2.right);
+    
+    // return the merged t1
+    return t1;
+};
