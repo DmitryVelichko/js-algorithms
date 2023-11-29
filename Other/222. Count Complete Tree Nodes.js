4d3446f0dc15c4c7a6aba76ee16c71c4ced8fdcd
Dmitry Velichko <soleil-2005@mail.ru>
2023-11-30 07:38:51 +0800

    Add algo

--- Other/222. Count Complete Tree Nodes.js
+++ Other/222. Count Complete Tree Nodes.js
@@ -19,3 +19,22 @@
 // Input: root = []
 // Output: 0
 // Example 3:
+
+// Input: root = [1]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5 * 104].
+// 0 <= Node.val <= 5 * 104
+// The tree is guaranteed to be complete.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
