976a1ea15a417aef79059bf0be2d7ceb9c8a92f2
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-02 11:28:04 +0800

    Add algo

--- Other/653. Two Sum IV - Input is a BST.js
+++ Other/653. Two Sum IV - Input is a BST.js
@@ -43,3 +43,7 @@ var findTarget = function (root, k) {
     if (!root) return false;
     const set = new Set();
     const stack = [root];
+    while (stack.length) {
+        const node = stack.pop();
+        if (set.has(k - node.val)) return true;
+        set.add(node.val);
