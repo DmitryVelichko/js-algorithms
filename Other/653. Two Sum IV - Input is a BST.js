3a2144cd016a0b3e97fd0e6efae7a522609762cf
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-02 11:27:28 +0800

    Add algo

---
+++ Other/653. Two Sum IV - Input is a BST.js
@@ -0,0 +1,6 @@
+// 653. Two Sum IV - Input is a BST
+// Easy
+// 6.5K
+// 255
+// Companies
+// Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
