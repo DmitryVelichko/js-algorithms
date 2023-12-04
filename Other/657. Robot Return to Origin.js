b84242971142845286267ef51ba9d552467af59c
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-04 10:41:44 +0800

    Add algo

--- Other/657. Robot Return to Origin.js
+++ Other/657. Robot Return to Origin.js
@@ -7,3 +7,10 @@
 
 // You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
 
+// Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
+
+// Note: The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once, 'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+
+ 
+
+// Example 1:
