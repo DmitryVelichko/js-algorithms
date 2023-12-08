7ea88db2d8e375ee1b1447632de0e3937fc39043
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-08 08:35:21 +0800

    Add algo

--- Other/671. Second Minimum Node In a Binary Tree.js
+++ Other/671. Second Minimum Node In a Binary Tree.js
@@ -19,3 +19,14 @@
 // Example 2:
 
 
+// Input: root = [2,2,2]
+// Output: -1
+// Explanation: The smallest value is 2, but there isn't any second smallest value.
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 25].
+// 1 <= Node.val <= 231 - 1
+// root.val == min(root.left.val, root.right.val) for each internal node of the tree.
+
