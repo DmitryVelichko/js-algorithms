093b6b8d9845a8eb25278c4f5a6514fe605a33b0
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-09 11:18:35 +0800

    Add algo

--- Other/674. Longest Continuous Increasing Subsequence.js
+++ Other/674. Longest Continuous Increasing Subsequence.js
@@ -16,3 +16,16 @@
 // Explanation: The longest continuous increasing subsequence is [1,3,5] with length 3.
 // Even though [1,3,5,7] is an increasing subsequence, it is not continuous as elements 5 and 7 are separated by element
 // 4.
+// Example 2:
+
+// Input: nums = [2,2,2,2,2]
+// Output: 1
+// Explanation: The longest continuous increasing subsequence is [2] with length 1. Note that it must be strictly
+// increasing.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+
