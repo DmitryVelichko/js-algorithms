86b2a8c181cd32a4399a0a90cd4c772ea9832f17
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-10 09:01:19 +0800

    Add algo

--- Other/680. Valid Palindrome II.js
+++ Other/680. Valid Palindrome II.js
@@ -33,3 +33,12 @@ var validPalindrome = function(s) {
     let low = 0
     let high = s.length - 1
     while(low<high) {
+        if(s[low] !== s[high]) {
+            return isPalindrome(s, low+1, high) || isPalindrome(s, low, high-1)
+        }
+        low++
+        high--
+    }
+    return true
+};
+
