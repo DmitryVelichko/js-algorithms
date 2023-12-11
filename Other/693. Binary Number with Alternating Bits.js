720f73509279fbda05232b4c0617f074427ac0c8
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-11 09:27:48 +0800

    Add algo

--- Other/693. Binary Number with Alternating Bits.js
+++ Other/693. Binary Number with Alternating Bits.js
@@ -4,3 +4,13 @@
 // 110
 // Companies
 // Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: true
+// Explanation: The binary representation of 5 is: 101
+// Example 2:
+
