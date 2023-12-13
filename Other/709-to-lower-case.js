438bd9136d436e649f2b3009e17cce8d280625cd
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-13 08:39:01 +0800

    Add algo

--- Other/709-to-lower-case.js
+++ Other/709-to-lower-case.js
@@ -16,3 +16,12 @@
 // Input: s = "here"
 // Output: "here"
 // Example 3:
+
+// Input: s = "LOVELY"
+// Output: "lovely"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists of printable ASCII characters.
