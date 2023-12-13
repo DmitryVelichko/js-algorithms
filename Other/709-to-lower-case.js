33d410bd53ddf921efb12b3ebfe6d32350db8cf0
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-13 08:38:52 +0800

    Add algo

--- Other/709-to-lower-case.js
+++ Other/709-to-lower-case.js
@@ -4,3 +4,15 @@
 // 2.7K
 // Companies
 // Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: s = "here"
+// Output: "here"
+// Example 3:
