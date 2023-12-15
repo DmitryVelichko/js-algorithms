6d1f958695f01dece8fbf3722cbefd6133ae06a8
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-15 08:45:24 +0800

    Add algo

---
+++ Other/771. Jewels and Stones.js
@@ -0,0 +1,6 @@
+// 771. Jewels and Stones
+// Easy
+// 4.9K
+// 570
+// Companies
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
