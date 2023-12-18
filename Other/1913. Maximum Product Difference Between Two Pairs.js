040f3fd0f9501e9d4d976b18285cd749f4160c40
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-18 09:07:55 +0800

    Add algo

---
+++ Other/1913. Maximum Product Difference Between Two Pairs.js
@@ -0,0 +1,6 @@
+// 1913. Maximum Product Difference Between Two Pairs
+// Easy
+// 850
+// 47
+// Companies
+// The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
