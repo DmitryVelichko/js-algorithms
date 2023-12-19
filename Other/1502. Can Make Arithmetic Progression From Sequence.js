259236c2e6d9276a0cc53e34d8d46a6ce4f74410
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-19 08:07:32 +0800

    Add algo

--- Other/1502. Can Make Arithmetic Progression From Sequence.js
+++ Other/1502. Can Make Arithmetic Progression From Sequence.js
@@ -16,3 +16,12 @@
 // Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
 // Example 2:
 
+// Input: arr = [1,2,4]
+// Output: false
+// Explanation: There is no way to reorder the elements to obtain an arithmetic progression.
+ 
+
+// Constraints:
+
+// 2 <= arr.length <= 1000
+// -106 <= arr[i] <= 106
