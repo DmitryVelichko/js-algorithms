974a957f28e9b7edfd4a167a5818e3eed140c550
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-22 09:02:07 +0800

    Add algo

---
+++ Other/1436. Destination City.js
@@ -0,0 +1,6 @@
+// 1436. Destination City
+// Easy
+// 2.1K
+// 95
+// Companies
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
