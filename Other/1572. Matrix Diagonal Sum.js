55839371c952f94398bbfd133d9c2c78978a4fb0
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-23 08:57:28 +0800

    Add algo

---
+++ Other/1572. Matrix Diagonal Sum.js
@@ -0,0 +1,8 @@
+// 1572. Matrix Diagonal Sum
+// Easy
+// 3.3K
+// 46
+// Companies
+// Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that 
