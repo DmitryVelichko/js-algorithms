45cf0f8919b3772067ff1a9d9052997704ca5cdc
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-25 09:56:58 +0800

    Add algo

---
+++ Other/1446. Consecutive Characters.js
@@ -0,0 +1,8 @@
+// 1446. Consecutive Characters
+// Easy
+// 1.7K
+// 33
+// Companies
+// The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+// Given a string s, return the power of s.
