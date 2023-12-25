d6aa8395b8877b5cdf8f60b7182b3bdfd448528f
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-25 09:57:23 +0800

    Add algo

--- Other/1446. Consecutive Characters.js
+++ Other/1446. Consecutive Characters.js
@@ -34,3 +34,9 @@ var maxPower = function(s) {
     let power = 1
     let count = 1
 
+    for(let i=0; i<s.length-1; i++) {
+        if(s[i] === s[i+1]) {
+            count++
+            power = Math.max(power, count)
+        } else {
+            count = 1
