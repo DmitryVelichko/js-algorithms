d7df0c07e5eeafdfa25030621401b937d6f44c42
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-28 08:49:22 +0800

    Add algo

--- All/1550. Three Consecutive Odds.js
+++ All/1550. Three Consecutive Odds.js
@@ -32,3 +32,10 @@ var threeConsecutiveOdds = function(arr) {
     for(let num of arr) {
         if(!(num % 2 === 0)) {
             count++
+        } else {
+            count = 0
+        }
+        if (count === 3) return true
+    }
+    return false
+};
