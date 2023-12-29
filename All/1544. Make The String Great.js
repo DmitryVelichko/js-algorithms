f79f5f96b2ad6264ba6c03ae70eb2952840b240a
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-29 11:23:06 +0800

    Add algo

--- All/1544. Make The String Great.js
+++ All/1544. Make The String Great.js
@@ -14,3 +14,13 @@
 // Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
 
 // Notice that an empty string is also good.
+
+ 
+
+// Example 1:
+
+// Input: s = "leEeetcode"
+// Output: "leetcode"
+// Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+// Example 2:
+
