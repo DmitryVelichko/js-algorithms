6a8d9fb8e9ed22d8561d069f2cf4674de4d15191
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-29 11:23:12 +0800

    Add algo

--- All/1544. Make The String Great.js
+++ All/1544. Make The String Great.js
@@ -24,3 +24,14 @@
 // Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
 // Example 2:
 
+// Input: s = "abBAcC"
+// Output: ""
+// Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+// "abBAcC" --> "aAcC" --> "cC" --> ""
+// "abBAcC" --> "abBA" --> "aA" --> ""
+// Example 3:
+
+// Input: s = "s"
+// Output: "s"
+ 
+
