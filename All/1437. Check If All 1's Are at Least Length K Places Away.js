0e811b3942410d41d96e1f85c0731594e1c4387c
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-30 10:51:26 +0800

    Add algo

--- All/1437. Check If All 1's Are at Least Length K Places Away.js
+++ All/1437. Check If All 1's Are at Least Length K Places Away.js
@@ -19,3 +19,11 @@
 // Input: nums = [1,0,0,1,0,1], k = 2
 // Output: false
 // Explanation: The second 1 and third 1 are only one apart from each other.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 0 <= k <= nums.length
+// nums[i] is 0 or 1
+
