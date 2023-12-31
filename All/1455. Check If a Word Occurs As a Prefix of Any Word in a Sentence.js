e1737e0191e3779bd3f5a74b3fe91bb396a3f599
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-31 13:18:21 +0800

    Add algo

--- All/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.js
+++ All/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.js
@@ -51,3 +51,11 @@ var isPrefixOfWord = function(sentence, searchWord) {
 
     let words = sentence.split(' ')
 
+    for(let i=0; i<words.length; i++) {
+        if(helper(words[i], searchWord)) return i+1
+    }
+    return -1
+};
+
+
+
