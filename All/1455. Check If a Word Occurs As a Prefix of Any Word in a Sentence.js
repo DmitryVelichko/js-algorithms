f5af6f6f3f3b05954348850b8d65a258a569e079
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-31 13:18:04 +0800

    Add algo

--- All/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.js
+++ All/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.js
@@ -18,3 +18,13 @@
 // Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
 // Example 2:
 
+// Input: sentence = "this problem is an easy problem", searchWord = "pro"
+// Output: 2
+// Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+// Example 3:
+
+// Input: sentence = "i am tired", searchWord = "you"
+// Output: -1
+// Explanation: "you" is not a prefix of any word in the sentence.
+
+
