dc4e1e2bf68b6fa89a5bb636b3858a52ee4a5c95
Dmitry Velichko <soleil-2005@mail.ru>
2023-12-31 13:17:52 +0800

    Add algo

---
+++ All/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.js
@@ -0,0 +1,6 @@
+// 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+// Easy
+// 803
+// 40
+// Companies
+// Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
