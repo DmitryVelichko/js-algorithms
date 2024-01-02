e7eedb3808608dc0d1c99df11df799b0d5c71410
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-03 01:11:01 +0800

    Add algo

---
+++ All/2610. Convert an Array Into a 2D Array With Conditions.js
@@ -0,0 +1,6 @@
+// 2610. Convert an Array Into a 2D Array With Conditions
+// Medium
+// 1.3K
+// 57
+// Companies
+// You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
