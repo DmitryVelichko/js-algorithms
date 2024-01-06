4225eb0382abdf4e3355a6f7a23e5c3967ab8b00
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-06 11:04:00 +0800

    Add algo

---
+++ All/1235. Maximum Profit in Job Scheduling.js
@@ -0,0 +1,7 @@
+// 1235. Maximum Profit in Job Scheduling
+// Hard
+// 5.7K
+// 72
+// Companies
+// We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
