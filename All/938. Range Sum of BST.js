212b6b06ec983aa2f4e66744e91acf70e44e0ea8
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-08 14:46:43 +0800

    Add algo

--- All/938. Range Sum of BST.js
+++ All/938. Range Sum of BST.js
@@ -29,3 +29,16 @@
 // All Node.val are unique.
 
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
