eb90e9c2079fa322edbe3f30e21b24feb024a9fb
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-09 09:47:42 +0800

    Add algo

---
+++ All/872. Leaf-Similar Trees.js
@@ -0,0 +1,8 @@
+// 872. Leaf-Similar Trees
+// Easy
+// 3.5K
+// 77
+// Companies
+// Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+ 
