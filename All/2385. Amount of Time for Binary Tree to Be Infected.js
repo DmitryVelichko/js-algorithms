8a3d6c26fb5de833260cde4d2bf60403b4565c85
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-10 09:56:37 +0800

    Add algo

---
+++ All/2385. Amount of Time for Binary Tree to Be Infected.js
@@ -0,0 +1,6 @@
+// 2385. Amount of Time for Binary Tree to Be Infected
+// Medium
+// 1.6K
+// 19
+// Companies
+// You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
