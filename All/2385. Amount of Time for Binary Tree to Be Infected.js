863bbb3f7ddc688307e61e71132291e56ecf67ec
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-10 09:56:54 +0800

    Add algo

--- All/2385. Amount of Time for Binary Tree to Be Infected.js
+++ All/2385. Amount of Time for Binary Tree to Be Infected.js
@@ -28,3 +28,15 @@
 // Example 2:
 
 
+// Input: root = [1], start = 1
+// Output: 0
+// Explanation: At minute 0, the only node in the tree is infected so we return 0.
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 105].
+// 1 <= Node.val <= 105
+// Each node has a unique value.
+// A node with a value of start exists in the tree.
+
