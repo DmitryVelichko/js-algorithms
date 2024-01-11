baa3cea9325910e344bd0d6ecd39ac0f58834d5f
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-11 21:16:32 +0800

    Add algo

--- All/1026. Maximum Difference Between Node and Ancestor.js
+++ All/1026. Maximum Difference Between Node and Ancestor.js
@@ -33,3 +33,15 @@
 // The number of nodes in the tree is in the range [2, 5000].
 // 0 <= Node.val <= 105
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
