84f703d238461fc1b17e505470f22d0d5bb1d3ed
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-13 21:37:12 +0800

    Add algo

---
+++ All/1347. Minimum Number of Steps to Make Two Strings Anagram.js
@@ -0,0 +1,7 @@
+// 1347. Minimum Number of Steps to Make Two Strings Anagram
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
