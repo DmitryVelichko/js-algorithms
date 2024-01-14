83c21c71f5ca65c021408fb138a29b78de1b2fd0
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-14 23:48:14 +0800

    Add algo

---
+++ All/1657. Determine if Two Strings Are Close.js
@@ -0,0 +1,7 @@
+// 1657. Determine if Two Strings Are Close
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Two strings are considered close if you can attain one from the other using the following operations:
