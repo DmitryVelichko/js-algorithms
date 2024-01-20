3cb1c13bba4b013fde715600b4ef00bbcc259904
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-20 21:13:33 +0800

    Add algo

--- All/907. Sum of Subarray Minimums.js
+++ All/907. Sum of Subarray Minimums.js
@@ -35,3 +35,10 @@ var sumSubarrayMins = function(arr) {
     const stack = [];
     let sumOfMinimums = 0;
 
+    for (let i = 0; i <= arr.length; i++) {
+        while (stack.length && (i === arr.length || arr[stack[stack.length - 1]] >= arr[i])) {
+            const mid = stack.pop();
+            const leftBoundary = stack.length === 0 ? -1 : stack[stack.length - 1];
+            const rightBoundary = i;
+
+            const count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
