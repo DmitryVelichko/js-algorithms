64e68c063a7e08840bf71fc639b46a0c94fa5707
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-21 23:12:53 +0800

    Add algo

--- All/198-House-Robber.js
+++ All/198-House-Robber.js
@@ -37,3 +37,7 @@ var rob = function(nums) {
     if(nums.length === 1) return nums[0]
     let dp = new Array(nums.length).fill(0)
 
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+
+    
