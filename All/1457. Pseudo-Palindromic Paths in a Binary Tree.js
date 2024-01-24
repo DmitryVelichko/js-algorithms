e58f96429df8694372e54929347e471bb4b100a7
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-24 22:00:11 +0800

    Add algo

--- All/1457. Pseudo-Palindromic Paths in a Binary Tree.js
+++ All/1457. Pseudo-Palindromic Paths in a Binary Tree.js
@@ -30,3 +30,16 @@
 // Output: 1
  
 
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 105].
+// 1 <= Node.val <= 9
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
