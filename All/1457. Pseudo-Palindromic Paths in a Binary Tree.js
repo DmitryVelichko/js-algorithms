8561b796f2cb2a1aa3514400c7e7eec47d5cc92c
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-24 22:00:27 +0800

    Add algo

--- All/1457. Pseudo-Palindromic Paths in a Binary Tree.js
+++ All/1457. Pseudo-Palindromic Paths in a Binary Tree.js
@@ -63,3 +63,11 @@ var dfs = function(root, mask) {
 
     mask ^= 1 << root.val;
 
+    if (!root.left && !root.right) {
+        // Check if the bitmask represents a pseudo-palindromic path
+        return (mask & (mask - 1)) === 0 ? 1 : 0;
+    }
+
+    // Recursively traverse left and right subtrees
+    return dfs(root.left, mask) + dfs(root.right, mask);
+};
