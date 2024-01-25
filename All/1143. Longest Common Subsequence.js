cf315a499c7cad056aeea3ff51ffacfb0325691f
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-25 13:19:42 +0800

    Add algo

--- All/1143. Longest Common Subsequence.js
+++ All/1143. Longest Common Subsequence.js
@@ -8,3 +8,15 @@
 
 // A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
 
+// For example, "ace" is a subsequence of "abcde".
+// A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+// Example 1:
+
+// Input: text1 = "abcde", text2 = "ace" 
+// Output: 3  
+// Explanation: The longest common subsequence is "ace" and its length is 3.
+// Example 2:
+
