778659d2b0623c416a1c664bfdf62cc8f0871a82
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-26 10:09:56 +0800

    Add algo

--- All/576. Out of Boundary Paths.js
+++ All/576. Out of Boundary Paths.js
@@ -12,3 +12,14 @@
 // Example 1:
 
 
+// Input: m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+// Output: 6
+// Example 2:
+
+
+// Input: m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1
+// Output: 12
+ 
+
+// Constraints:
+
