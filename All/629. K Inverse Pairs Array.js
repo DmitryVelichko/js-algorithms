b41940179851dcbccfda4a20a5145926d2f33501
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-27 20:40:31 +0800

    Add algo

---
+++ All/629. K Inverse Pairs Array.js
@@ -0,0 +1,6 @@
+// 629. K Inverse Pairs Array
+// Solved
+// Hard
+// Topics
+// Companies
+// For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j].
