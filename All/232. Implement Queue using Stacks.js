5fe27a948ca9ea9147636bd286f3e187725e5295
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-29 17:11:50 +0800

    Add algo

--- All/232. Implement Queue using Stacks.js
+++ All/232. Implement Queue using Stacks.js
@@ -50,3 +50,23 @@ var MyQueue = function() {
     this.stack2 = [];
 };
 
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+    return this.stack2.pop();
+};
+
