7ac0983d75008cc26ba4493c772da2ac2675f347
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-29 17:11:14 +0800

    Add algo

---
+++ All/232. Implement Queue using Stacks.js
@@ -0,0 +1,8 @@
+
+// 232. Implement Queue using Stacks
+// Solved
+// Easy
+// Topics
+// Companies
+// Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
