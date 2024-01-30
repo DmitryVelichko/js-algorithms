a225d8450774fea3533069bc5a5b258321b5a0e4
Dmitry Velichko <soleil-2005@mail.ru>
2024-01-30 12:18:59 +0800

    Add algo

--- All/150. Evaluate Reverse Polish Notation.js
+++ All/150. Evaluate Reverse Polish Notation.js
@@ -52,3 +52,28 @@
 var evalRPN = function(tokens) {
     let stack = [];
     const operators = new Set(["+", "-", "*", "/"]);
+
+    for (let token of tokens) {
+        if (operators.has(token)) {
+            let a = stack.pop();
+            let b = stack.pop();
+            switch (token) {
+                case "+":
+                    stack.push(b + a);
+                    break;
+                case "-":
+                    stack.push(b - a);
+                    break;
+                case "*":
+                    stack.push(b * a);
+                    break;
+                case "/":
+                    stack.push(Math.trunc(b / a));
+                    break;
+            }
+        } else {
+            stack.push(parseInt(token));
+        }
+    }
+    return stack[0];
+};
