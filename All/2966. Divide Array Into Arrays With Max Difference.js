557358006b18ad63b975667c0d8f97f8c0914db7
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-01 14:26:41 +0800

    Add algo

---
+++ All/2966. Divide Array Into Arrays With Max Difference.js
@@ -0,0 +1,7 @@
+// 2966. Divide Array Into Arrays With Max Difference
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given an integer array nums of size n and a positive integer k.
