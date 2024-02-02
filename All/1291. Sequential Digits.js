0699c5398debc37f8e2ef8a64701450d2093ff01
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-02 11:40:49 +0800

    Add algo

---
+++ All/1291. Sequential Digits.js
@@ -0,0 +1,7 @@
+// 1291. Sequential Digits
+// Medium
+// Topics
+// Companies
+// Hint
+// An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
