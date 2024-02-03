23079b9205a0b0f569fe705884a477dcfda75baa
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-03 09:54:21 +0800

    Add algo

--- All/1043. Partition Array for Maximum Sum.js
+++ All/1043. Partition Array for Maximum Sum.js
@@ -43,3 +43,12 @@ var maxSumAfterPartitioning = function(arr, k) {
     for (let i = 1; i <= n; i++) {
         let max = 0;
         let maxSum = 0;
+        for (let j = 1; j <= k && i - j >= 0; j++) {
+            max = Math.max(max, arr[i - j]);
+            maxSum = Math.max(maxSum, dp[i - j] + max * j);
+        }
+        dp[i] = maxSum;
+    }
+    
+    return dp[n];
+};
