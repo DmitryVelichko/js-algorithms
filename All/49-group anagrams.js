6fed574517206859e79f68c860ddd1f3610e3b28
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-06 20:20:50 +0800

    Add algo

--- All/49-group anagrams.js
+++ All/49-group anagrams.js
@@ -25,3 +25,17 @@
 // 0 <= strs[i].length <= 100
 // strs[i] consists of lowercase English letters.
 
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+// Time O(N * K) | Space O(N * K)
+// Time Complexity: O(n*k) where n is the size of input array and k is the maximum length of string in input array
+
+
+var groupAnagrams = function (strs) {
+    let map = {};
+    for (let str of strs) {
+        let arr = new Array(26).fill(0);
+        for (let char of str) arr[char.charCodeAt() - 'a'.charCodeAt()]++;
+
