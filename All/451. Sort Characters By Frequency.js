6447d55bb3804e0c2be71b2778bc7d0a9e590b21
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-07 09:23:52 +0800

    Add algo

--- All/451. Sort Characters By Frequency.js
+++ All/451. Sort Characters By Frequency.js
@@ -17,3 +17,15 @@
 // So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
 // Example 2:
 
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+ 
+
