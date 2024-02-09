7d255cedc849426b37e26cd1d8ed56ee7f5e5374
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-09 20:03:22 +0800

    Add algo

--- All/368. Largest Divisible Subset.js
+++ All/368. Largest Divisible Subset.js
@@ -20,3 +20,15 @@
 
 // Input: nums = [1,2,4,8]
 // Output: [1,2,4,8]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 2 * 109
+// All the integers in nums are unique.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
