09cd79c540fdfa83aaa641647a56e582bf5dd5af
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-10 09:43:49 +0800

    Add algo

--- All/647-palindromic-substrings.js
+++ All/647-palindromic-substrings.js
@@ -4,3 +4,17 @@
 // Companies
 // Hint
 // Given a string s, return the number of palindromic substrings in it.
+
+// A string is a palindrome when it reads the same backward as forward.
+
+// A substring is a contiguous sequence of characters within the string.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc"
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
+// Example 2:
+
