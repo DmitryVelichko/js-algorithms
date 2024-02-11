35ec8beeb3d25486e8aa4b7949fbc2b142da4246
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-11 21:17:47 +0800

    Add algo

--- All/1463. Cherry Pickup II.js
+++ All/1463. Cherry Pickup II.js
@@ -31,3 +31,17 @@
 // Example 2:
 
 
+// Input: grid = [[1,0,0,0,0,0,1],[2,0,0,0,0,3,0],[2,0,9,0,0,0,0],[0,3,0,5,4,0,0],[1,0,2,3,0,0,6]]
+// Output: 28
+// Explanation: Path of robot #1 and #2 are described in color green and blue respectively.
+// Cherries taken by Robot #1, (1 + 9 + 5 + 2) = 17.
+// Cherries taken by Robot #2, (1 + 3 + 4 + 3) = 11.
+// Total of cherries: 17 + 11 = 28.
+ 
+
+// Constraints:
+
+// rows == grid.length
+// cols == grid[i].length
+// 2 <= rows, cols <= 70
+// 0 <= grid[i][j] <= 100
