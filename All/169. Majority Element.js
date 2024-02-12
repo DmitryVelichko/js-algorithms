00851a3822f9ed4fa0836494920bff5f86668d38
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-12 11:46:01 +0800

    Add algo

--- All/169. Majority Element.js
+++ All/169. Majority Element.js
@@ -32,3 +32,15 @@ function majorityElement(nums) {
     let candidate;
     let count = 0;
     
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        
+        count += (num === candidate) ? 1 : -1
+    }
+    
+    return candidate;
+};
+
+console.log(majorityElement([2,2,1,1,1,2,2])) 
