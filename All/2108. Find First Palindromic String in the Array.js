3242cfdb2f11137e90e670116cf38de2cc8c5905
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-13 10:24:13 +0800

    Add algo

---
+++ All/2108. Find First Palindromic String in the Array.js
@@ -0,0 +1,8 @@
+// 2108. Find First Palindromic String in the Array
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
