9b55fc3a8a79326cb35a7c9d4510e2d37b763d10
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-16 08:39:00 +0800

    Add algo

---
+++ All/1481. Least Number of Unique Integers after K Removals.js
@@ -0,0 +1,7 @@
+// 1481. Least Number of Unique Integers after K Removals
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
