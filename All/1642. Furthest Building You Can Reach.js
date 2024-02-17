19f2c16726c8964ae7a34b76042cd8dd5d742183
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-17 12:56:13 +0800

    Add algo

--- All/1642. Furthest Building You Can Reach.js
+++ All/1642. Furthest Building You Can Reach.js
@@ -71,3 +71,38 @@ var furthestBuilding = function(h, b, l) {
     return i;
 };
 
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+    
+    push(val) {
+        this.heap.push(val);
+        this.heapifyUp();
+    }
+    
+    pop() {
+        const max = this.heap[0];
+        const last = this.heap.pop();
+        
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.heapifyDown();
+        }
+        
+        return max;
+    }
+    
+    heapifyUp() {
+        let current = this.heap.length - 1;
+        while (current > 0) {
+            const parent = Math.floor((current - 1) / 2);
+            if (this.heap[parent] >= this.heap[current]) {
+                break;
+            }
+            [this.heap[parent], this.heap[current]] = [this.heap[current], this.heap[parent]];
+            current = parent;
+        }
+    }
+    
+
