4b68eb9dbb5b4a1819b24499c0af8aaa61b93e60
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-20 11:57:26 +0800

    Add algo

--- All/268. Missing Number.js
+++ All/268. Missing Number.js
@@ -38,3 +38,13 @@
  * @param {number[]} nums
  * @return {number}
  */
+var missingNumber = function(nums) {
+    let n = nums.length + 1;
+       let total = (n * (n-1)) / 2;
+       
+       for (let num of nums) {
+           total -= num;
+       }
+       
+       return total;
+};
