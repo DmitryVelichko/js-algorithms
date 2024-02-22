2d8180f0fa8357241bff09de1cc48c4bca734631
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-22 11:06:44 +0800

    Add algo

---
+++ All/997. Find the Town Judge.js
@@ -0,0 +1,5 @@
+// 997. Find the Town Judge
+// Easy
+// Topics
+// Companies
+// In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
