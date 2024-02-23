570854175f610939496f4d575df0b782802a82ad
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-23 15:11:19 +0800

    Add algo

---
+++ All/787. Cheapest Flights Within K Stops.js
@@ -0,0 +1,6 @@
+// 787. Cheapest Flights Within K Stops
+// Solved
+// Medium
+// Topics
+// Companies
+// There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
