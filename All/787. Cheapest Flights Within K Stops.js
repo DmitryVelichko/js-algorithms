0393144a5e60e26f8ba672eaf4355f9ef6a5e67b
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-23 15:11:37 +0800

    Add algo

--- All/787. Cheapest Flights Within K Stops.js
+++ All/787. Cheapest Flights Within K Stops.js
@@ -20,3 +20,21 @@
 // Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
 // Example 2:
 
+
+// Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+// Output: 200
+// Explanation:
+// The graph is shown above.
+// The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+// Example 3:
+
+
+// Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+// Output: 500
+// Explanation:
+// The graph is shown above.
+// The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+ 
+
+// Constraints:
+
