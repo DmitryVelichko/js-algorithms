588eb16a2b8fb1ae0dea0f5961fa2f5210340e88
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-24 11:47:17 +0800

    Add algo

--- All/2092. Find All People With Secret.js
+++ All/2092. Find All People With Secret.js
@@ -16,3 +16,22 @@
 
 // Example 1:
 
+// Input: n = 6, meetings = [[1,2,5],[2,3,8],[1,5,10]], firstPerson = 1
+// Output: [0,1,2,3,5]
+// Explanation:
+// At time 0, person 0 shares the secret with person 1.
+// At time 5, person 1 shares the secret with person 2.
+// At time 8, person 2 shares the secret with person 3.
+// At time 10, person 1 shares the secret with person 5.​​​​
+// Thus, people 0, 1, 2, 3, and 5 know the secret after all the meetings.
+// Example 2:
+
+// Input: n = 4, meetings = [[3,1,3],[1,2,2],[0,3,3]], firstPerson = 3
+// Output: [0,1,3]
+// Explanation:
+// At time 0, person 0 shares the secret with person 3.
+// At time 2, neither person 1 nor person 2 know the secret.
+// At time 3, person 3 shares the secret with person 0 and person 1.
+// Thus, people 0, 1, and 3 know the secret after all the meetings.
+// Example 3:
+
