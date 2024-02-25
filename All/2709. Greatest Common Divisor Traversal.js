c9710032d638c9c5f01186bbbcc8a353e3257197
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-25 12:10:30 +0800

    Add algo

---
+++ All/2709. Greatest Common Divisor Traversal.js
@@ -0,0 +1,7 @@
+// 2709. Greatest Common Divisor Traversal
+// Solved
+// Hard
+// Topics
+// Companies
+// Hint
+// You are given a 0-indexed integer array nums, and you are allowed to traverse between its indices. You can traverse between index i and index j, i != j, if and only if gcd(nums[i], nums[j]) > 1, where gcd is the greatest common divisor.
