5a2be14b77a58767ec6aab63ed9dc189890b925f
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-25 12:11:06 +0800

    Add algo

--- All/2709. Greatest Common Divisor Traversal.js
+++ All/2709. Greatest Common Divisor Traversal.js
@@ -14,3 +14,15 @@
 
 // Example 1:
 
+// Input: nums = [2,3,6]
+// Output: true
+// Explanation: In this example, there are 3 possible pairs of indices: (0, 1), (0, 2), and (1, 2).
+// To go from index 0 to index 1, we can use the sequence of traversals 0 -> 2 -> 1, where we move from index 0 to index 2 because gcd(nums[0], nums[2]) = gcd(2, 6) = 2 > 1, and then move from index 2 to index 1 because gcd(nums[2], nums[1]) = gcd(6, 3) = 3 > 1.
+// To go from index 0 to index 2, we can just go directly because gcd(nums[0], nums[2]) = gcd(2, 6) = 2 > 1. Likewise, to go from index 1 to index 2, we can just go directly because gcd(nums[1], nums[2]) = gcd(3, 6) = 3 > 1.
+// Example 2:
+
+// Input: nums = [3,9,5]
+// Output: false
+// Explanation: No sequence of traversals can take us from index 0 to index 2 in this example. So, we return false.
+// Example 3:
+
