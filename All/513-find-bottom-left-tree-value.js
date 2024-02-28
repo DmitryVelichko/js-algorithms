aa2d524a50b439010ba48ba14fc59a073b5f32d1
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-28 18:26:05 +0800

    Add algo

--- All/513-find-bottom-left-tree-value.js
+++ All/513-find-bottom-left-tree-value.js
@@ -42,3 +42,14 @@ class TreeNode {
 
         leftmostValue = node.val;
 
+        if (node.right) {
+            queue.push(node.right);
+        }
+        if (node.left) {
+            queue.push(node.left);
+        }
+    }
+
+    return leftmostValue;
+};
+  
