b549241ce38d34bda7d80461d2311b4f5cca6986
Dmitry Velichko <soleil-2005@mail.ru>
2024-02-29 15:50:02 +0800

    Add algo

--- All/1609. Even Odd Tree.js
+++ All/1609. Even Odd Tree.js
@@ -16,3 +16,24 @@
 // Example 1:
 
 
+// Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
+// Output: true
+// Explanation: The node values on each level are:
+// Level 0: [1]
+// Level 1: [10,4]
+// Level 2: [3,7,9]
+// Level 3: [12,8,6,2]
+// Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd.
+// Example 2:
+
+
+// Input: root = [5,4,2,3,3,7]
+// Output: false
+// Explanation: The node values on each level are:
+// Level 0: [5]
+// Level 1: [4,2]
+// Level 2: [3,3,7]
+// Node values in level 2 must be in strictly increasing order, so the tree is not Even-Odd.
+// Example 3:
+
+
