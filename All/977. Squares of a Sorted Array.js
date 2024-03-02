b7ed8fcc6f62a64719c03108909a912308f30f5e
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-03 00:00:10 +0800

    Add algo

--- All/977. Squares of a Sorted Array.js
+++ All/977. Squares of a Sorted Array.js
@@ -26,3 +26,14 @@
  
 
 // Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */ 
+var sortedSquares = function(nums) {
+    for(let i=0; i<nums.length; i++) {
+        nums[i] = nums[i] * nums[i]
+    }
+    return nums.sort((a,b) => a-b)
+};
