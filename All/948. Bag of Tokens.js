dc3bc41b6825f246d015b8984612e04baa7a6f5a
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-04 15:29:50 +0800

    Add algo

---
+++ All/948. Bag of Tokens.js
@@ -0,0 +1,6 @@
+// 948. Bag of Tokens
+// Solved
+// Medium
+// Topics
+// Companies
+// You start with an initial power of power, an initial score of 0, and a bag of tokens given as an integer array tokens, where each tokens[i] donates the value of tokeni.
