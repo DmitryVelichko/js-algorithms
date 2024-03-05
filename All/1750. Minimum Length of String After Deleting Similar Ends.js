ce32843c126274cfb9b38181afbf95d9d6e30591
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-05 22:09:14 +0800

    Add algo

--- All/1750. Minimum Length of String After Deleting Similar Ends.js
+++ All/1750. Minimum Length of String After Deleting Similar Ends.js
@@ -47,3 +47,16 @@
  * @param {string} s
  * @return {number}
  */
+var minimumLength = function(s) {
+    let left = 0; // Initialize left pointer
+    let right = s.length - 1; // Initialize right pointer
+
+    // Iterate while left pointer is less than right pointer and characters at both pointers are the same
+    while (left < right && s[left] === s[right]) {
+        let char = s[left]; // Store the common character to both ends
+        
+        // Move left pointer to the right until a different character is encountered
+        while (left <= right && s[left] === char) {
+            left++;
+        }
+        
