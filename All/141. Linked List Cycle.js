97fd6685d5c392a607dc60106776c86f4aeb4632
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-06 14:25:08 +0800

    Add algo

--- All/141. Linked List Cycle.js
+++ All/141. Linked List Cycle.js
@@ -18,3 +18,16 @@
 // Output: true
 // Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 // Example 2:
+
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+// Example 3:
+
+
+// Input: head = [1], pos = -1
+// Output: false
+// Explanation: There is no cycle in the linked list.
+ 
+
