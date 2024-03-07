db91a8ae4103b6fa0ee4de068920736bca37dbd4
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-07 12:43:30 +0800

    Add algo

--- All/876. Middle of the Linked List.js
+++ All/876. Middle of the Linked List.js
@@ -35,3 +35,13 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next
+    }
+    return slow
+};
