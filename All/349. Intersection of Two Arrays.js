dd1862fc9c2771749fc33a19bbd5999273ee8447
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-10 23:15:50 +0800

    Add algo

--- All/349. Intersection of Two Arrays.js
+++ All/349. Intersection of Two Arrays.js
@@ -36,3 +36,13 @@ var intersection = function(nums1, nums2) {
         hash[num] = (hash[num] || 0) + 1
     }
 
+    for(let num of nums2) {
+        if(hash[num]) {
+            res.push(num)
+            delete hash[num]
+        }
+    }
+
+    return res
+
+};
