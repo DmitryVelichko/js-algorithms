e4684a3ec90d84da385e0839a56ff2a739559ba7
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-11 21:36:50 +0800

    Add algo

---
+++ All/791. Custom Sort String.js
@@ -0,0 +1,6 @@
+// 791. Custom Sort String
+// Solved
+// Medium
+// Topics
+// Companies
+// You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
