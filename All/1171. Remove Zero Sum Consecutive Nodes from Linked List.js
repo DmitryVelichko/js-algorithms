f46b0c6be220ccdd17d980243ecc2c20149fe235
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-12 12:32:21 +0800

    Add algo

---
+++ All/1171. Remove Zero Sum Consecutive Nodes from Linked List.js
@@ -0,0 +1,7 @@
+// 1171. Remove Zero Sum Consecutive Nodes from Linked List
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
