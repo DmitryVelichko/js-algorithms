04dad587b93fbc562ceda5ba4249821eb44bfa5d
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-13 12:30:23 +0800

    Add algo

--- All/2485. Find the Pivot Integer.js
+++ All/2485. Find the Pivot Integer.js
@@ -18,3 +18,13 @@
 // Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
 // Example 2:
 
+// Input: n = 1
+// Output: 1
+// Explanation: 1 is the pivot integer since: 1 = 1.
+// Example 3:
+
+// Input: n = 4
+// Output: -1
+// Explanation: It can be proved that no such integer exist.
+ 
+
