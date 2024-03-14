ad95703a6147ea8f89df8b0181ca91479b111ef5
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-14 16:46:23 +0800

    Add algo

---
+++ All/930. Binary Subarrays With Sum.js
@@ -0,0 +1,6 @@
+// 930. Binary Subarrays With Sum
+// Solved
+// Medium
+// Topics
+// Companies
+// Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
