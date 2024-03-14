20b57b4080f0f41924fb69ea4515d4027883d529
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-14 16:46:49 +0800

    Add algo

--- All/930. Binary Subarrays With Sum.js
+++ All/930. Binary Subarrays With Sum.js
@@ -41,3 +41,13 @@ var numSubarraysWithSum = function(nums, goal) {
     let currSum = 0;
     let totalSubarrays = 0;
 
+    for (let num of nums) {
+        currSum += num;
+        if (count.has(currSum - goal)) {
+            totalSubarrays += count.get(currSum - goal);
+        }
+        count.set(currSum, (count.get(currSum) || 0) + 1);
+    }
+
+    return totalSubarrays;
+};
