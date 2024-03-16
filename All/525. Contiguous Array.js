4a3c34b7091f9f78daa1fb3cc842dae2c2dcac46
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-16 12:38:40 +0800

    Add algo

--- All/525. Contiguous Array.js
+++ All/525. Contiguous Array.js
@@ -24,3 +24,16 @@
 // 1 <= nums.length <= 105
 // nums[i] is either 0 or 1.
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxLength = (nums) => {
+    const hash = {};
+    let max_length = 0;
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      const current = nums[i];
+      if (current === 0) {
+          // if the current element is 0, then we decrement the count
+        count--;
