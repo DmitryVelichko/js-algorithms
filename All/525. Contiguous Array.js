f85054e988c6e3fb42538e6c54c0bed6362d2039
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-16 12:38:33 +0800

    Add algo

--- All/525. Contiguous Array.js
+++ All/525. Contiguous Array.js
@@ -14,3 +14,13 @@
 // Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
 // Example 2:
 
+// Input: nums = [0,1,0]
+// Output: 2
+// Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
