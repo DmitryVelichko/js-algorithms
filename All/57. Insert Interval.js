29cedfac8df2544fb2e1ab1268876a7311aa7a72
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-17 19:27:11 +0800

    Add algo

--- All/57. Insert Interval.js
+++ All/57. Insert Interval.js
@@ -47,3 +47,16 @@ var insert = function(intervals, newInterval) {
         i++;
     }
 
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])];
+        i++;
+    }
+    merged.push(newInterval);
+
+    while (i < intervals.length) {
+        merged.push(intervals[i]);
+        i++;
+    }
+
+    return merged;
+};
