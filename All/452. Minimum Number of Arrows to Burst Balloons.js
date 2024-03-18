f9169628f67515c8923a6b2729db08e6f12a9c03
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-18 15:09:40 +0800

    Add algo

--- All/452. Minimum Number of Arrows to Burst Balloons.js
+++ All/452. Minimum Number of Arrows to Burst Balloons.js
@@ -20,3 +20,15 @@
 // - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
 // Example 2:
 
+// Input: points = [[1,2],[3,4],[5,6],[7,8]]
+// Output: 4
+// Explanation: One arrow needs to be shot for each balloon for a total of 4 arrows.
+// Example 3:
+
+// Input: points = [[1,2],[2,3],[3,4],[4,5]]
+// Output: 2
+// Explanation: The balloons can be burst by 2 arrows:
+// - Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
+// - Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
+ 
+
