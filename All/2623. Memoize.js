249d5af5de9dfcea2508b1198116c92aab0b45b4
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-20 15:09:12 +0800

    Add algo

--- All/2623. Memoize.js
+++ All/2623. Memoize.js
@@ -86,3 +86,17 @@ function memoize(fn) {
 
         return result
     }
+
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
