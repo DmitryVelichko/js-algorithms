0df0b5b310cb22840712811b6a084c60ffbe517a
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-22 12:27:07 +0800

    Add algo

---
+++ All/2727. Is Object Empty.js
@@ -0,0 +1,8 @@
+// 2727. Is Object Empty
+// Easy
+
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
