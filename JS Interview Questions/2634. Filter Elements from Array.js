c9a967145f00add4ed42ee90834c851f3dbb374d
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-25 21:04:32 +0800

    Add algo

--- JS Interview Questions/2634. Filter Elements from Array.js
+++ JS Interview Questions/2634. Filter Elements from Array.js
@@ -15,3 +15,12 @@
 
  
 
+// Example 1:
+
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+// Output: [20,30]
+// Explanation:
+// const newArray = filter(arr, fn); // [20, 30]
+// The function filters out values that are not greater than 10
+// Example 2:
+
