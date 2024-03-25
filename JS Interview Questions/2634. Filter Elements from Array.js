48d0a63e1fd7287e7d316fa28af2a312b214d06a
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-25 21:04:48 +0800

    Add algo

--- JS Interview Questions/2634. Filter Elements from Array.js
+++ JS Interview Questions/2634. Filter Elements from Array.js
@@ -37,3 +37,13 @@
 // Falsey values such as 0 should be filtered out
  
 
+// Constraints:
+
+// 0 <= arr.length <= 1000
+// -109 <= arr[i] <= 109
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
