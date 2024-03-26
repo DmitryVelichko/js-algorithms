e4ed8b03a8ad38515ca4d423fa0aa8653cb75e10
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-26 11:12:10 +0800

    Add algo

---
+++ JS Interview Questions/2665. Counter II.js
@@ -0,0 +1,6 @@
+// 2665. Counter II
+// Solved
+// Easy
+// Companies
+// Hint
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
