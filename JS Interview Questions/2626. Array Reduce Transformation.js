fa5c9f56875018b27b51edc3513c8591a220d753
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-27 12:43:38 +0800

    Add algo

--- JS Interview Questions/2626. Array Reduce Transformation.js
+++ JS Interview Questions/2626. Array Reduce Transformation.js
@@ -29,3 +29,17 @@
 // The final answer is 10.
 // Example 2:
 
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr * curr; }
+// init = 100
+// Output: 130
+// Explanation:
+// initially, the value is init=100.
+// (100) + nums[0] * nums[0] = 101
+// (101) + nums[1] * nums[1] = 105
+// (105) + nums[2] * nums[2] = 114
+// (114) + nums[3] * nums[3] = 130
+// The final answer is 130.
+// Example 3:
+
