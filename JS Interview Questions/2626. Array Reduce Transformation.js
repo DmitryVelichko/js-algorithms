94618286a9e80ac4d23fe7cb8af7560c9de1be99
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-27 12:43:52 +0800

    Add algo

--- JS Interview Questions/2626. Array Reduce Transformation.js
+++ JS Interview Questions/2626. Array Reduce Transformation.js
@@ -57,3 +57,19 @@
 // 0 <= nums[i] <= 1000
 // 0 <= init <= 1000
 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if(nums.length === 0) return init
+
+    let acc = init
+
+    for(let i=0; i<nums.length; i++) {
+        acc = fn(acc, nums[i])
+    }
+    return acc
+};
