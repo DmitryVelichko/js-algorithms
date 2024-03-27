daaaf1896922e6a98d92dc7220720509219a1aa1
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-27 12:43:15 +0800

    Add algo

---
+++ JS Interview Questions/2626. Array Reduce Transformation.js
@@ -0,0 +1,6 @@
+// 2626. Array Reduce Transformation
+// Solved
+// Easy
+// Companies
+// Hint
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
