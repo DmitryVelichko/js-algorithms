cdf040ced2d6def534d233dcb019a14b7b8752c0
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-28 12:55:13 +0800

    Add algo

--- JS Interview Questions/2667. Create Hello World Function.js
+++ JS Interview Questions/2667. Create Hello World Function.js
@@ -15,3 +15,13 @@
 
 // The function returned by createHelloWorld should always return "Hello World".
 // Example 2:
+
+// Input: args = [{},null,42]
+// Output: "Hello World"
+// Explanation:
+// const f = createHelloWorld();
+// f({}, null, 42); // "Hello World"
+
+// Any arguments could be passed to the function but it should still always return "Hello World".
+ 
+
