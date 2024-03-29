6143539a14c75c60898654f8cc8acdda03dfc4ba
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-29 10:58:44 +0800

    Add algo

--- JS Interview Questions/2704. To Be Or Not To Be.js
+++ JS Interview Questions/2704. To Be Or Not To Be.js
@@ -34,3 +34,14 @@ var expect = function(val) {
             if(value === val) return true
             else throw new Error("Not Equal")
         },
+        notToBe(value) {
+            if(value !== val) return true
+            else throw new Error('Equal')
+        }
+    }
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
