0c45e17e629c8e472dd7efc7915e601eeaee1bad
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-29 10:58:20 +0800

    Add algo

---
+++ JS Interview Questions/2704. To Be Or Not To Be.js
@@ -0,0 +1,5 @@
+// 2704. To Be Or Not To Be
+// Solved
+// Easy
+// Companies
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
