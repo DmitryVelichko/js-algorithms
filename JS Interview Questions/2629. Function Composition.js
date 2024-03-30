a186e79279327211e3a75ea6be648d74692fba22
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-30 11:45:04 +0800

    Add algo

---
+++ JS Interview Questions/2629. Function Composition.js
@@ -0,0 +1,6 @@
+// 2629. Function Composition
+// Solved
+// Easy
+// Companies
+// Hint
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
