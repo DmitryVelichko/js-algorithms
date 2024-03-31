f7a65024669c36d246ba55e834d5d38bef7d00fd
Dmitry Velichko <soleil-2005@mail.ru>
2024-03-31 10:14:46 +0800

    Add algo

---
+++ JS Interview Questions/2715. Timeout Cancellation.js
@@ -0,0 +1,6 @@
+// 2715. Timeout Cancellation
+// Solved
+// Easy
+// Companies
+// Given a function fn, an array of arguments args, and a timeout t in milliseconds, return a cancel function cancelFn.
+
