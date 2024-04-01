6bec1c5eb7ffb8f0bebea23184a7549f090c0310
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-01 21:23:59 +0800

    Add algo

--- JS Interview Questions/2623. Memoize.js
+++ JS Interview Questions/2623. Memoize.js
@@ -83,3 +83,13 @@ function memoize(fn) {
 }
 
 
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
