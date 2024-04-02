3853b58a04670275a21a79845e13280daf467add
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-02 10:47:28 +0800

    Add algo

--- JS Interview Questions/2677. Chunk Array.js
+++ JS Interview Questions/2677. Chunk Array.js
@@ -38,3 +38,16 @@
 // 2 <= JSON.stringify(arr).length <= 105
 // 1 <= size <= arr.length + 1
 
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let res = []
+
+    while(arr.length) {
+        res.push(arr.splice(0,size))
+    }
+    return res
+};
