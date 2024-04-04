53a95508a82a03c18eb01f0663a5dd1a6e89d0f9
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-04 13:11:53 +0800

    Add algo

--- JS Interview Questions/2703. Return Length of Arguments Passed.js
+++ JS Interview Questions/2703. Return Length of Arguments Passed.js
@@ -25,3 +25,19 @@
 
 // Constraints:
 
+// args is a valid JSON array
+// 0 <= args.length <= 100
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+
+    return args.length
+ 
+ };
+ 
+ /**
+  * argumentsLength(1, 2, 3); // 3
+  */
