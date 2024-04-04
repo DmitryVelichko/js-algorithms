d5025c9c4bae1f13b3468d3f04885ae54e7d7d55
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-04 13:11:45 +0800

    Add algo

--- JS Interview Questions/2703. Return Length of Arguments Passed.js
+++ JS Interview Questions/2703. Return Length of Arguments Passed.js
@@ -15,3 +15,13 @@
 // One value was passed to the function so it should return 1.
 // Example 2:
 
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation: 
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+ 
+
+// Constraints:
+
