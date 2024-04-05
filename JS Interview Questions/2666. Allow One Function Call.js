bfa952eb63431917d9c67c08231ec0ae81d8433e
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-05 09:55:56 +0800

    Add algo

--- JS Interview Questions/2666. Allow One Function Call.js
+++ JS Interview Questions/2666. Allow One Function Call.js
@@ -17,3 +17,13 @@
 // onceFn(1, 2, 3); // 6
 // onceFn(2, 3, 6); // undefined, fn was not called
 // Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+ 
+
