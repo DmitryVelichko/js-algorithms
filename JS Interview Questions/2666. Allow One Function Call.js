934888aa263a7a1b18fce375594e6d454193e907
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-05 09:55:43 +0800

    Add algo

---
+++ JS Interview Questions/2666. Allow One Function Call.js
@@ -0,0 +1,5 @@
+// 2666. Allow One Function Call
+// Solved
+// Easy
+// Companies
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
