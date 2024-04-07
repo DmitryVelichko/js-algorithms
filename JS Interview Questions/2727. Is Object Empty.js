1dc0dca3a013b223610b2801d3cc03f9fb70fdf7
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-07 11:28:15 +0800

    Add algo

--- JS Interview Questions/2727. Is Object Empty.js
+++ JS Interview Questions/2727. Is Object Empty.js
@@ -17,3 +17,13 @@
 // Explanation: The object has 2 key-value pairs so it is not empty.
 // Example 2:
 
+// Input: obj = {}
+// Output: true
+// Explanation: The object doesn't have any key-value pairs so it is empty.
+// Example 3:
+
+// Input: obj = [null, false, 0]
+// Output: false
+// Explanation: The array has 3 elements so it is not empty.
+ 
+
