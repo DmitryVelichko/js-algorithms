3c4a4b4e3fa02a84863ed035c181393e4ed91708
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-09 10:59:45 +0800

    Add algo

--- JS Interview Questions/2635. Apply Transform Over Each Element in Array.js
+++ JS Interview Questions/2635. Apply Transform Over Each Element in Array.js
@@ -41,3 +41,12 @@
  * @param {Function} fn
  * @return {number[]}
  */
+var map = function(arr, fn) {
+    let newArr = []
+
+    for(let i=0; i<arr.length; i++) {
+        newArr[i] = fn(arr[i], i)
+    }
+
+    return newArr
+};
