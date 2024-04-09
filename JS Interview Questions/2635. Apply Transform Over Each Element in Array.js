462e5e4a4ffde1b144b3dd27c8e1fbd1ec3ad662
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-09 10:59:13 +0800

    Add algo

---
+++ JS Interview Questions/2635. Apply Transform Over Each Element in Array.js
@@ -0,0 +1,6 @@
+// 2635. Apply Transform Over Each Element in Array
+// Solved
+// Easy
+// Companies
+// Hint
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
