5b6c4985518674cc1e950bca82c84741c3f1c8fc
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-10 11:28:03 +0800

    Add algo

--- JS Interview Questions/2725. Interval Cancellation.js
+++ JS Interview Questions/2725. Interval Cancellation.js
@@ -10,3 +10,22 @@
 // The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
 
  
+
+ 
+
+// Example 1:
+
+// Input: fn = (x) => x * 2, args = [4], t = 35
+// Output: 
+// [
+//    {"time": 0, "returned": 8},
+//    {"time": 35, "returned": 8},
+//    {"time": 70, "returned": 8},
+//    {"time": 105, "returned": 8},
+//    {"time": 140, "returned": 8},
+//    {"time": 175, "returned": 8}
+// ]
+// Explanation: 
+// const cancelTimeMs = 190;
+// const cancelFn = cancellable((x) => x * 2, [4], 35);
+// setTimeout(cancelFn, cancelTimeMs);
