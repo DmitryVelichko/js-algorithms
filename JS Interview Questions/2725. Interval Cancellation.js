54919fd744477301d9bd8b3dbdff8820a3f81add
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-10 11:28:23 +0800

    Add algo

--- JS Interview Questions/2725. Interval Cancellation.js
+++ JS Interview Questions/2725. Interval Cancellation.js
@@ -65,3 +65,24 @@
 // Cancelled at 165ms
 // Example 3:
 
+// Input: fn = (x1, x2, x3) => (x1 + x2 + x3), args = [5, 1, 3], t = 50
+// Output: 
+// [
+//    {"time": 0, "returned": 9},
+//    {"time": 50, "returned": 9},
+//    {"time": 100, "returned": 9},
+//    {"time": 150, "returned": 9}
+// ]
+// Explanation: 
+// const cancelTimeMs = 180;
+// const cancelFn = cancellable((x1, x2, x3) => (x1 + x2 + x3), [5, 1, 3], 50)
+// setTimeout(cancelFn, cancelTimeMs)
+
+// Every 50ms, fn(5, 1, 3) is called. Until t=180ms, then it is cancelled. 
+// 1st fn call is at 0ms
+// 2nd fn call is at 50ms
+// 3rd fn call is at 100ms
+// 4th fn call is at 150ms
+// Cancelled at 180ms
+ 
+
