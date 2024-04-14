370204c8f18a272d93ffef314e11e34efd68f9d6
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-14 12:10:25 +0800

    Add algo

--- JS Interview Questions/2695. Array Wrapper.js
+++ JS Interview Questions/2695. Array Wrapper.js
@@ -56,3 +56,17 @@ ArrayWrapper.prototype.valueOf = function() {
     return res
 }
 
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(',')}]`
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
