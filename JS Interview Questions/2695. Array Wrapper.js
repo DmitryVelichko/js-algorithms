08ac80fca2887af773f4dc84d23d761c1a162737
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-14 12:10:02 +0800

    Add algo

--- JS Interview Questions/2695. Array Wrapper.js
+++ JS Interview Questions/2695. Array Wrapper.js
@@ -10,3 +10,17 @@
 
 // Example 1:
 
+// Input: nums = [[1,2],[3,4]], operation = "Add"
+// Output: 10
+// Explanation:
+// const obj1 = new ArrayWrapper([1,2]);
+// const obj2 = new ArrayWrapper([3,4]);
+// obj1 + obj2; // 10
+// Example 2:
+
+// Input: nums = [[23,98,42,70]], operation = "String"
+// Output: "[23,98,42,70]"
+// Explanation:
+// const obj = new ArrayWrapper([23,98,42,70]);
+// String(obj); // "[23,98,42,70]"
+// Example 3:
