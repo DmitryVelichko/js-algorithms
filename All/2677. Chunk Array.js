b24befad45fe0893f2921768282e543ef6a92abb
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-15 22:46:33 +0800

    Add algo

--- All/2677. Chunk Array.js
+++ All/2677. Chunk Array.js
@@ -16,3 +16,13 @@
 // Explanation: The arr has been split into subarrays each with 1 element.
 // Example 2:
 
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+// Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+// Example 3:
+
+// Input: arr = [8,5,3,2,6], size = 6
+// Output: [[8,5,3,2,6]]
+// Explanation: Size is greater than arr.length thus all elements are in the first subarray.
+// Example 4:
+
