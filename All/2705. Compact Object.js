028d376272517fb1261554e09397b7b60c46087f
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-20 21:05:33 +0800

    Add algo

---
+++ All/2705. Compact Object.js
@@ -0,0 +1,5 @@
+// 2705. Compact Object
+// Medium
+
+// Given an object or array obj, return a compact object. A compact object is the same as the original object, except with keys containing falsy values removed. This operation applies to the object and any nested objects. Arrays are considered objects where the indices are keys. A value is considered falsy when Boolean(value) returns false.
+
