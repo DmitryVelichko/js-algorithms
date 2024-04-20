65b62a0a58e0277db86ad6846c8e264fb80c488d
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-21 07:21:28 +0800

    Add algo

--- All/2705.Compact Object.js
+++ All/2705.Compact Object.js
@@ -40,3 +40,18 @@ var compactObject = function(obj) {
     function dfs(obj) {
         if (!obj) return false;
         if (typeof obj !== 'object') return obj;
+  
+        if (Array.isArray(obj)) {
+            const newArr = [];
+            for(let i = 0; i < obj.length; i++) {
+                const curr = obj[i];
+                const subRes = dfs(curr);
+  
+                if(subRes) {
+                    newArr.push(subRes);
+                }
+            }
+  
+            return newArr;
+        }
+  
