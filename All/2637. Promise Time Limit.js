1b1f6020d689334780c16d9b9a031f57afd3b437
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-22 10:28:27 +0800

    Add algo

--- All/2637. Promise Time Limit.js
+++ All/2637. Promise Time Limit.js
@@ -36,3 +36,28 @@
 
 // The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
 // Example 2:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 150
+// Output: {"resolved":25,"time":100}
+// Explanation:
+// The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+// Example 3:
+
+// Input: 
+// fn = async (a, b) => { 
+//   await new Promise(res => setTimeout(res, 120)); 
+//   return a + b; 
+// }
+// inputs = [5,10]
+// t = 150
+// Output: {"resolved":15,"time":120}
+// Explanation:
+// ​​​​The function resolved 5 + 10 = 15 at t=120ms. The time limit is never reached.
+// Example 4:
+
