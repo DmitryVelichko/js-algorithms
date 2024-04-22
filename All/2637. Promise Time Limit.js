617d07cd1dbae95215065dcbdef26e9d287e2a93
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-22 10:28:18 +0800

    Add algo

--- All/2637. Promise Time Limit.js
+++ All/2637. Promise Time Limit.js
@@ -22,3 +22,17 @@
 // inputs = [5]
 // t = 50
 // Output: {"rejected":"Time Limit Exceeded","time":50}
+// Explanation:
+// const limited = timeLimit(fn, t)
+// const start = performance.now()
+// let result;
+// try {
+//    const res = await limited(...inputs)
+//    result = {"resolved": res, "time": Math.floor(performance.now() - start)};
+// } catch (err) {
+//    result = {"rejected": err, "time": Math.floor(performance.now() - start)};
+// }
+// console.log(result) // Output
+
+// The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+// Example 2:
