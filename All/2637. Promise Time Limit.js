5748b3c39400d0e1df880378eb443f488a47e282
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-22 10:28:01 +0800

    Add algo

---
+++ All/2637. Promise Time Limit.js
@@ -0,0 +1,8 @@
+
+// 2637. Promise Time Limit
+// Solved
+// Medium
+// Companies
+// Hint
+// Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
+
