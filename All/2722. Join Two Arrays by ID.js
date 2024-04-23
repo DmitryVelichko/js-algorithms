5fae545ebeb171531d23bb6ef8c5636eaa243e54
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-23 10:15:00 +0800

    Add algo

---
+++ All/2722. Join Two Arrays by ID.js
@@ -0,0 +1,6 @@
+// 2722. Join Two Arrays by ID
+// Solved
+// Medium
+// Companies
+// Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. 
+
