b41c14e6f25cc973a0b775bcad556f79da94ae8d
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-25 10:26:09 +0800

    Add algo

--- All/63-unique-paths-II.js
+++ All/63-unique-paths-II.js
@@ -12,3 +12,26 @@
 
  
 
+// Example 1:
+
+
+// Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: 2
+// Explanation: There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+// Example 2:
+
+
+// Input: obstacleGrid = [[0,1],[0,0]]
+// Output: 1
+ 
+
+// Constraints:
+
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] is 0 or 1.
+
