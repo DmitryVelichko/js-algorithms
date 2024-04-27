d8363969f662aa177169b2bc2894612e44e7d740
Dmitry Velichko <soleil-2005@mail.ru>
2024-04-27 15:34:30 +0800

    Add algo

--- All/valid-palindrome.js
+++ All/valid-palindrome.js
@@ -15,3 +15,15 @@ let isPalindrome1 = function (s) {
     x = x.toString()
     var i = 0;
     var j = x.length - 1;
+    while(i < j){
+        if(x[i] !== x[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+  };
+  
+  let isPalindrome = function (x) {
+    let reversedStr = ''
+    let xStr = x.toString()
+  
