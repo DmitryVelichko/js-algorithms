64a33acb1a595cbf534b1ec766f9539ba8d338c6
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-03 22:23:48 +0800

    Add algo

--- All/1st-unique-char-string.js
+++ All/1st-unique-char-string.js
@@ -21,3 +21,11 @@ let firstUniqChar = function (s) {
 };
 
 console.log(firstUniqChar(s));
+
+// binary search
+
+// Binary Search O(log n)
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+
