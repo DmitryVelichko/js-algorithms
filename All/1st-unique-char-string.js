2e2e731153082d483849bfa36f0f902b7e8b4a12
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-03 22:23:17 +0800

    Add algo

--- All/1st-unique-char-string.js
+++ All/1st-unique-char-string.js
@@ -2,3 +2,11 @@ let s = 'aabbc';
 
 let firstUniqChar = function (s) {
   let map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === undefined) {
+        map[s[i]] = true
+    } else {
+        map[s[i]] = false
+    }
+  }
