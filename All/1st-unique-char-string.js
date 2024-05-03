fd614ba6d8aaf22c8a8932b8016c1a84ee242bca
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-03 22:24:35 +0800

    Add algo

--- All/1st-unique-char-string.js
+++ All/1st-unique-char-string.js
@@ -28,4 +28,22 @@ console.log(firstUniqChar(s));
 
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
+const binarySearch = (arr, value) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    
+    if (value === arr[middle] ) return middle;
+
+    if (value < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return -1;
+};
+console.log(binarySearch(arr, 11));
 
