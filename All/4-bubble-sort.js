ecde51b39590d715ceed7c1eee037833651d7d12
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-04 22:26:26 +0800

    Add algo

--- All/4-bubble-sort.js
+++ All/4-bubble-sort.js
@@ -3,3 +3,7 @@
 const arr = [9,8,7,6,5,4,3,2,1,1,0,-1,8,9,-2,-3];
 
 
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if(arr[j+1] === undefined) console.log('1 !!!')
