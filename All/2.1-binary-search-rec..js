1a55ed749658b246278d1e5ca325ad589658b335
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-12 20:17:59 +0800

    Add algo

--- All/2.1-binary-search-rec..js
+++ All/2.1-binary-search-rec..js
@@ -3,3 +3,12 @@
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let start = 0;
 let end = arr.length -1;
+
+const binarySearchRec = (arr, value, start, end) => {
+
+  if (start > end) return -1;
+
+  let middle = Math.floor((start + end) / 2);
+
+  if (value === arr[middle]) return middle;
+
