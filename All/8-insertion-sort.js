d2472e5b7cd927e897d9d870a119b3a3e5737ba9
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-12 20:18:39 +0800

    Add algo

--- All/8-insertion-sort.js
+++ All/8-insertion-sort.js
@@ -5,3 +5,13 @@ function insertionSort(arr) {
     let current = arr[i];
     let j = i - 1;
 
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(insertionSort(arr));
