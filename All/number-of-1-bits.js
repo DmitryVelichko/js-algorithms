97ea08a05aa9c43154160e69be7192b9e06ec83d
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-13 20:51:56 +0800

    Add algo

--- All/number-of-1-bits.js
+++ All/number-of-1-bits.js
@@ -42,3 +42,19 @@
  * @return {number}
  */
 
+var hammingWeight = function(n) {
+
+    let count = 0;
+    
+    while(n !== 0) {
+        
+        let isOne = n & 1;
+        if(isOne === 1) count++
+        
+        n = n >>> 1
+    }
+    return count
+
+};
+
+console.log(hammingWeight(00110111))
