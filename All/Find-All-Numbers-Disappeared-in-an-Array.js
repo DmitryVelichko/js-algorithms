1a2cd33861bde9c5e9825bb8fbfb189a1afcc6fe
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-14 20:54:21 +0800

    Add algo

--- All/Find-All-Numbers-Disappeared-in-an-Array.js
+++ All/Find-All-Numbers-Disappeared-in-an-Array.js
@@ -15,3 +15,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+var findDisappearedNumbers = function(nums) {
+    const arr = [];
+    for(let i=1; i<=nums.length; i++) {
+        if(!(nums.includes(i))){
+            arr.push(i);
+        }
+    }
+    return arr;
+ };
