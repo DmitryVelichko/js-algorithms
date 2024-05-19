301f2e40b3a2cc5121351b0994fecf97396676d8
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-19 13:38:52 +0800

    Add algo

--- All/contains-duplicate.js
+++ All/contains-duplicate.js
@@ -17,3 +17,12 @@ console.log(containsDuplicate([1,2,3,4,1]))
 // let containsDuplicate2 = function(nums) {
 //     nums.sort()
 //     for(let i = 0; i < nums.length; i++) {
+
+//         if(nums[i] === nums[i+1]) {
+//             return true
+//         }
+//     }
+//     return false
+// };
+
+// console.log(containsDuplicate2([2,3,4,1]))
