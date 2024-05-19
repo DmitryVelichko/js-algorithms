fa3ae7e36226db5f6587fe7e34ceee6956ca3c11
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-19 13:38:35 +0800

    Add algo

--- All/contains-duplicate.js
+++ All/contains-duplicate.js
@@ -5,3 +5,12 @@ let containsDuplicate = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         if(nums[i] in map) {
             return true
+        } else {
+            map[nums[i]] = 1
+        }
+    }
+    return false
+};
+
+console.log(containsDuplicate([1,2,3,4,1]))
+
