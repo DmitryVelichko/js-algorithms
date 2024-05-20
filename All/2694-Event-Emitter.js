014743b424a160ea5dd9ade1d329fc829948ccc6
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-20 11:34:06 +0800

    Add algo

---
+++ All/2694-Event-Emitter.js
@@ -0,0 +1,7 @@
+// 2694. Event Emitter
+// Medium
+
+// Design an EventEmitter class. This interface is similar (but with some differences) to the one found in Node.js or the Event Target interface of the DOM. The EventEmitter should allow for subscribing to events and emitting them.
+
+// Your EventEmitter class should have the following two methods:
+
