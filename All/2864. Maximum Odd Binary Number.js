bcaf84ec5227c0edfb96a2a59e8c2a43b9e3e190
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-25 13:26:45 +0300

    Add algo

--- All/2864. Maximum Odd Binary Number.js
+++ All/2864. Maximum Odd Binary Number.js
@@ -26,3 +26,16 @@
 // Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
  
 
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists only of '0' and '1'.
+// s contains at least one '1'.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maximumOddBinaryNumber = function(s) {
+    let arr = s.split('').sort().reverse()
+
