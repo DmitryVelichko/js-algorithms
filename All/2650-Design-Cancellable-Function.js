8a67637ab5def1e648e9fe0725b1926ff5e84acf
Dmitry Velichko <soleil-2005@mail.ru>
2024-05-27 22:09:21 +0300

    Add algo

--- All/2650-Design-Cancellable-Function.js
+++ All/2650-Design-Cancellable-Function.js
@@ -23,3 +23,30 @@
 
  
 
+// Example 1:
+
+// Input: 
+// generatorFunction = function*() { 
+//   return 42; 
+// }
+// cancelledAt = 100
+// Output: {"resolved": 42}
+// Explanation:
+// const generator = generatorFunction();
+// const [cancel, promise] = cancellable(generator);
+// setTimeout(cancel, 100);
+// promise.then(console.log); // resolves 42 at t=0ms
+
+// The generator immediately yields 42 and finishes. Because of that, the returned promise immediately resolves 42. Note that cancelling a finished generator does nothing.
+// Example 2:
+
+// Input:
+// generatorFunction = function*() { 
+//   const msg = yield new Promise(res => res("Hello")); 
+//   throw `Error: ${msg}`; 
+// }
+// cancelledAt = null
+// Output: {"rejected": "Error: Hello"}
+// Explanation:
+// A promise is yielded. The function handles this by waiting for it to resolve and then passes the resolved value back to the generator. Then an error is thrown which has the effect of causing the promise to reject with the same thrown error.
+// Example 3:
