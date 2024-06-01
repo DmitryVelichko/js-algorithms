6a6c2635c212cd7dbaeb636699f5ca5abf565c12
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-01 11:40:51 +0300

    Add algo

--- All/2722. Join Two Arrays by ID.js
+++ All/2722. Join Two Arrays by ID.js
@@ -64,3 +64,33 @@
 // Explanation: The two objects with id=1 are merged together. For the keys "b" and "v" the values from arr2 are used. Since the key "y" only exists in arr1, that value is taken form arr1.
  
 
+// Constraints:
+
+// arr1 and arr2 are valid JSON arrays
+// Each object in arr1 and arr2 has a unique integer id key
+// 2 <= JSON.stringify(arr1).length <= 106
+// 2 <= JSON.stringify(arr2).length <= 106
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+  const combinedArray = arr1.concat(arr2);
+  const merged = {};
+
+  combinedArray.forEach((obj) => {
+    const id = obj.id;
+    if (!merged[id]) {
+      merged[id] = { ...obj };
+    } else {
+      merged[id] = { ...merged[id], ...obj };
+    }
+  });
+
+  const joinedArray = Object.values(merged);
+  joinedArray.sort((a, b) => a.id - b.id);
+
+  return joinedArray;
+};
