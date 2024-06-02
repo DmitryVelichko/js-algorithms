584cd605900e442debf858a2476b8d37af0809b4
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-02 10:38:59 +0300

    Add algo

--- All/2705. Compact Object.js
+++ All/2705. Compact Object.js
@@ -16,3 +16,13 @@
 // Output: [1]
 // Explanation: All falsy values have been removed from the array.
 // Example 2:
+
+// Input: obj = {"a": null, "b": [false, 1]}
+// Output: {"b": [1]}
+// Explanation: obj["a"] and obj["b"][0] had falsy values and were removed.
+// Example 3:
+
+// Input: obj = [null, 0, 5, [0], [false, 16]]
+// Output: [5, [], [16]]
+// Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
+ 
