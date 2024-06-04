3bc524d42cbe771a9512e02b774fe9ac455a26dc
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-04 10:41:39 +0300

    Add algo

---
+++ All/2870. Minimum Number of Operations to Make Array Empty.js
@@ -0,0 +1,8 @@
+// 2870. Minimum Number of Operations to Make Array Empty
+// Medium
+// 293
+// 16
+// Companies
+// You are given a 0-indexed array nums consisting of positive integers.
+
+// There are two types of operations that you can apply on the array any number of times:
