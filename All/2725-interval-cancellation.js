4af5051d1cf6c974ad4f8074127060d96f177316
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-11 18:04:54 +0300

    Add algo

--- All/2725-interval-cancellation.js
+++ All/2725-interval-cancellation.js
@@ -7,3 +7,19 @@
 
  
 
+// Example 1:
+
+// Input: fn = (x) => x * 2, args = [4], t = 35, cancelT = 190
+// Output: 
+// [
+//    {"time": 0, "returned": 8},
+//    {"time": 35, "returned": 8},
+//    {"time": 70, "returned": 8},
+//    {"time": 105, "returned": 8},
+//    {"time": 140, "returned": 8},
+//    {"time": 175, "returned": 8}
+// ]
+// Explanation: 
+// const cancel = cancellable(x => x * 2, [4], 35);
+// setTimeout(cancel, 190);
+
