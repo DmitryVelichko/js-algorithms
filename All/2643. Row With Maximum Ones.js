98d9ca7adca4f4c5198218a502cd6686aa45694d
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-12 11:18:19 +0300

    Add algo

---
+++ All/2643. Row With Maximum Ones.js
@@ -0,0 +1,6 @@
+// 2643. Row With Maximum Ones
+// Easy
+// 374
+// 11
+// Companies
+// Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count of ones, and the number of ones in that row.
