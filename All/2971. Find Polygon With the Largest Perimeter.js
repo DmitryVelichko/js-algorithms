266d85236d8d3dc7f7e6c3b136ca9345198f4501
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-13 10:48:46 +0300

    Add algo

--- All/2971. Find Polygon With the Largest Perimeter.js
+++ All/2971. Find Polygon With the Largest Perimeter.js
@@ -44,3 +44,15 @@
  * @param {number[]} nums
  * @return {number}
  */
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b);
+    let previousElementsSum = 0;
+    let ans = -1;
+    for (let num of nums) {
+        if (num < previousElementsSum) {
+            ans = num + previousElementsSum;
+        }
+        previousElementsSum += num;
+    }
+    return ans;
+};
