cc0e813044410a307daf962a79877d1325c7c800
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-14 11:24:16 +0300

    Add algo

--- All/2722. Join Two Arrays by ID.js
+++ All/2722. Join Two Arrays by ID.js
@@ -71,3 +71,26 @@
 // 2 <= JSON.stringify(arr1).length <= 106
 // 2 <= JSON.stringify(arr2).length <= 106
 
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+  const combinedArray = arr1.concat(arr2);
+  const merged = {};
+
+  combinedArray.forEach((obj) => {
+    const id = obj.id;
+    if (!merged[id]) {
+      merged[id] = { ...obj };
+    } else {
+      merged[id] = { ...merged[id], ...obj };
+    }
+  });
+
+  const joinedArray = Object.values(merged);
+  joinedArray.sort((a, b) => a.id - b.id);
+
+  return joinedArray;
+};
