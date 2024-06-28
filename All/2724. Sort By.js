0d2974dc0d20397d39fda0c1496878792ff8b747
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-28 14:05:12 +0300

    Add algo

---
+++ All/2724. Sort By.js
@@ -0,0 +1,4 @@
+// 2724. Sort By
+// Easy
+
+// Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. sortedArray must be sorted in ascending order by fn output.
