2260c0f8cefa1f4f3ff907e5da727eb1a3d09f7a
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-29 10:33:37 +0300

    Add algo

--- All/2694-Event-Emitter.js
+++ All/2694-Event-Emitter.js
@@ -111,3 +111,15 @@ class EventEmitter {
     }
   }
   
+  
+  /**
+   * const emitter = new EventEmitter();
+   *
+   * // Subscribe to the onClick event with onClickCallback
+   * function onClickCallback() { return 99 }
+   * const sub = emitter.subscribe('onClick', onClickCallback);
+   *
+   * emitter.emit('onClick'); // [99]
+   * sub.unsubscribe(); // undefined
+   * emitter.emit('onClick'); // []
+   */
