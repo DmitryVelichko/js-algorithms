7f6169ae45bc147e31c618ab7f7c11a35136c3a0
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-30 14:58:10 +0300

    Add algo

--- All/2648-Generate-Fibonacci-Sequence.js
+++ All/2648-Generate-Fibonacci-Sequence.js
@@ -16,3 +16,16 @@
 // Input: callCount = 5
 // Output: [0,1,1,2,3]
 // Explanation:
+// const gen = fibGenerator();
+// gen.next().value; // 0
+// gen.next().value; // 1
+// gen.next().value; // 1
+// gen.next().value; // 2
+// gen.next().value; // 3
+// Example 2:
+
+// Input: callCount = 0
+// Output: []
+// Explanation: gen.next() is never called so nothing is outputted
+ 
+
