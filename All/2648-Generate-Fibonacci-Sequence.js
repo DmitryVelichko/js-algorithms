915528fc31f450604088d85133611e46a4382539
Dmitry Velichko <soleil-2005@mail.ru>
2024-06-30 14:58:05 +0300

    Add algo

--- All/2648-Generate-Fibonacci-Sequence.js
+++ All/2648-Generate-Fibonacci-Sequence.js
@@ -4,3 +4,15 @@
 // 15
 // Companies
 // Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+ 
+
+// Example 1:
+
+// Input: callCount = 5
+// Output: [0,1,1,2,3]
+// Explanation:
