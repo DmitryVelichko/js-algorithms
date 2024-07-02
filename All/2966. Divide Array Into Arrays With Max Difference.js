c2e324d228912ad7347c7c7ce77edd73bd1b4dfd
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-02 15:37:46 +0300

    Add algo

--- All/2966. Divide Array Into Arrays With Max Difference.js
+++ All/2966. Divide Array Into Arrays With Max Difference.js
@@ -26,3 +26,18 @@
 // Input: nums = [1,3,3,2,7,3], k = 3
 // Output: []
 // Explanation: It is not possible to divide the array satisfying all the conditions.
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// n is a multiple of 3.
+// 1 <= nums[i] <= 105
+// 1 <= k <= 105
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
