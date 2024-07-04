304d77b58d94b41273461da98ec0ab9b23c504bf
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-04 23:57:31 +0300

    Add algo

---
+++ All/2693-call-function-with-custom-context.js
@@ -0,0 +1,5 @@
+// 2693. Call Function with Custom Context
+// Medium
+
+// Enhance all functions to have the callPolyfill method. The method accepts an object obj as it's first parameter and any number of additional arguments. The obj becomes the this context for the function. The additional arguments are passed to the function (that the callPolyfill method belongs on).
+
