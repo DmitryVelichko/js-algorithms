a07a586cb0e08d39e6dd00aa9af70b034eb911e5
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-05 23:53:46 +0300

    Add game

---
+++ All/2966. Divide Array Into Arrays With Max Difference.js
@@ -0,0 +1,10 @@
+// 2966. Divide Array Into Arrays With Max Difference
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given an integer array nums of size n and a positive integer k.
+
+// Divide the array into one or more arrays of size 3 satisfying the following conditions:
+
