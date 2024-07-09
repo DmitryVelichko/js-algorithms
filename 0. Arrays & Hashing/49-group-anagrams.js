03eae68b5ca0000e5b71e0c3ecbfb6f999f19fca
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-09 19:32:52 +0300

    Add algo

--- 0. Arrays & Hashing/49-group-anagrams.js
+++ 0. Arrays & Hashing/49-group-anagrams.js
@@ -10,3 +10,18 @@
 // Input: strs = ["eat","tea","tan","ate","nat","bat"]
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 // Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+
+// Constraints:
+
+// 1 <= strs.length <= 104
+// 0 <= strs[i].length <= 100
+// strs[i] consists of lowercase English letters.
+
