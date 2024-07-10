d591606e39fa5f888181d7b53de7cdfec519390d
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-10 21:28:54 +0300

    Add algo

--- All/playground.js
+++ All/playground.js
@@ -4,3 +4,18 @@ const trigger = function (obj) {
     obj._hidden = new Set(); // Initialize outside the loop
     obj._hiddenValues = {};
   
+    return function () {
+      if (obj._hidden) {
+        for (const prop of obj._hidden) {
+          obj[prop] = obj._hiddenValues[prop];
+          hiddenProps.delete(prop);
+        }
+        delete obj._hidden;
+        delete obj._hiddenValues;
+      } else {
+        for (const prop in obj) {
+          if (!prop.startsWith('$') && !hiddenProps.has(prop)) {
+            hiddenProps.add(prop);
+            obj._hidden.add(prop);
+            obj._hiddenValues[prop] = obj[prop];
+            delete obj[prop];
