6ed0aefd7e33821ed12bb56e92792694e68e3993
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-13 22:48:19 +0300

    Add algo

--- 0. Arrays & Hashing/128-longest-consecutive-seq.js
+++ 0. Arrays & Hashing/128-longest-consecutive-seq.js
@@ -31,3 +31,13 @@
 // С помощью цилка for of ищем в сете минимальный элемент num-1
 // во вложенном цикле while пока в сете есть num+1 обновляем currMax и currNum
 // Вычисляем максимальный элемент (max, currMax) и возвращаем его
+var longestConsecutive = function (nums) {
+    let max = 0
+    const set = new Set(nums)
+
+    // Проходимся по сету и находим наименьший элемент num-1
+    for (let num of set) {
+        if (set.has(num - 1)) continue
+        let currNum = num
+        let currMax = 1
+        // проходимся по сету пока там есть currNum+1
