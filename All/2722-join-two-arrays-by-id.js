e42740aa02d374be8ee7e4c164061c48de1e8631
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-14 16:38:31 +0300

    Add algo

--- All/2722-join-two-arrays-by-id.js
+++ All/2722-join-two-arrays-by-id.js
@@ -21,3 +21,21 @@
 // arr2 = [
 //     {"id": 3, "x": 5}
 // ]
+// Output: 
+// [
+//     {"id": 1, "x": 1},
+//     {"id": 2, "x": 9},
+//     {"id": 3, "x": 5}
+// ]
+// Explanation: There are no duplicate ids so arr1 is simply concatenated with arr2.
+// Example 2:
+
+// Input: 
+// arr1 = [
+//     {"id": 1, "x": 2, "y": 3},
+//     {"id": 2, "x": 3, "y": 6}
+// ], 
+// arr2 = [
+//     {"id": 2, "x": 10, "y": 20},
+//     {"id": 3, "x": 0, "y": 0}
+// ]
