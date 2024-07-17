b88b498d01b3b02f665792c33ec50279c3a35da6
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-17 23:37:06 +0300

    Add algo

--- JS Interview Questions/outsource-company.js
+++ JS Interview Questions/outsource-company.js
@@ -11,3 +11,27 @@ const obj = {
 const getName = obj.getName;
 getName(); // res ?
 
+/* 2 */
+
+let x = "outer value";
+if (true){
+      let x = "inner value";
+  console.log(x);
+}
+
+/* 3 */
+const user = {
+  name: "Bob",
+  userThis: this,
+  func() {
+    console.log(this);
+  },
+  arrowFunc: () => {
+    console.log(this);
+  }
+};
+
+console.log(user.userThis); // undefined
+user.func(); // ? obj user
+user.arrowFunc(); // ? undefined
+
