637f3b211ad9c18def62155b80d8a1aad76773f0
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-22 17:57:06 +0300

    Add algo

--- 1. Two Pointers/125-valid-palindrome.js
+++ 1. Two Pointers/125-valid-palindrome.js
@@ -65,5 +65,5 @@
 
 // var isPalindrome = function(s) {
 //     let newStr = s.replace(/[^a-z0-9]/gi,"").toLowerCase();
-//     return newStr.split("").reverse().join("") === newStr ? true : false;
+//     return newStr.split("").reverse().join("") === newStr;
 // };
