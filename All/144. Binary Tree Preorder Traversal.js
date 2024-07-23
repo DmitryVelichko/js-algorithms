c2f85ea0c8655b4abf835406286aab6020e93e5e
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-23 09:00:17 +0300

    Add algo

--- All/144. Binary Tree Preorder Traversal.js
+++ All/144. Binary Tree Preorder Traversal.js
@@ -31,3 +31,15 @@
 
 // Follow up: Recursive solution is trivial, could you do it iteratively?
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
