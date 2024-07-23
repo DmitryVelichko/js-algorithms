eddc0d4946fa42ec1e0391da7b91123f553d3a0a
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-23 09:00:22 +0300

    Add algo

--- All/144. Binary Tree Preorder Traversal.js
+++ All/144. Binary Tree Preorder Traversal.js
@@ -43,3 +43,16 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+var preorderTraversal = function(root) {
+    if(!root) return []
+    let res = []
+    
+    function traverse(node) {
+        res.push(node.val)
+        if(node.left) traverse(node.left)
+        if(node.right) traverse(node.right)
+       
+    } 
+    traverse(root)
+    return res
+};
