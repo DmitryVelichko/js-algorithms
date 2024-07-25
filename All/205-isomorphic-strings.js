425a5c2d5f8d1d4f273ca7f5d432ec1f37cb6ef1
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-25 13:12:36 +0300

    Add algo

--- All/205-isomorphic-strings.js
+++ All/205-isomorphic-strings.js
@@ -33,3 +33,24 @@
  * @param {string} t
  * @return {boolean}
  */
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length) return false
+    let sMap = {}
+    let tMap = {}
+    for(let i = 0; i<s.length; i++) {
+        sChar = s[i]
+        tChar = t[i]
+
+        if(sMap[sChar] === undefined) {
+            sMap[sChar] = tChar;
+        }
+
+        if(tMap[tChar] === undefined) {
+            tMap[tChar] = sChar;
+        }
+
+        if(sMap[sChar] !== tChar || tMap[tChar] !== sChar) return false
+    }
+
+    return true
+};
