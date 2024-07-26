4177290e8b7366aa60d8af091f93468f27985bf3
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-26 12:50:32 +0300

    Add algo

--- All/205-isomorphic-strings.js
+++ All/205-isomorphic-strings.js
@@ -14,3 +14,17 @@
 // Output: true
 // Example 2:
 
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 104
+// t.length == s.length
+// s and t consist of any valid ascii character.
+
