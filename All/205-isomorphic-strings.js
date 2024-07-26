9aa139dcaeb4191097b1d31e9ed5bbb32a16141d
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-26 12:50:36 +0300

    Add algo

--- All/205-isomorphic-strings.js
+++ All/205-isomorphic-strings.js
@@ -28,3 +28,29 @@
 // t.length == s.length
 // s and t consist of any valid ascii character.
 
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length) return false
+    let sMap = {}
+    let tMap = {}
+    for(let i = 0; i<s.length; i++) {
+        sChar = s[i]
+        tChar = t[i]
+
+        if(sMap[sChar] === undefined) {
+            sMap[sChar] = tChar;
+        }
+
+        if(tMap[tChar] === undefined) {
+            tMap[tChar] = sChar;
+        }
+
+        if(sMap[sChar] !== tChar || tMap[tChar] !== sChar) return false
+    }
+
+    return true
+};
