7d1a5326467422d3d415d42514fd0f6d43f80c66
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-26 12:50:20 +0300

    Add algo

---
+++ All/205-isomorphic-strings.js
@@ -0,0 +1,6 @@
+// 205. Isomorphic Strings
+// Easy
+
+// Given two strings s and t, determine if they are isomorphic.
+
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
