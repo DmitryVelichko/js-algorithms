b0e672ccbaf746e814957791decfbe8ca2302898
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-28 17:24:20 +0300

    Add algo

--- All/142-linked-list-cycle-II.js
+++ All/142-linked-list-cycle-II.js
@@ -15,3 +15,17 @@
 // Explanation: There is a cycle in the linked list, where tail connects to the second node.
 // Example 2:
 
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+// Example 3:
+
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+
+// Constraints:
+
+// The number of the nodes in the list is in the range [0, 104].
+// -105 <= Node.val <= 105
+// pos is -1 or a valid index in the linked-list.
