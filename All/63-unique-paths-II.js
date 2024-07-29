28c6f692e6abec27e7c00a35a2cf756ba2bf60ae
Dmitry Velichko <soleil-2005@mail.ru>
2024-07-29 21:44:50 +0300

    Add algo

--- All/63-unique-paths-II.js
+++ All/63-unique-paths-II.js
@@ -30,3 +30,12 @@
 
 // Constraints:
 
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] is 0 or 1.
+
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
