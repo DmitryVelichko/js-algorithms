9bec4ee022ba2393876e206c458c55ea8ba7c1f8
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-01 21:51:41 +0300

    Add algo

--- All/232. Implement Queue using Stacks.js
+++ All/232. Implement Queue using Stacks.js
@@ -14,3 +14,6 @@
 // boolean empty() Returns true if the queue is empty, false otherwise.
 // Notes:
 
+// You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+// Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
