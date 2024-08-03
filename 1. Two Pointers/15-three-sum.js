2c530ab6bb55819f143c7c979e42fcf853132c0a
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-03 15:33:00 +0300

    Add algo

--- 1. Two Pointers/15-three-sum.js
+++ 1. Two Pointers/15-three-sum.js
@@ -54,3 +54,22 @@
         let l = i + 1;
         let r = nums.length - 1;
 
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r];
+            if (threeSum > 0) {
+                r--;
+            } else if (threeSum < 0) {
+                l++;
+            } else {
+                res.push([a, nums[l], nums[r]]);
+                l++;
+                r--;
+                while (nums[l] === nums[l - 1] && l < r) {
+                    l++;
+                }
+            }
+        }
+    }
+
+    return res;
+}
