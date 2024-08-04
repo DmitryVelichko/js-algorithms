2e01bbeefa0c03ac7101bdde6e4dbb1c74fd0f42
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-04 10:20:01 +0300

    Add algo

--- 2. Stack/11-container-with-most-water.js
+++ 2. Stack/11-container-with-most-water.js
@@ -41,3 +41,15 @@ var maxArea = function (height) {
     let res = 0;
     let l = 0;
     let r = height.length - 1;
+    while (l < r) {
+        let area = (r - l) * Math.min(height[l], height[r]);
+        res = Math.max(res, area)
+        if (height[l] < height[r]) {
+            l++;
+        } else {
+            r--
+        }
+    }
+
+    return res;
+};
