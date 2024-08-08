2ecf82d5abb0e765c9dd63d27f8cff1d8fd5ca6b
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-08 23:17:16 +0300

    Add algo

--- All/7-merge-sort.js
+++ All/7-merge-sort.js
@@ -29,3 +29,13 @@ function mergesort(arr) {
     const rightArr = arr.slice(mid)
     return merge(mergesort(leftArr), mergesort(rightArr))
   }
+  
+  function merge(leftArr, rightArr) {
+    const sortedArr = []
+    while (leftArr.length && rightArr.length) {
+      if (leftArr[0] <= rightArr[0]) {
+        sortedArr.push(leftArr.shift())
+      } else {
+        sortedArr.push(rightArr.shift())
+      }
+    }
