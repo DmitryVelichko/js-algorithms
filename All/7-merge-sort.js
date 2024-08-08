6967b676f6c035390e5d5808e50acde045ba94bd
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-08 23:17:00 +0300

    Add algo

--- All/7-merge-sort.js
+++ All/7-merge-sort.js
@@ -3,3 +3,15 @@
 
 const arr = [-5, 23, 7, 5, 3, -12, -29, 21, 54, 35, 0];
 
+function merge(left, right) {
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
