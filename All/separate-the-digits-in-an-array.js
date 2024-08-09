0ec62a915d67b54b26a0cfd7ec010c4d3e355bff
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-09 15:30:24 +0300

    Add algo

--- All/separate-the-digits-in-an-array.js
+++ All/separate-the-digits-in-an-array.js
@@ -16,3 +16,16 @@
 // - The separation of 83 is [8,3].
 // - The separation of 77 is [7,7].
 // answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+// Example 2:
+
+// Input: nums = [7,1,3,9]
+// Output: [7,1,3,9]
+// Explanation: The separation of each integer in nums is itself.
+// answer = [7,1,3,9].
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 105
+
