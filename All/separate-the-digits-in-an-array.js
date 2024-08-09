33c09867f029ad3dd36ec4f052cee6a183d0e9f7
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-09 15:30:08 +0300

    Add algo

---
+++ All/separate-the-digits-in-an-array.js
@@ -0,0 +1,3 @@
+// 2553. Separate the Digits in an Array
+
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
