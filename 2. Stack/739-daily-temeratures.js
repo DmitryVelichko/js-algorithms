3f087219ff28c428380e6a6310b87a0bbcdaec3d
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-11 12:54:00 +0300

    Add algo

--- 2. Stack/739-daily-temeratures.js
+++ 2. Stack/739-daily-temeratures.js
@@ -2,3 +2,10 @@
 // Medium
 // 10.8K
 // 242
+// Companies
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+// Example 1:
+
