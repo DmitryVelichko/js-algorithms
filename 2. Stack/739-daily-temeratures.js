1ca211817ba1886b7b9952b809347ea39fac4ac1
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-11 12:54:19 +0300

    Add algo

--- 2. Stack/739-daily-temeratures.js
+++ 2. Stack/739-daily-temeratures.js
@@ -34,3 +34,12 @@
 var dailyTemperatures = function(temperatures) {
     let stack = [];
     let result = new Array(temperatures.length).fill(0);
+     for(let i=0; i < temperatures.length; i++){
+         while(stack.length && temperatures[i] > temperatures[stack[stack.length-1]]){
+             let index = stack.pop();
+             result[index] = i-index;   
+         }
+         stack.push(i)
+     }
+     return result;
+ };
