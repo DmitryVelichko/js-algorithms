be3fc318efff8843857f4abb55cbcc3b4b7c84c2
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-16 22:03:13 +0300

    Add algo

--- All/290-word-pattern.js
+++ All/290-word-pattern.js
@@ -4,3 +4,16 @@
 
 // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
+// Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+// Example 2:
+
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+// Example 3:
+
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+ 
