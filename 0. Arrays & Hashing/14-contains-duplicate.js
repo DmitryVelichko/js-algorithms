2bf7dbc2a42fae4bd5dc8e21f87379014ef0aff9
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-17 13:58:12 +0300

    Add algo

--- 0. Arrays & Hashing/14-contains-duplicate.js
+++ 0. Arrays & Hashing/14-contains-duplicate.js
@@ -3,17 +3,3 @@
 // Solution
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-// Time - O(n), space - O(n)
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var containsDuplicate = function(nums) {
-    // O(n), O(n)
-    let hash = {}
-    for(let num of nums) {
-        if(hash[num] !== undefined) return true
-        hash[num] = num
-    }
-    return false
-};
