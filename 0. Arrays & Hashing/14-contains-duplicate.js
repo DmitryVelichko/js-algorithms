eebb41c37eaf05ba0f301ee9f9a3aeeb356b1ae0
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-17 13:58:19 +0300

    Add algo

--- 0. Arrays & Hashing/14-contains-duplicate.js
+++ 0. Arrays & Hashing/14-contains-duplicate.js
@@ -3,3 +3,17 @@
 // Solution
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ /**
+ * Вернуть true если в массиве есть дубликат числа
+ *
+ * hashmap
+ *   В цикле проходимся по массиву
+ *       Число есть в hash? Возвращаем true
+ *      Добавляем число в хэш
+ *   return false
+ *
+  */
