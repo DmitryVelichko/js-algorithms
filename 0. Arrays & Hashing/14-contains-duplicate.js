43ba8b7dd14060f15a65fc21c6c84e7a33d784d8
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-17 13:58:54 +0300

    Add algo

--- 0. Arrays & Hashing/14-contains-duplicate.js
+++ 0. Arrays & Hashing/14-contains-duplicate.js
@@ -1,29 +1,45 @@
-// Contains Duplicate
-
-// Solution
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
 /**
- * @param {number[]} nums
+ * @param {string} s
+ * @param {string} t
  * @return {boolean}
  */
- /**
- * Вернуть true если в массиве есть дубликат числа
- *
- * hashmap
- *   В цикле проходимся по массиву
- *       Число есть в hash? Возвращаем true
- *      Добавляем число в хэш
- *   return false
- *
-  */
- var containsDuplicate = function(nums) {
-    // O(n), O(n)
-   
-   const hash = {}
-   for(let num of nums) {
-        if(hash[num] !== undefined) return true
-        hash[num] = num
-   }
-   return false
+
+/**
+    Если 2 строки s и t анаграммы возвращаем true, false если нет
+
+    Создаем hashmap
+
+    длина s и t разная? return false
+
+    Проходимся в цикле for по длине s
+        если буквы из s нет в хэше инициализируем нулем
+        if (hash[t[i]] === undefined) hash[t[i]] = 0
+
+        хэш[буква из s]++
+        хэш[буква из t]--
+    
+    Проходимся в цикле for по хэшу
+        if (hash[key] !==0) return false
+
+    return true
+
+ */
+
+// O(n), O(1)/O(k) - 24 буквы в алфавите
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false
+    const hash = {}
+
+    for (let i = 0; i < s.length; i++) {
+        if (hash[s[i]] === undefined) hash[s[i]] = 0
+        if (hash[t[i]] === undefined) hash[t[i]] = 0
+
+        hash[s[i]]++
+        hash[t[i]]--
+    }
+
+    for (let key in hash) {
+        if (hash[key] !== 0) return false
+    }
+    return true
 };
