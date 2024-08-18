f9ba30cfbf871e3b6b80f6cf5e3609273bceb2da
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-18 11:31:00 +0300

    Add algo

--- 3. Binary Search/153-find-minimum-in-rotated-sorted-arr.js
+++ 3. Binary Search/153-find-minimum-in-rotated-sorted-arr.js
@@ -10,3 +10,13 @@
 
 compare middle val and save to result
 
+ l    m        r
+ [3,4,5,     1,2]
+ middle > left value? Searching in right sorted arr
+ else searcing in left sorted arr
+ */
+ var findMin = function (nums) {
+    let res = nums[0]
+    let l = 0
+    let r = nums.length - 1
+
