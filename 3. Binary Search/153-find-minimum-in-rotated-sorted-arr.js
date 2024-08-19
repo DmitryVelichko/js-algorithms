73aff4fa5ea7984cf497e354c066a6113ba3d28a
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-19 23:43:47 +0300

    Add algo

--- 3. Binary Search/153-find-minimum-in-rotated-sorted-arr.js
+++ 3. Binary Search/153-find-minimum-in-rotated-sorted-arr.js
@@ -2,3 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
+// O(log n), O(1)
+// binary search 2 pointers
+
+/**
+ Is array sorted? return smallest val to the left
+
+compare middle val and save to result
+
