e55c093bec61708b17ac950e72299865e0630542
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-22 23:17:06 +0300

    Add algo

--- All/268. Missing Number.js
+++ All/268. Missing Number.js
@@ -24,3 +24,13 @@
 // Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
  
 
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 104
+// 0 <= nums[i] <= n
+// All the numbers of nums are unique.
+ 
+
+// Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
