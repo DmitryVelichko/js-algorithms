aad37f17921ff73b0472721a82ab4852b8526602
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-23 22:25:02 +0300

    Add algo

--- All/171-Excel-Sheet-Column-Number.js
+++ All/171-Excel-Sheet-Column-Number.js
@@ -25,3 +25,14 @@
 // Output: 28
 // Example 3:
 
+// Input: columnTitle = "ZY"
+// Output: 701
+ 
+
+// Constraints:
+
+// 1 <= columnTitle.length <= 7
+// columnTitle consists only of uppercase English letters.
+// columnTitle is in the range ["A", "FXSHRXW"].
+
+
