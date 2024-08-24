23a566d6a1a9f5140c52949329474db6b3efd632
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-24 17:45:36 +0300

    Add algo

--- 0. Arrays & Hashing/36-valid-sudoku.js
+++ 0. Arrays & Hashing/36-valid-sudoku.js
@@ -54,24 +54,40 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-// O(9**2), O(9**2)
-var isValidSudoku = function (board) {
-    const set = new Set()
+ /**
+ * 
+ new Set()
+ for
+    for(j<board[i].length)
+    cell === '.' ? continue
 
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            const cell = board[i][j]
-            if (cell === '.') continue
-            const row = `row: ${i}, value: ${cell}`
-            const col = `col: ${j}, value: ${cell}`
-            const box = `box: ${Math.floor(i / 3)}, ${Math.floor(j / 3)} value: ${cell}`
+    row = '${row}, ${cell}'
+    col = '${col}, ${cell}'
+    box = '${i/3} ${j/3}, ${cell}'
 
-            if (set.has(row) || set.has(col) || set.has(box)) return false
+    if(set.has(row/col/box)) false
+    set.add(row/col/box)
+return true
 
-            set.add(row)
-            set.add(col)
-            set.add(box)
+ */
+ // O(9**2), O(9**2) {
+    var isValidSudoku = function (board) {
+        const set = new Set()
+    
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const cell = board[i][j]
+                if (cell === '.') continue
+                const row = `row: ${i}, value: ${cell}`
+                const col = `col: ${j}, value: ${cell}`
+                const box = `box: ${Math.floor(i / 3)}, ${Math.floor(j / 3)} value: ${cell}`
+    
+                if (set.has(row) || set.has(col) || set.has(box)) return false
+    
+                set.add(row)
+                set.add(col)
+                set.add(box)
+            }
         }
-    }
-    return true
-};
+        return true
+    };
