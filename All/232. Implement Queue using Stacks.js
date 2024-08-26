02198ed1c210e654b19b9b998998e8916df8beca
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-26 20:51:29 +0300

    Add algo

--- All/232. Implement Queue using Stacks.js
+++ All/232. Implement Queue using Stacks.js
@@ -82,3 +82,18 @@ MyQueue.prototype.peek = function() {
     return this.stack2[this.stack2.length - 1];
 };
 
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
