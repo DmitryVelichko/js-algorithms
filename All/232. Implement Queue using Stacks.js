6825041bcef69919436085dae515409975c064b5
Dmitry Velichko <soleil-2005@mail.ru>
2024-08-26 20:50:54 +0300

    Add algo

--- All/232. Implement Queue using Stacks.js
+++ All/232. Implement Queue using Stacks.js
@@ -25,3 +25,20 @@
 // [[], [1], [2], [], [], []]
 // Output
 // [null, null, null, 1, 1, false]
+
+// Explanation
+// MyQueue myQueue = new MyQueue();
+// myQueue.push(1); // queue is: [1]
+// myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+// myQueue.peek(); // return 1
+// myQueue.pop(); // return 1, queue is [2]
+// myQueue.empty(); // return false
+ 
+
+// Constraints:
+
+// 1 <= x <= 9
+// At most 100 calls will be made to push, pop, peek, and empty.
+// All the calls to pop and peek are valid.
+ 
+
