486ae2d46a1d9a4e98aa8a4f3e3aa12f17a5e864
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-01 19:06:07 +0300

    Add algo

--- All/76. Minimum Window Substring.js
+++ All/76. Minimum Window Substring.js
@@ -8,3 +8,19 @@
 // substring
 //  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
 
+// The testcases will be generated such that the answer is unique.
+
+ 
+
+// Example 1:
+
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+// Example 2:
+
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+// Example 3:
+
