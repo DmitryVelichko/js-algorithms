0c088640d0dda0ff71d8ae05705fc1e526a1f0ba
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-08 12:37:37 +0300

    Add algo

--- 4. Sliding Window/3-Longest-Substring-without.js
+++ 4. Sliding Window/3-Longest-Substring-without.js
@@ -41,3 +41,18 @@ add right char to set, update max string size
 */
 // O(n), O(n)
 
+var lengthOfLongestSubstring = function (s) {
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, set.size);
+    }
+    return max;
+};
