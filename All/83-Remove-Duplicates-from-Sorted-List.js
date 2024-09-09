bf160d5577e654d2fdfe7949659d058dbfdff5b9
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-09 23:04:48 +0300

    Add algo

--- All/83-Remove-Duplicates-from-Sorted-List.js
+++ All/83-Remove-Duplicates-from-Sorted-List.js
@@ -12,3 +12,18 @@
 
 // Input: head = [1,1,2,3,3]
 // Output: [1,2,3]
+
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
