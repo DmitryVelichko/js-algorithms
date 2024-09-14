53e54fdbcb356b3fecf59c2e902adc7fc1c0bff6
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-14 17:41:03 +0300

    Add algo

---
+++ All/1043. Partition Array for Maximum Sum.js
@@ -0,0 +1,7 @@
+// 1043. Partition Array for Maximum Sum
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has their values changed to become the maximum value of that subarray.
