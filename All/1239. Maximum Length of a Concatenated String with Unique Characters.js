5e017b97e4e150826b24f5fef3bfbb3e32a850b0
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-17 22:11:46 +0300

    Add algo

--- All/1239. Maximum Length of a Concatenated String with Unique Characters.js
+++ All/1239. Maximum Length of a Concatenated String with Unique Characters.js
@@ -5,3 +5,23 @@
 // Companies
 // Hint
 // You are given an array of strings arr. A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+
+// Return the maximum possible length of s.
+
+// A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+
+
+// Example 1:
+
+// Input: arr = ["un","iq","ue"]
+// Output: 4
+// Explanation: All the valid concatenations are:
+// - ""
+// - "un"
+// - "iq"
+// - "ue"
+// - "uniq" ("un" + "iq")
+// - "ique" ("iq" + "ue")
+// Maximum length is 4.
+// Example 2:
