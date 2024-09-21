e6dbc20e6e7dbeeeb3846565656c14995ee18284
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-21 22:50:50 +0300

    Add algo

--- All/201. Bitwise AND of Numbers Range.js
+++ All/201. Bitwise AND of Numbers Range.js
@@ -30,3 +30,13 @@
  * @param {number} right
  * @return {number}
  */
+var rangeBitwiseAnd = function(left, right) {
+
+    let count=0;
+    while(left!=right){
+        left>>=1;
+        right>>=1;
+        count++;
+    }
+    return left<< count;
+ };
