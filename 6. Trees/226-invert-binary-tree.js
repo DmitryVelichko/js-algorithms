fd1c4e95c77a44d55668c8b1f346e8b326092d90
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-22 20:11:01 +0300

    Add algo

--- 6. Trees/226-invert-binary-tree.js
+++ 6. Trees/226-invert-binary-tree.js
@@ -1,20 +1,3 @@
-// 226. Invert Binary Tree
-// Easy
-
-// Given the root of a binary tree, invert the tree, and return its root.
-
-// Example 1:
-
-// Input: root = [4,2,7,1,3,6,9]
-// Output: [4,7,2,9,6,3,1]
-// Example 2:
-
-// Input: root = [2,1,3]
-// Output: [2,3,1]
-// Example 3:
-
-// Input: root = []
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -23,51 +6,43 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-
-// Iterative Approach:
-// Time O(n), Space O(n)
-
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+// DFS recursive pre-order: O(n), O(h) h = height of the tree (for 3 nodes h = 2, for 1 node h = 0).
+// For a skewed tree space: O(n), balanced binary tree: O(log n)
 var invertTree = function (root) {
 
-    const stack = [root]
+    if (!root) return null
+
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+};
+
+
+// DFS iterative
+var invertTree2 = function (root) {
+    if (root === null) return null;
+
+    const stack = [root];
+
     while (stack.length > 0) {
-        let node = stack.pop()
-        if (node !== null) {
+        const node = stack.pop();
 
-            let hold = node.left
-            node.left = node.right
-            node.right = hold
+        [node.left, node.right] = [node.right, node.left];
 
-            stack.push(node.left)
-            stack.push(node.right)
-        }
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
     }
-    return root
+
+    return root;
 };
 
 
-// Recursive Approach:
-// Time O(n), Space O(h), h-height of the tree, n - number of nodes 
-
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree = function (root) {
-
-    function reverseNode(node) {
-        if (node === null) return
-        reverseNode(node.left)
-        reverseNode(node.right)
-
-        let hold = node.left
-        node.left = node.right
-        node.right = hold
-    }
-    reverseNode(root)
-    return root
-};
