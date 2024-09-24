9886ef57bafe262eb6d6543116a697047a9e53c1
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-24 23:24:40 +0300

    Add algo

--- 6. Trees/100-is-same-tree.js
+++ 6. Trees/100-is-same-tree.js
@@ -57,3 +57,11 @@
 // ! || ! || !== false
 // => (l,l) && (r,r)
 
+// O(p + q), O(h)
+var isSameTree = function (p, q) {
+
+    if (!p && !q) return true
+    if ((!p || !q) || (p.val !== q.val)) return false
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
