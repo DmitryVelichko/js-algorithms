75993d2ab3e442f822edf6f214e27d0bd25ddb1f
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-24 23:24:36 +0300

    Add algo

--- 6. Trees/100-is-same-tree.js
+++ 6. Trees/100-is-same-tree.js
@@ -44,3 +44,16 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// ! && ! true
+// ! || ! || !== false
+// => (l,l) && (r,r)
+
