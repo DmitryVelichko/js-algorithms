eeb791f0b76a115bad68a2e26d1151a35de6d7a0
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-25 21:55:16 +0300

    Add algo

--- 6. Trees/226-invert-binary-tree.js
+++ 6. Trees/226-invert-binary-tree.js
@@ -36,3 +36,22 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// DFS recursive pre-order: O(n), O(h) h = height of the tree (for 3 nodes h = 2, for 1 node h = 0).
+// For a skewed tree space: O(n), balanced binary tree: O(log n)
+var invertTree = function (root) {
+
+    if (!root) return null
+
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+};
