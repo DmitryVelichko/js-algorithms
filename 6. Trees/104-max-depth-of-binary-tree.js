a6fd9cb0f0d6dab9ae1f487708608c03ca7999d7
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-28 10:54:54 +0300

    Add algo

--- 6. Trees/104-max-depth-of-binary-tree.js
+++ 6. Trees/104-max-depth-of-binary-tree.js
@@ -13,3 +13,18 @@
 // 3 ways: DFS(recursive/iterative) & BFS
 // O(n), O(h): which is O(n) in worst case of an unbalanced tree
 
+// DFS recursive
+const maxDepth = (root) => {
+    if (root === null) return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+// DFS iterative
+
+function maxDepth2(root) {
+    let stack = [[root, 1]]; // Stack to store nodes and their corresponding depth
+    let res = 0;
+
+    while (stack.length > 0) {
+        let [node, depth] = stack.pop(); // Destructure node and depth from stack
+
