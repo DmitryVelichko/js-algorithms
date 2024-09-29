625d6ce02f50cff7c4fc1ae65389ee1560705bb0
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-29 22:46:06 +0300

    Add algo

--- 6. Trees/543-diameter-of-binary-tree.js
+++ 6. Trees/543-diameter-of-binary-tree.js
@@ -22,3 +22,12 @@
         
         let left = dfs(node.left)
         let right = dfs(node.right)
+
+        max = Math.max(left + right, max)
+        
+        return Math.max(left, right) + 1
+    }
+
+    dfs(root)
+    return max
+};
