2f6ddaacaf89838f0d667035fc80ea8cb7c30e78
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-29 22:45:57 +0300

    Add algo

--- 6. Trees/543-diameter-of-binary-tree.js
+++ 6. Trees/543-diameter-of-binary-tree.js
@@ -5,3 +5,14 @@
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
  * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+ // T: O(n), S: O(h): h = height of the tree (O(log n) - for balanced tree, O(n) for unbalanced)
+ var diameterOfBinaryTree = function(root) {
+    if(root === null) return 0
+    let max = 0;
+
