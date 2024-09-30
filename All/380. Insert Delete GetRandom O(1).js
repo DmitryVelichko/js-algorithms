6260bf1e3cd8b95a876cae3e641001fd921f88db
Dmitry Velichko <soleil-2005@mail.ru>
2024-09-30 22:37:42 +0300

    Add algo

--- All/380. Insert Delete GetRandom O(1).js
+++ All/380. Insert Delete GetRandom O(1).js
@@ -30,3 +30,20 @@
 // randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
 // randomizedSet.insert(2); // 2 was already in the set, so return false.
 // randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+ 
+
+// Constraints:
+
+// -231 <= val <= 231 - 1
+// At most 2 * 105 calls will be made to insert, remove, and getRandom.
+// There will be at least one element in the data structure when getRandom is called.
+
+
+var RandomizedSet = function() {
+    this.set = new Set()
+};
+
+/** 
+* @param {number} val
+* @return {boolean}
+*/
