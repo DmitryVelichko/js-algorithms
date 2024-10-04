3af4cb05aac35adb60522e09ee14ea7a4ed1c564
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-04 23:47:02 +0300

    Add algo

--- All/70-climbing-stairs.js
+++ All/70-climbing-stairs.js
@@ -15,3 +15,12 @@
 // 1. 1 step + 1 step
 // 2. 2 steps
 // Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+
