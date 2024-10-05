683a3944f443b4fd56926569b90e6bba3fe212d9
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-05 13:24:12 +0300

    Add algo

--- 0. Arrays & Hashing/1-two-sum.js
+++ 0. Arrays & Hashing/1-two-sum.js
@@ -1,59 +1,9 @@
-// 1. Two Sum
-// Easy
 
+// 1. Two Sum
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-// You can return the answer in any order.
-
-// Example 1:
-
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
-
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-// Example 3:
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
-
-
-// Constraints:
-
-// 2 <= nums.length <= 104
-// -109 <= nums[i] <= 109
-// -109 <= target <= 109
-// Only one valid answer exists.
-
-
-// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
-
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
- /**
- Вернуть индексы двух чисел дающих в сумме target 
- 
- O(n), O(n)
-
- hashmap
- проходимся в for по массиву
-   находим complement pair
-   ее нет в hash? Кладем туда
-   Есть? возвращаем массив индексов
-  */
-   var twoSum = function (nums, target) {
-    // O(n), O(n)
-    const hash = {}
-    for (let [index, num] of nums.entries()) {
-        let complement = target - num
-        if (hash[complement] === undefined) hash[num] = index
-        else return [hash[complement], index]
-    }
-}
