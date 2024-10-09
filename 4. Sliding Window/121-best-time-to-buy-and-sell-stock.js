4d8fde440eb6a3a756bfda2410a480e22d29e665
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-09 23:03:25 +0300

    Add algo

--- 4. Sliding Window/121-best-time-to-buy-and-sell-stock.js
+++ 4. Sliding Window/121-best-time-to-buy-and-sell-stock.js
@@ -36,3 +36,13 @@
  сравниваем текущий профит с разницей между minPrice и текущей ценой
  */
  // O(n), O(1)
+ var maxProfit = function(prices) {
+    let minPrice= prices[0] 
+    let maxProfit = 0 
+    for(let price of prices) {
+        minPrice = Math.min(minPrice, price)
+        maxProfit = Math.max(maxProfit, price - minPrice)
+    }
+
+    return maxProfit
+};
