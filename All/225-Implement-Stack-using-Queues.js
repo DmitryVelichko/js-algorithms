41af774c028c2ffd5fe1457539eae8cf1257a157
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-13 22:37:05 +0300

    Add algo

--- All/225-Implement-Stack-using-Queues.js
+++ All/225-Implement-Stack-using-Queues.js
@@ -69,3 +69,25 @@ MyStack.prototype.pop = function() {
     return this.q1.shift()
 };
 
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.q1[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q1.length === 0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
