b84b7247581b95158f0f325dca42f18fabd731da
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-13 22:37:00 +0300

    Add algo

--- All/225-Implement-Stack-using-Queues.js
+++ All/225-Implement-Stack-using-Queues.js
@@ -46,3 +46,26 @@ var MyStack = function() {
     this.q1 = []
     this.q2 = []
 };
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    while (this.q1.length !== 0) {
+        this.q2.push(this.q1.shift());
+    }
+    
+    this.q1.push(x);
+    while (this.q2.length !== 0) {
+        this.q1.push(this.q2.shift());
+    }
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.q1.shift()
+};
+
