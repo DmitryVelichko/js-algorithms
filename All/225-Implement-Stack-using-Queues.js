4bcf5b734c64a2c74ebdcc129ef128e9539b9a42
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-13 22:36:52 +0300

    Add algo

--- All/225-Implement-Stack-using-Queues.js
+++ All/225-Implement-Stack-using-Queues.js
@@ -30,3 +30,19 @@
 // myStack.top(); // return 2
 // myStack.pop(); // return 2
 // myStack.empty(); // return False
+ 
+
+// Constraints:
+
+// 1 <= x <= 9
+// At most 100 calls will be made to push, pop, top, and empty.
+// All the calls to pop and top are valid.
+ 
+
+// Follow-up: Can you implement the stack using only one queue?
+
+
+var MyStack = function() {
+    this.q1 = []
+    this.q2 = []
+};
