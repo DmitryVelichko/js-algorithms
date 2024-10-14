2e94575c4703505ff803750c13f41bf39e0b46a8
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-14 22:03:33 +0300

    Add algo

--- All/228-summary-ranges.js
+++ All/228-summary-ranges.js
@@ -33,3 +33,28 @@
 // [8,9] --> "8->9"
  
 
+// Constraints:
+
+// 0 <= nums.length <= 20
+// -231 <= nums[i] <= 231 - 1
+// All the values of nums are unique.
+// nums is sorted in ascending order.
+
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+var summaryRanges = function(nums) {
+    // Create a list of string to store the output result...
+    const output = [];
+    // Start traversing the array from idx = 0 till idx < sizeofarray in a while loop.
+    let idx = 0;
+    while(idx < nums.length) {
+        // Initialize beg and last index for identifying the continuous element in the array...
+        let beg, last;
+        // Mark the number at current index as beginning element of the range...
+        beg = nums[idx];
+        // Traverse the array beggining from current index & find the last element whose difference from previous element is exactly 1, i.e. nums[idx + 1] == nums[idx] + 1...
+    
