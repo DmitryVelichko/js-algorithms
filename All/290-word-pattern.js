40d3dc4ea38f51b874ebb57ee7da0c9388fa0b0f
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-15 23:21:32 +0300

    Add algo

--- All/290-word-pattern.js
+++ All/290-word-pattern.js
@@ -17,3 +17,17 @@
 // Input: pattern = "aaaa", s = "dog cat cat dog"
 // Output: false
  
+// Constraints:
+
+// 1 <= pattern.length <= 300
+// pattern contains only lower-case English letters.
+// 1 <= s.length <= 3000
+// s contains only lowercase English letters and spaces ' '.
+// s does not contain any leading or trailing spaces.
+// All the words in s are separated by a single space.
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
