26e235c12b4a8f2138ec7cb1d443018eb61fb161
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-16 21:27:02 +0300

    Add algo

--- 2. Stack/853-car-fleet.js
+++ 2. Stack/853-car-fleet.js
@@ -31,3 +31,21 @@
 // Example 2:
 
 // Input: target = 10, position = [3], speed = [3]
+
+// Output: 1
+
+// Explanation:
+
+// There is only one car, hence there is only one fleet.
+// Example 3:
+
+// Input: target = 100, position = [0,2,4], speed = [4,2,1]
+
+// Output: 1
+
+// Explanation:
+
+// The cars starting at 0 (speed 4) and 2 (speed 2) become a fleet, meeting each other at 4. The car starting at 4 (speed 1) travels to 5.
+// Then, the fleet at 4 (speed 2) and the car at position 5 (speed 1) become one fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+
+
