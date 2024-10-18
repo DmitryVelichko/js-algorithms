75fecc3b00cd1c03b7b327974ba15e898fab0dab
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-18 22:29:09 +0300

    Add algo

--- 2. Stack/155-min-stack.js
+++ 2. Stack/155-min-stack.js
@@ -5,3 +5,11 @@
 
 // Implement the MinStack class:
 
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+
