93ac098e6d46b0720a299ebc72b8dea3f498bdd2
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-18 22:29:37 +0300

    Add algo

--- 2. Stack/155-min-stack.js
+++ 2. Stack/155-min-stack.js
@@ -65,3 +65,25 @@ MinStack.prototype.pop = function () {
     this.minStack.pop();
 };
 
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
