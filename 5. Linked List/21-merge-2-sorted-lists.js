1d02ffb7f3d918ff88efbc5beec0ad8d1f7e1772
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-20 13:06:52 +0300

    Add algo

--- 5. Linked List/21-merge-2-sorted-lists.js
+++ 5. Linked List/21-merge-2-sorted-lists.js
@@ -59,3 +59,22 @@
 // l2 еще есть ? tail -> l2
 // возвращаем dummy.next
 
+// O(n), O(1)
+var mergeTwoLists = function (l1, l2) {
+    let dummy = new ListNode(-Infinity)
+    let tail = dummy
+
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            tail.next = l1
+            l1 = l1.next
+        } else {
+            tail.next = l2
+            l2 = l2.next
+        }
+        tail = tail.next
+    }
+    if (l1) tail.next = l1
+    else if (l2) tail.next = l2
+    return dummy.next
+}
