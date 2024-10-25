35570edc11fdc631ee84f8b42a9baecbdcef162e
Dmitry Velichko <soleil-2005@mail.ru>
2024-10-25 23:48:43 +0300

    Add algo

--- All/145-binary-tree-postorder-traversal.js
+++ All/145-binary-tree-postorder-traversal.js
@@ -35,3 +35,20 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if(!root) return []
+    let res = []
+    function traverse(node) {
+        if(node === null) return
+        if(node.left) traverse(node.left)
+        if(node.right) traverse(node.right)
+        res.push(node.val)
+    }
+
+    traverse(root)
+    return res
+};
