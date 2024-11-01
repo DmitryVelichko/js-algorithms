273a0219055bf62dc66f82f61eaf319ee6c5a01d
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-01 23:00:43 +0300

    Add algo

--- All/292-Nim-Game.js
+++ All/292-Nim-Game.js
@@ -11,3 +11,14 @@
 
  
 
+// Example 1:
+
+// Input: n = 4
+// Output: false
+// Explanation: These are the possible outcomes:
+// 1. You remove 1 stone. Your friend removes 3 stones, including the last stone. Your friend wins.
+// 2. You remove 2 stones. Your friend removes 2 stones, including the last stone. Your friend wins.
+// 3. You remove 3 stones. Your friend removes the last stone. Your friend wins.
+// In all outcomes, your friend wins.
+// Example 2:
+
