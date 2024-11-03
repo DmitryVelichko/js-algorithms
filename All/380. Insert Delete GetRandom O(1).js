5a6059bae1d02edf24d5d1640e3948a272ea609e
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-03 21:43:23 +0300

    Add algo

--- All/380. Insert Delete GetRandom O(1).js
+++ All/380. Insert Delete GetRandom O(1).js
@@ -47,3 +47,23 @@ var RandomizedSet = function() {
 * @param {number} val
 * @return {boolean}
 */
+RandomizedSet.prototype.insert = function(val) {
+   if(this.set.has(val)) {
+      return false
+   } else {
+       this.set.add(val)
+       return true
+   }
+};
+
+/** 
+* @param {number} val
+* @return {boolean}
+*/
+RandomizedSet.prototype.remove = function(val) {
+   if(this.set.has(val)) {
+       this.set.delete(val)
+       return true
+   }
+   return false
+};
