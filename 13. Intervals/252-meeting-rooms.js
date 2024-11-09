48df11854b6b420c861d475f19b4fac90aa6760e
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-09 12:15:11 +0300

    Add algo

--- 13. Intervals/252-meeting-rooms.js
+++ 13. Intervals/252-meeting-rooms.js
@@ -38,3 +38,17 @@ class Solution {
      * @param {Interval[]} intervals
      * @returns {boolean}
      */
+    canAttendMeetings(intervals) {
+        const n = intervals.length;
+        for (let i = 0; i < n; i++) {
+            const A = intervals[i];
+            for (let j = i + 1; j < n; j++) {
+                const B = intervals[j];
+                if (Math.min(A.end, B.end) > Math.max(A.start, B.start)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
