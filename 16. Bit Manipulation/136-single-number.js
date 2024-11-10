cd615c3af00fe652023e009864c470f90437afc5
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-10 21:58:53 +0300

    Add algo

--- 16. Bit Manipulation/136-single-number.js
+++ 16. Bit Manipulation/136-single-number.js
@@ -34,3 +34,10 @@
  * @param {number[]} nums
  * @return {number}
  */
+var singleNumber = function(nums) {
+    let result = 0
+    for(let num of nums) {
+        result = result ^ num
+    }
+    return result
+};
