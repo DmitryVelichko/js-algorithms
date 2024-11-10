46459490cf04e1738374e8ecc8b6ddda808dfd08
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-10 21:58:50 +0300

    Add algo

--- 16. Bit Manipulation/136-single-number.js
+++ 16. Bit Manipulation/136-single-number.js
@@ -23,3 +23,14 @@
 // Input: nums = [1]
 // Output: 1
  
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
