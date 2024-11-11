5dbe297b22e9c7f3c20e0b7724feb7a9f3ebc168
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-11 22:24:43 +0300

    Add algo

--- 16. Bit Manipulation/191. Number of 1 Bits.js
+++ 16. Bit Manipulation/191. Number of 1 Bits.js
@@ -3,3 +3,15 @@
 // Easy
 // Topics
 // Companies
+// Given a positive integer n, write a function that returns the number of 
+// set bits
+//  in its binary representation (also known as the Hamming weight).
+
+ 
+
+// Example 1:
+
+// Input: n = 11
+
+// Output: 3
+
