0a8a40541a1fbc49334fa78893811596cbaac7f3
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-14 19:20:32 +0300

    Add algo

--- All/238. Product of Array Except Self.js
+++ All/238. Product of Array Except Self.js
@@ -38,3 +38,12 @@
  // Задача на Arrays and Hashing
  // 
  // T: O(n), S:O(1) result array не считается по условиям задачи
+ var productExceptSelf = function(nums) {
+    const res = [];
+    let start = 1;
+    let start2 = 1
+    // Перемножаем между собой числа слева направо кроме последнего и пушим в массив
+    for (let i = 0; i < nums.length; i++) {
+        res.push(start)
+        start = start * nums[i]
+    }
