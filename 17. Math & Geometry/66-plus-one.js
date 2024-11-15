c868c3c491392002135f6192517f6dfda93f99be
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-15 23:19:04 +0300

    Add algo

--- 17. Math & Geometry/66-plus-one.js
+++ 17. Math & Geometry/66-plus-one.js
@@ -25,3 +25,10 @@
 // Thus, the result should be [4,3,2,2].
 // Example 3:
 
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+ 
+
