f9268622243eb27b17922fb30b04f68db42f3ff5
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-20 23:20:44 +0300

    Add algo

--- All/319-bulb-switcher.js
+++ All/319-bulb-switcher.js
@@ -12,3 +12,12 @@
 // Example 1:
 
 
+// Input: n = 3
+// Output: 1
+// Explanation: At first, the three bulbs are [off, off, off].
+// After the first round, the three bulbs are [on, on, on].
+// After the second round, the three bulbs are [on, off, on].
+// After the third round, the three bulbs are [on, off, off]. 
+// So you should return 1 because there is only one bulb is on.
+// Example 2:
+
