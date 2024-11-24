9130d3338426dbf10bb977558f721a0ec239921f
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-24 22:09:11 +0300

    Add algo

--- All/1716. Calculate Money in Leetcode Bank.js
+++ All/1716. Calculate Money in Leetcode Bank.js
@@ -22,3 +22,15 @@
 // Input: n = 10
 // Output: 37
 // Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+// Example 3:
+
+// Input: n = 20
+// Output: 96
+// Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+
+
+// Constraints:
+
+// 1 <= n <= 1000
+
+
