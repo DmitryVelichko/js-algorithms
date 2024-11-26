b85972d8131d307cb2b265e45dc8a93615099797
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-26 23:53:47 +0300

    Add algo

---
+++ All/859-buddy-strings.js
@@ -0,0 +1,7 @@
+// 859. Buddy Strings
+// Easy
+// 2K
+// 1.2K
+// Companies
+// Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
