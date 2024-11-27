7b604543b7a45400ab4f93bc5c0b2a4ed996f576
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-27 23:45:57 +0300

    Add algo

---
+++ All/806-number-of-lines.js
@@ -0,0 +1,5 @@
+// 806. Number of Lines To Write String
+// Easy
+// Topics
+// Companies
+// You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
