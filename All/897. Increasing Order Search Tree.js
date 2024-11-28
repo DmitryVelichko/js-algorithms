a4e6c629d4713286f972b7960ae3d040b742fbd2
Dmitry Velichko <soleil-2005@mail.ru>
2024-11-28 19:32:53 +0300

    Add algo

---
+++ All/897. Increasing Order Search Tree.js
@@ -0,0 +1,8 @@
+// 897. Increasing Order Search Tree
+// Easy
+// 4.2K
+// 663
+// Companies
+// Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+ 
