f104282c1f54691179caa44cafe7a250e8e80cc5
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-03 19:40:43 +0300

    Add algo

--- All/2966. Divide Array Into Arrays With Max Difference.js
+++ All/2966. Divide Array Into Arrays With Max Difference.js
@@ -36,3 +36,18 @@
 // 1 <= nums[i] <= 105
 // 1 <= k <= 105
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    const res = []
+    nums.sort((a,b) => a - b)
+
+    for(let i=0; i<nums.length; i+=3) {
+        if((nums[i+2] - nums[i]) > k ) return []
+        res.push([nums[i], nums[i+1], nums[i+2]])
+    }
+    return res
+};
