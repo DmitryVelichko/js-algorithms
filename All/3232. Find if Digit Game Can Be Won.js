f594237a28eac3521d50a69d413795f80448cac8
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-04 21:54:57 +0300

    Add algo

--- All/3232. Find if Digit Game Can Be Won.js
+++ All/3232. Find if Digit Game Can Be Won.js
@@ -22,3 +22,21 @@
 
 // Alice cannot win by choosing either single-digit or double-digit numbers.
 
+// Example 2:
+
+// Input: nums = [1,2,3,4,5,14]
+
+// Output: true
+
+// Explanation:
+
+// Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+// Example 3:
+
+// Input: nums = [5,5,5,25]
+
+// Output: true
+
+// Explanation:
+
