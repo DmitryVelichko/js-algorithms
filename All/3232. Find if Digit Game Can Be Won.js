c2c8c5034027afd284f58b172b9be95341b1fc25
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-04 21:55:58 +0300

    Add algo

--- All/3232. Find if Digit Game Can Be Won.js
+++ All/3232. Find if Digit Game Can Be Won.js
@@ -67,3 +67,15 @@ var canAliceWin1 = function (nums) {
     if (sin > doub || doub > sin) return true
     else return false
 };
+// O(n), O(1)
+var canAliceWin = function (nums) {
+    let single = 0
+    let double = 0
+
+    for (let num of nums) {
+        if (num < 10) single += num
+        else double += num
+    }
+
+    return (double > single || single > double)
+};
