e038a86e9a65dc63a9d691a59b6391e67eaac5d9
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-05 19:03:31 +0300

    Add algo

--- All/design-hashmap.js
+++ All/design-hashmap.js
@@ -35,3 +35,14 @@ MyHashMap.prototype.get = function(key) {
  * @param {number} key
  * @return {void}
  */
+MyHashMap.prototype.remove = function(key) {
+    delete this.hashMap[key]
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
