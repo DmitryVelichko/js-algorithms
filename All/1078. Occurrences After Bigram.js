d2be42031196261d58c7148b489d086701cc089c
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-07 22:41:56 +0300

    Add algo

---
+++ All/1078. Occurrences After Bigram.js
@@ -0,0 +1,7 @@
+// 1078. Occurrences After Bigram
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given two strings first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
