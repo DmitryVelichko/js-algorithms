08c6b3115222226f5c9d6a24a8836d8ba7d1d390
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-08 18:13:22 +0300

    Add algo

---
+++ All/1309. Decrypt String from Alphabet to Integer Mapping.js
@@ -0,0 +1,6 @@
+// 1309. Decrypt String from Alphabet to Integer Mapping
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
