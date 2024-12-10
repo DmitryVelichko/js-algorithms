5948f2fa350ba683c049144e15c2900a471458a2
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-10 23:36:34 +0300

    Add algo

---
+++ All/1295. Find Numbers with Even Number of Digits.js
@@ -0,0 +1,8 @@
+// 1295. Find Numbers with Even Number of Digits
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
