0a3728d2632b39f84ca7ac44e768c71e0eeb77fe
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-12 23:49:44 +0300

    Add algo

---
+++ All/1346. Check If N and Its Double Exist.js
@@ -0,0 +1,13 @@
+// 1346. Check If N and Its Double Exist
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array arr of integers, check if there exist two indices i and j such that :
+
+// i != j
+// 0 <= i, j < arr.length
+// arr[i] == 2 * arr[j]
+
+
