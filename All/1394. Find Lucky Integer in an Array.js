7f1683f719e8ddc29bad80dfbe9e0a2df9d70a00
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-13 22:40:49 +0300

    Add algo

---
+++ All/1394. Find Lucky Integer in an Array.js
@@ -0,0 +1,9 @@
+// 1394. Find Lucky Integer in an Array
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
