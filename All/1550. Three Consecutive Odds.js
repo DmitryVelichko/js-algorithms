6f0351d1f20a8c953633afce28e8146128b50c04
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-16 23:45:29 +0300

    Add algo

---
+++ All/1550. Three Consecutive Odds.js
@@ -0,0 +1,6 @@
+// 1550. Three Consecutive Odds
+// Easy
+// 621
+// 62
+// Companies
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
