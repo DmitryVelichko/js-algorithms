224625fa2833b249df543646f2e8c12f776e33e6
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-18 23:05:03 +0300

    Add algo

--- All/58. Length of Last Word.js
+++ All/58. Length of Last Word.js
@@ -5,3 +5,16 @@
 // Companies
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
