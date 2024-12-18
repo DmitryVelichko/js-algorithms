146bd54458ff76a7c107c72ea0344f09602eae11
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-18 23:05:15 +0300

    Add algo

--- All/58. Length of Last Word.js
+++ All/58. Length of Last Word.js
@@ -27,3 +27,10 @@
 // Output: 6
 // Explanation: The last word is "joyboy" with length 6.
 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
