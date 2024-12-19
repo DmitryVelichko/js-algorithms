841e6d7ffca54a168dbd16dbf7f97fedf6911fe2
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-19 23:47:08 +0300

    Add algo

---
+++ All/1796. Second Largest Digit in a String.js
@@ -0,0 +1,7 @@
+// 1796. Second Largest Digit in a String
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
