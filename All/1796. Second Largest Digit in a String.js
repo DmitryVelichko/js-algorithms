175d231f0964983d095e3910c6bfcf7578282da9
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-19 23:47:50 +0300

    Add algo

--- All/1796. Second Largest Digit in a String.js
+++ All/1796. Second Largest Digit in a String.js
@@ -22,3 +22,32 @@
 // Explanation: The digits that appear in s are [1]. There is no second largest digit. 
 
 
+// Constraints:
+
+// 1 <= s.length <= 500
+// s consists of only lowercase English letters and digits.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// O(n), O(1)
+var secondHighest = function (s) {
+    let first = -1, second = -1;
+
+    for (let char of s) {
+        if (!isNaN(char)) {
+            let digit = Number(char);
+
+            if (digit > first) {
+                second = first; // Update second largest
+                first = digit;  // Update largest
+            } else if (digit > second && digit < first) {
+                second = digit; // Update second largest
+            }
+        }
+    }
+
+    return second;
+};
+
