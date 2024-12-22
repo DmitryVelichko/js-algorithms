8f9bc44dbca3c93035a774969d34892bd44af309
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-22 20:59:32 +0300

    Add algo

--- All/1952. Three Divisors.js
+++ All/1952. Three Divisors.js
@@ -1,3 +1,12 @@
 // 1952. Three Divisors
 // Solved
 // Easy
+// Topics
+// Companies
+// Hint
+// Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+
+// An integer m is a divisor of n if there exists an integer k such that n = k * m.
+
+
+
