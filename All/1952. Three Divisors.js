c842387f2d8b28a01a63508954a4a18ac6f44c48
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-22 20:59:44 +0300

    Add algo

--- All/1952. Three Divisors.js
+++ All/1952. Three Divisors.js
@@ -16,3 +16,13 @@
 // Output: false
 // Explantion: 2 has only two divisors: 1 and 2.
 // Example 2:
+
+// Input: n = 4
+// Output: true
+// Explantion: 4 has three divisors: 1, 2, and 4.
+
+
+// Constraints:
+
+// 1 <= n <= 104
+
