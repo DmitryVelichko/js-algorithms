7f72fc49cd498e69dd05a1530ddf6bb1ba3dfe7e
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-24 23:24:52 +0300

    Add algo

--- All/1854. Maximum Population Year.js
+++ All/1854. Maximum Population Year.js
@@ -5,3 +5,14 @@
 // Companies
 // Hint
 // You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+
+// The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
+
+// Return the earliest year with the maximum population.
+
+// Example 1:
+
+// Input: logs = [[1993,1999],[2000,2010]]
+// Output: 1993
+// Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
+// Example 2:
