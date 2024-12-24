3ded9a14d559bb3303e06359546a9d1a3335d906
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-24 23:24:59 +0300

    Add algo

--- All/1854. Maximum Population Year.js
+++ All/1854. Maximum Population Year.js
@@ -16,3 +16,11 @@
 // Output: 1993
 // Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
 // Example 2:
+
+// Input: logs = [[1950,1961],[1960,1971],[1970,1981]]
+// Output: 1960
+// Explanation: 
+// The maximum population is 2, and it had happened in years 1960 and 1970.
+// The earlier year between them is 1960.
+
+
