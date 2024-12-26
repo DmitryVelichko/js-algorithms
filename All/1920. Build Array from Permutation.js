5e376eaa377dc0cd52905c7c2aee88380d97ea36
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-26 23:42:37 +0300

    Add algo

--- All/1920. Build Array from Permutation.js
+++ All/1920. Build Array from Permutation.js
@@ -26,3 +26,13 @@
 // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
 //     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
 //     = [4,5,0,1,2,3]
+
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// 0 <= nums[i] < nums.length
+// The elements in nums are distinct.
+
+
+// Follow-up: Can you solve it without using an extra space (i.e., O(1) memory)?
