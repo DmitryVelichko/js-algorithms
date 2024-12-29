3c9e8fb4fd6c0360a8a3a3605460ec7441a6a566
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-29 14:57:19 +0300

    Add algo

---
+++ All/1935. Maximum Number of Words You Can Type.js
@@ -0,0 +1,8 @@
+// 1935. Maximum Number of Words You Can Type
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+
