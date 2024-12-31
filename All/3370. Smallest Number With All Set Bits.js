08e58ebcc40c37367696c6fca5d7acce45a210c9
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-31 19:13:46 +0300

    Add algo

--- All/3370. Smallest Number With All Set Bits.js
+++ All/3370. Smallest Number With All Set Bits.js
@@ -20,3 +20,19 @@
 // Explanation:
 
 // The binary representation of 7 is "111".
+
+// Example 2:
+
+// Input: n = 10
+
+// Output: 15
+
+// Explanation:
+
+// The binary representation of 15 is "1111".
+
+// Example 3:
+
+// Input: n = 3
+
+// Output: 3
