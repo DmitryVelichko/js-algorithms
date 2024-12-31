b6b64dd793c6d42f7d02a1765e767f8db4c62ddf
Dmitry Velichko <soleil-2005@mail.ru>
2024-12-31 19:13:06 +0300

    Add algo

--- All/3370. Smallest Number With All Set Bits.js
+++ All/3370. Smallest Number With All Set Bits.js
@@ -4,3 +4,19 @@
 // Topics
 // Companies
 // Hint
+// You are given a positive number n.
+
+// Return the smallest number x greater than or equal to n, such that the binary representation of x contains only 
+// set bits
+
+
+
+// Example 1:
+
+// Input: n = 5
+
+// Output: 7
+
+// Explanation:
+
+// The binary representation of 7 is "111".
