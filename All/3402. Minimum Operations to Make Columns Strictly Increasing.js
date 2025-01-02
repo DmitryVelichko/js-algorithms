5171ac1d9d3821c766cbd56a2a251611127524e8
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-02 19:22:31 +0300

    Add algo

--- All/3402. Minimum Operations to Make Columns Strictly Increasing.js
+++ All/3402. Minimum Operations to Make Columns Strictly Increasing.js
@@ -8,3 +8,17 @@
 // In one operation, you can increment the value of any grid[i][j] by 1.
 
 // Return the minimum number of operations needed to make all columns of grid strictly increasing.
+
+
+
+// Example 1:
+
+// Input: grid = [[3,2],[1,3],[3,4],[0,1]]
+
+// Output: 15
+
+// Explanation:
+
+// To make the 0th column strictly increasing, we can apply 3 operations on grid[1][0], 2 operations on grid[2][0], and 6 operations on grid[3][0].
+// To make the 1st column strictly increasing, we can apply 4 operations on grid[3][1].
+
