d025df64b7e323f349df4f5d213f14e492d259ae
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-03 11:03:03 +0300

    Add algo

--- All/3110. Score of a String.js
+++ All/3110. Score of a String.js
@@ -5,3 +5,14 @@
 // Companies
 // Hint
 // You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+// Return the score of s.
+
+
+
+// Example 1:
+
+// Input: s = "hello"
+
+// Output: 13
+
