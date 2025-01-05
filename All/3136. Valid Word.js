a9780a0ca6425ec20f1892693e48a2233310cb79
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-05 12:14:52 +0300

    Add algo

--- All/3136. Valid Word.js
+++ All/3136. Valid Word.js
@@ -39,3 +39,16 @@
 // Explanation:
 
 // The length of this word is fewer than 3, and does not have a vowel.
+
+// Example 3:
+
+// Input: word = "a3$e"
+
+// Output: false
+
+// Explanation:
+
+// This word contains a '$' character and does not have a consonant.
+
+
+
