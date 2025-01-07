c6ccc0b3d3975582fa45b9766f27ac4470d55285
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-07 14:23:33 +0300

    Add algo

--- All/3162. Find the Number of Good Pairs I.js
+++ All/3162. Find the Number of Good Pairs I.js
@@ -8,3 +8,18 @@
 
 // A pair (i, j) is called good if nums1[i] is divisible by nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1).
 
+// Return the total number of good pairs.
+
+
+
+// Example 1:
+
+// Input: nums1 = [1,3,4], nums2 = [1,3,4], k = 1
+
+// Output: 5
+
+// Explanation:
+
+// The 5 good pairs are (0, 0), (1, 0), (1, 1), (2, 0), and (2, 2).
+// Example 2:
+
