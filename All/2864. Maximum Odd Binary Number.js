c637dab6ed5a08b889d6665be2155b184ca7178c
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-10 22:18:57 +0300

    Add algo

--- All/2864. Maximum Odd Binary Number.js
+++ All/2864. Maximum Odd Binary Number.js
@@ -5,3 +5,12 @@
 // Companies
 // Hint
 // You are given a binary string s that contains at least one '1'.
+
+// You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+// Return a string representing the maximum odd binary number that can be created from the given combination.
+
+// Note that the resulting string can have leading zeros.
+
+ 
+
