e096a4c5b47f70fd4a11aaeac6cedcee4ab776dc
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-11 12:50:28 +0300

    Add algo

--- All/TOP 100/88. Merge Sorted Array.js
+++ All/TOP 100/88. Merge Sorted Array.js
@@ -44,3 +44,19 @@
 
 // Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+// Time complexity: O((m+n)log(m+n)), Space complexity: O(1)
+var merge2 = function (nums1, m, nums2, n) {
+    for (let i = m, j = 0; j < n; i++, j++) {
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b);
+};
+
+
