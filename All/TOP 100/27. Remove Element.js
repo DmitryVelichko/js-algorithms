5fe00141f0dcf13293e82e6e806f5e351d27e8f4
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-12 22:06:32 +0300

    Add algo

--- All/TOP 100/27. Remove Element.js
+++ All/TOP 100/27. Remove Element.js
@@ -19,3 +19,14 @@
 // int[] expectedNums = [...]; // The expected answer with correct length.
 //                             // It is sorted with no values equaling val.
 
+// int k = removeElement(nums, val); // Calls your implementation
+
+// assert k == expectedNums.length;
+// sort(nums, 0, k); // Sort the first k elements of nums
+// for (int i = 0; i < actualLength; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+
+
