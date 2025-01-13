5bf8843516fda9af5e7b3ee703b34567c8f57b50
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-13 19:12:56 +0300

    Add algo

--- All/TOP 100/169. Majority Element.js
+++ All/TOP 100/169. Majority Element.js
@@ -27,3 +27,23 @@
 
 
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// O(n), O(1)
+var majorityElement = function (nums) {
+    let candidate = null
+    let count = 0
+
+    for (let num of nums) {
+        if (count === 0) candidate = num
+
+        if (num === candidate) count++
+        else count--
+    }
+    return candidate
+}
+
