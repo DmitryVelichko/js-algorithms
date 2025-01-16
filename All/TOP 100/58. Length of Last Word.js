2a6af4e5815fa0a9d20eabb39c521b8bbf990be1
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-16 15:21:38 +0300

    Add algo

--- All/TOP 100/58. Length of Last Word.js
+++ All/TOP 100/58. Length of Last Word.js
@@ -23,3 +23,15 @@
 // Explanation: The last word is "moon" with length 4.
 // Example 3:
 
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+
