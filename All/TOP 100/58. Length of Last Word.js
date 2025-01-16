35f3624830f9dd54d65d0984500158f46b0db2b9
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-16 15:21:31 +0300

    Add algo

--- All/TOP 100/58. Length of Last Word.js
+++ All/TOP 100/58. Length of Last Word.js
@@ -9,3 +9,17 @@
 // substring
 //  consisting of non-space characters only.
 
+
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
