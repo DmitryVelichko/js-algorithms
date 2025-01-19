c650f53526309bf1fada157ac6bcc84f3dd69137
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-19 19:31:08 +0300

    Add algo

--- All/TOP 100/189. Rotate Array.js
+++ All/TOP 100/189. Rotate Array.js
@@ -56,3 +56,11 @@ var rotate = function (nums, k) {
         }
     }
 
+    // Step 1: Reverse the entire array
+    reverse(0, n - 1);
+    // Step 2: Reverse the first k elements
+    reverse(0, k - 1);
+    // Step 3: Reverse the remaining elements
+    reverse(k, n - 1);
+
+};
