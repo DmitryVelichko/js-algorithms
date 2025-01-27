626aa2c18f370e5659f54d48df52ca85da23ae2e
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-27 22:06:08 +0800

    Add algo

---
+++ All/TOP 100/20. Valid Parentheses.js
@@ -0,0 +1,7 @@
+// 20. Valid Parentheses
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
