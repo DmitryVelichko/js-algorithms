7d2ce4c1a7d7110f0914129e774a6647b0baa70b
Dmitry Velichko <soleil-2005@mail.ru>
2025-01-28 22:26:11 +0800

    Add algo

--- All/TOP 100/141. Linked List Cycle.js
+++ All/TOP 100/141. Linked List Cycle.js
@@ -11,3 +11,12 @@
 
 
 
+// Example 1:
+
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+// Example 2:
+
+
