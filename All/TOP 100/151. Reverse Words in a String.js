fab85a17c94055c9e904953780d02dae3ac5f9c2
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-01 12:27:17 +0800

    Add algo

--- All/TOP 100/151. Reverse Words in a String.js
+++ All/TOP 100/151. Reverse Words in a String.js
@@ -13,3 +13,14 @@
 
 
 
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
