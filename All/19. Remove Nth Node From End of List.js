094274e8ee2c95e99dd5bdbc2f2c06697b083070
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-03 22:42:48 +0800

    Add algo

---
+++ All/19. Remove Nth Node From End of List.js
@@ -0,0 +1,8 @@
+// 19. Remove Nth Node From End of List
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
