baa9c56dd8703c99f8142bf77b4bd2dabe37472d
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-04 21:53:06 +0800

    Add algo

--- All/TOP 100/392. Is Subsequence.js
+++ All/TOP 100/392. Is Subsequence.js
@@ -27,3 +27,22 @@
 
 
 // Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// O(n), O(1)
+var isSubsequence = function (s, t) {
+    let i = 0; // Pointer for `s`
+
+    for (let char of t) {
+        if (s[i] === char) {
+            i++; // Move `s` pointer when we find a match
+        }
+        if (i === s.length) return true; // If we matched all characters, return true
+    }
+
+    return i === s.length; // If `i` reached the end of `s`, it's a subsequence
+};
