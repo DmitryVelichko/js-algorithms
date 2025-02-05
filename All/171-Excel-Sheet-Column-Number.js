62df586a1359422f2baa6f39109bcca993eda520
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-05 22:47:07 +0800

    Add algo

--- All/171-Excel-Sheet-Column-Number.js
+++ All/171-Excel-Sheet-Column-Number.js
@@ -36,3 +36,14 @@
 // columnTitle is in the range ["A", "FXSHRXW"].
 
 
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    let total = 0
+    for (let i = 0; i < s.length; i++) {
+      total = total * 26 + s.charCodeAt(i) - "A".charCodeAt(0) + 1
+    }
+    return total
+  };
