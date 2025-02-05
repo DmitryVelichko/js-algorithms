81b10f4bbfd7b2acdae07539d934cf70794bb9ba
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-05 22:46:44 +0800

    Add algo

---
+++ All/171-Excel-Sheet-Column-Number.js
@@ -0,0 +1,6 @@
+// 171. Excel Sheet Column Number
+// Easy
+
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+// For example:
