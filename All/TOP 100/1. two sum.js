1622d5b063843b747476ec9bc5a6c053601fe8d6
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-06 23:43:32 +0800

    Add algo

---
+++ All/TOP 100/1. two sum.js
@@ -0,0 +1,8 @@
+// 1. Two Sum
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
