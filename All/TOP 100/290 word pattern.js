b54cb8ac5201391d791857c5d289b02774557998
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-08 10:33:08 +0800

    Add algo

--- All/TOP 100/290 word pattern.js
+++ All/TOP 100/290 word pattern.js
@@ -38,3 +38,12 @@
 
 
 
+// Constraints:
+
+// 1 <= pattern.length <= 300
+// pattern contains only lower-case English letters.
+// 1 <= s.length <= 3000
+// s contains only lowercase English letters and spaces ' '.
+// s does not contain any leading or trailing spaces.
+// All the words in s are separated by a single space.
+
