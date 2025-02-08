c689e963d54b35dd4aba748efcecb5dec689c21f
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-08 10:32:51 +0800

    Add algo

--- All/TOP 100/290 word pattern.js
+++ All/TOP 100/290 word pattern.js
@@ -10,3 +10,17 @@
 // Each letter in pattern maps to exactly one unique word in s.
 // Each unique word in s maps to exactly one letter in pattern.
 // No two letters map to the same word, and no two words map to the same letter.
+
+
+// Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+
+// Output: true
+
+// Explanation:
+
+// The bijection can be established as:
+
+// 'a' maps to "dog".
+// 'b' maps to "cat".
