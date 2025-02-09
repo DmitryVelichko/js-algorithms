06033b2bf2c448f5d9cf121e2acd9f21d1cb6469
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-09 10:36:11 +0800

    Add algo

---
+++ All/TOP 100/383 Ransome note.js
@@ -0,0 +1,11 @@
+// 383. Ransom Note
+// Solved
+// Easy
+// Topics
+// Companies
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+
+
