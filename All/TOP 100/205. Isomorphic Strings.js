0a195edc6cdea8437e5aad88f8a2a30ab95680ba
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-10 12:07:21 +0800

    Add algo

--- All/TOP 100/205. Isomorphic Strings.js
+++ All/TOP 100/205. Isomorphic Strings.js
@@ -23,3 +23,13 @@
 
 // Mapping 'e' to 'a'.
 // Mapping 'g' to 'd'.
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+
+// Output: false
+
+// Explanation:
+
+// The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
