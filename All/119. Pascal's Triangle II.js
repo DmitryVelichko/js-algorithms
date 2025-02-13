abd088f5790a863be2104a6029f88a1b561632c8
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-13 23:47:58 +0800

    Add algo

--- All/119. Pascal's Triangle II.js
+++ All/119. Pascal's Triangle II.js
@@ -13,3 +13,15 @@
 // Output: [1,3,3,1]
 // Example 2:
 
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+ 
+
+// Constraints:
+
+// 0 <= rowIndex <= 33
+ 
