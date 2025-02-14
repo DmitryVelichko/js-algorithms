ceeae0df929a619648411a45b1cc93424c151c43
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-14 11:18:28 +0800

    Add algo

--- All/TOP 100/530. Minimum Absolute Difference in BST.js
+++ All/TOP 100/530. Minimum Absolute Difference in BST.js
@@ -19,3 +19,19 @@
 // Output: 1
 
 
+// Constraints:
+
+// The number of nodes in the tree is in the range [2, 104].
+// 0 <= Node.val <= 105
+
+
+// Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
