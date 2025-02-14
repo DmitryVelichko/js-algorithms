42d4be3181de9c1db91b2b503c5d72227db22e8a
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-14 11:18:12 +0800

    Add algo

---
+++ All/TOP 100/530. Minimum Absolute Difference in BST.js
@@ -0,0 +1,8 @@
+// 530. Minimum Absolute Difference in BST
+// Solved
+// Easy
+// Topics
+// Companies
+// Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+
