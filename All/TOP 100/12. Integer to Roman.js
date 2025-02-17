aa775c42bbc3bab78d5b74dac90fa67c07942957
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-17 12:36:31 +0800

    Add algo

--- All/TOP 100/12. Integer to Roman.js
+++ All/TOP 100/12. Integer to Roman.js
@@ -59,3 +59,26 @@
 //    4 = IV
 
 
+// Constraints:
+
+// 1 <= num <= 3999
+/**
+ * @param {number} num
+ * @return {string}
+ */
+// O(1), O(1)
+var intToRoman = function (num) {
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let result = "";
+
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {  // Keep subtracting while num is greater than value
+            result += symbols[i];
+            num -= values[i];
+        }
+    }
+
+    return result;
+};
