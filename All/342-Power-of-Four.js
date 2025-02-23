81ed1614c12270ac278aca753b022c0aba4727d3
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-23 23:47:38 +0800

    Add algo

---
+++ All/342-Power-of-Four.js
@@ -0,0 +1,8 @@
+// 342. Power of Four
+// Easy
+// 3.3K
+// 346
+// Companies
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
