2997a73f0ac4d5c8b95c7f9b6920bca9d004dce0
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-25 21:13:41 +0800

    Add algo

--- All/414-Third-Maximum-Number.js
+++ All/414-Third-Maximum-Number.js
@@ -39,3 +39,18 @@
 
 // Follow up: Can you find an O(n) solution?
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    if(nums.length<3) return Math.max(...nums);
+    let u = new Set(nums)
+    if(u.size<3) return Math.max(...u);
+    
+    for(let i=0;i<2;i++){
+        u.delete(Math.max(...u))
+    }
+    
+    return Math.max(...u);
+};
