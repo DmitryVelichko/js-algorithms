d88b4f9bb7700dacdbc10d99bb5518d257c22407
Dmitry Velichko <soleil-2005@mail.ru>
2025-02-27 20:28:16 +0800

    Add algo

--- All/9. Palindrome Number.js
+++ All/9. Palindrome Number.js
@@ -23,3 +23,26 @@
 // Input: x = 10
 // Output: false
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+// O(n), O(n)
+var isPalindrome = function (x) {
+    let xStr = x.toString()
+    let reversed = ''
+
+    for (let num of xStr) {
+        reversed = num + reversed;
+    }
+    return xStr === reversed
+};
