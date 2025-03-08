d9b94d4ec0b54e5476a98705e756e5179caa7003
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-09 00:26:41 +0800

    Add algo

--- All/78-subsets.js
+++ All/78-subsets.js
@@ -15,3 +15,16 @@
 // Input: nums = [0]
 // Output: [[],[0]]
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums, depth = 0, subset = [], results = []) {
+    if (depth === nums.length) {
+        results.push(subset)
+    } else {
+        subsets(nums, depth + 1, subset, results)
+        subsets(nums, depth + 1, [...subset, nums[depth]], results)
+    }
+    return results
+};
