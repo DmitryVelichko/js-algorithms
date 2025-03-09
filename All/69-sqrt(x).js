608af4f742630b8b94191036fb1c6dcdee343d67
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-09 12:39:24 +0800

    Add algo

---
+++ All/69-sqrt(x).js
@@ -0,0 +1,7 @@
+// 69. Sqrt(x)
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
