20840dd3b518fbe8e52062260dd0703526936930
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-10 22:14:48 +0800

    Add algo

--- All/859. Buddy Strings.js
+++ All/859. Buddy Strings.js
@@ -45,3 +45,16 @@ var buddyStrings = function (s, goal) {
         let charSet = new Set(s);
         return charSet.size < s.length; // Check if there's at least one duplicate character
     }
+
+    let diffs = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== goal[i]) diffs.push(i);
+        if (diffs.length > 2) return false; // More than 2 differences means swapping won't work
+    }
+
+    return diffs.length === 2 &&
+        s[diffs[0]] === goal[diffs[1]] &&
+        s[diffs[1]] === goal[diffs[0]];
+};
+
+
