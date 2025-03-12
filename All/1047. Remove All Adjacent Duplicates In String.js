a79cc70f82c854fbec687ef7e5e77cf4675984df
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-12 10:15:16 +0800

    Add algo

---
+++ All/1047. Remove All Adjacent Duplicates In String.js
@@ -0,0 +1,7 @@
+// 1047. Remove All Adjacent Duplicates In String
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
