1816e8e279f5c34c2845ff7642eb10006d050719
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-13 22:08:00 +0800

    Add algo

--- All/1037. Valid Boomerang.js
+++ All/1037. Valid Boomerang.js
@@ -31,3 +31,15 @@
  * @return {boolean}
  */
 // O(1), O(1)
+var isBoomerang = function (points) {
+    const [x1, y1] = points[0];
+    const [x2, y2] = points[1];
+    const [x3, y3] = points[2];
+
+    // Calculate the area of the triangle using the determinant method
+    const area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+
+
+    return area !== 0;
+};
+
