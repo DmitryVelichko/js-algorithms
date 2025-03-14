b7c34b9b0c9bbab2edf5d48b22313946e3cc875f
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-14 20:46:31 +0800

    Add algo

---
+++ All/1021. Remove Outermost Parentheses.js
@@ -0,0 +1,7 @@
+// 1021. Remove Outermost Parentheses
+// Easy
+// Topics
+// Companies
+// Hint
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
