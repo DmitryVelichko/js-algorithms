a503810f527caf0a4cfc56ea69384ab6df766c04
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-20 21:19:25 +0800

    Add algo

--- All/257-binary-tree-paths.js
+++ All/257-binary-tree-paths.js
@@ -31,3 +31,26 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+
+var binaryTreePaths = function(root) {
+    let paths = [];
+    
+    function dfsTraversal(root, cur) {
+        if (!root) return;
+        if (!root.left && !root.right) {
+            paths.push(cur + root.val);
+            return;
+        }
+        dfsTraversal(root.left, cur + root.val + "->");
+        dfsTraversal(root.right, cur + root.val + "->");
+    }
+    
+    dfsTraversal(root, "");
+    return paths;
+    // Time Complexity: O(N), we always visit all nodes
+    // Space Complexity: O(H) or O(N), height can be at most N (in case of a skewed tree)
+};
