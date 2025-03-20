e2272f1a32b90221fc7fcf1f92b3377b2f654f6e
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-20 21:19:20 +0800

    Add algo

--- All/257-binary-tree-paths.js
+++ All/257-binary-tree-paths.js
@@ -18,3 +18,16 @@
 // Output: ["1"]
  
 
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
