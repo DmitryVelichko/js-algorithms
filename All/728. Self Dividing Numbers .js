72f11abd886bad7102d5ce4a136ad6db2304693d
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-21 10:33:19 +0800

    Add algo

--- All/728. Self Dividing Numbers .js
+++ All/728. Self Dividing Numbers .js
@@ -11,7 +11,7 @@
 
 // Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
 
- 
+
 
 // Example 1:
 
@@ -21,7 +21,7 @@
 
 // Input: left = 47, right = 85
 // Output: [48,55,66,77]
- 
+
 
 // Constraints:
 
@@ -40,3 +40,34 @@
 // Companies
 // Hint 1
 // For each number in the range, check whether it is self dividing by converting that number to a character array (or string in Python), then checking that each digit is nonzero and divides the original number.
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+// The inner loop iterates over the digits of each number. The number of digits is at most 5 (since right ≤ 10,000).
+// In the worst case, every number has 5 digits, so the inner loop runs O(d) ≈ O(5) = O(1).
+// 👉 Total Time Complexity = O(n × d) ≈ O(n) (since d is at most 5, it's treated as a constant).
+
+// O(n), O(n)
+var selfDividingNumbers = function (left, right) {
+    let res = []
+    for (let i = left; i <= right; i++) {
+        let num = i
+        if (num < 10 && num > 0) {
+            res.push(num);
+            continue;
+        }
+        let arr = num.toString().split('')
+        let isSelfDividingflag = true
+        for (let digit of arr) {
+            let digitNum = Number(digit)
+            if (digitNum === 0 || num % digitNum !== 0) {
+                isSelfDividingflag = false;
+                break;
+            }
+        }
+        if (isSelfDividingflag) res.push(num)
+    }
+    return res
+};
