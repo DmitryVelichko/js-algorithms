8318557d7c18ad04b41c30934b5cf0488f35e9dc
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-24 12:50:09 +0800

    Add algo

--- All/509. Fibonacci Number.js
+++ All/509. Fibonacci Number.js
@@ -19,3 +19,16 @@
 // Example 2:
 
 // Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Example 3:
+
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+// Constraints:
+
+// 0 <= n <= 30
+
