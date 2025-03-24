5991622c4fa432ae58422a2ede95be9ebf61ee6f
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-24 12:50:21 +0800

    Add algo

--- All/509. Fibonacci Number.js
+++ All/509. Fibonacci Number.js
@@ -46,3 +46,11 @@ var fib = function (n) {
     return fib(n - 1) + fib(n - 2)
 };
 
+// O(n), O(n) = Memoized recursion
+var fib = function (n, memo = {}) {
+    if (n in memo) return memo[n]; // Return cached result
+    if (n <= 1) return n;
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+};
