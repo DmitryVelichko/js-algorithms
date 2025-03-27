02ae03146c1016ebd309e19f51e8ae50a5ed8c60
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-27 23:44:37 +0800

    Add algo

---
+++ All/150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,7 @@
+// 150. Evaluate Reverse Polish Notation
+// Solved
+// Medium
+// Topics
+// Companies
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
