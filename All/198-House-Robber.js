dca1b08a7c6290c0b337a68a98e1181834284755
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-28 22:07:28 +0800

    Add algo

--- All/198-House-Robber.js
+++ All/198-House-Robber.js
@@ -23,3 +23,27 @@
 // Total amount you can rob = 2 + 9 + 1 = 12.
  
 
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length === 0) return 0
+    if(nums.length === 1) return nums[0]
+    let dp = new Array(nums.length).fill(0)
+
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+
+    
+    for(let i=2; i<nums.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1])
+    }
+
+    return dp[dp.length - 1]
+};
