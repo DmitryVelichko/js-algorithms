1ee5b841431bdf14cc29a22de6eb96cb94e26db2
Dmitry Velichko <soleil-2005@mail.ru>
2025-03-30 11:27:45 +0800

    Add algo

--- All/66-plus-one.js
+++ All/66-plus-one.js
@@ -42,3 +42,16 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+// O(n), O(1)
+let plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    digits.unshift(1)
+    return digits;
+};
