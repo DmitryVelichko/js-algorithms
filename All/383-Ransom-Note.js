cf821bd63aebbb664373a60cf5ddd8ad896748d0
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-01 13:13:45 +0800

    Add algo

--- All/383-Ransom-Note.js
+++ All/383-Ransom-Note.js
@@ -13,3 +13,14 @@
 
 // Input: ransomNote = "aa", magazine = "ab"
 // Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+
+// Constraints:
+
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
