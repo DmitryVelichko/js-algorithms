7efb55d38ed13e3a6639ec0e1910ead448cf15dc
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-02 10:07:43 +0800

    Add algo

--- All/374-Guess-Number-Higher-or-Lower.js
+++ All/374-Guess-Number-Higher-or-Lower.js
@@ -16,7 +16,7 @@
 // 0: your guess is equal to the number I picked (i.e. num == pick).
 // Return the number that I picked.
 
- 
+
 
 // Example 1:
 
@@ -30,7 +30,7 @@
 
 // Input: n = 2, pick = 1
 // Output: 1
- 
+
 
 // Constraints:
 
@@ -45,3 +45,19 @@
  *               otherwise return 0
  * var guess = function(num) {}
  */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+    let l = 0, r = n - 1;
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let res = guess(mid);
+        if (res == 0) return mid;
+        else if (res == 1) l = mid + 1;
+        else r = mid - 1;
+    }
+    return l;
+};
