f7f5c1a1279bd7030c6e7b6dc5113c669b0e3aa3
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-02 10:07:38 +0800

    Add algo

--- All/374-Guess-Number-Higher-or-Lower.js
+++ All/374-Guess-Number-Higher-or-Lower.js
@@ -32,3 +32,16 @@
 // Output: 1
  
 
+// Constraints:
+
+// 1 <= n <= 231 - 1
+// 1 <= pick <= n
+
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
