c7098ca43d8c0697cef94f1016de2bb71e198313
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-02 10:07:16 +0800

    Add algo

--- All/374-Guess-Number-Higher-or-Lower.js
+++ All/374-Guess-Number-Higher-or-Lower.js
@@ -11,3 +11,14 @@
 
 // You call a pre-defined API int guess(int num), which returns three possible results:
 
+// -1: Your guess is higher than the number I picked (i.e. num > pick).
+// 1: Your guess is lower than the number I picked (i.e. num < pick).
+// 0: your guess is equal to the number I picked (i.e. num == pick).
+// Return the number that I picked.
+
+ 
+
+// Example 1:
+
+// Input: n = 10, pick = 6
+// Output: 6
