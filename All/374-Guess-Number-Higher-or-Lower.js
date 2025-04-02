7f2ef4548bdcfeb2d9187d98213a213fb79f6b27
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-02 10:07:07 +0800

    Add algo

---
+++ All/374-Guess-Number-Higher-or-Lower.js
@@ -0,0 +1,13 @@
+// 374. Guess Number Higher or Lower
+// Easy
+// 3.4K
+// 436
+// Companies
+// We are playing the Guess Game. The game is as follows:
+
+// I pick a number from 1 to n. You have to guess which number I picked.
+
+// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+// You call a pre-defined API int guess(int num), which returns three possible results:
+
