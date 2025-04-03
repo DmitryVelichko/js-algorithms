8999eaca9c1462eb18e8e632ca70b323d5bfcab2
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-03 16:04:38 +0800

    Add algo

--- All/3477. Fruits Into Baskets II.js
+++ All/3477. Fruits Into Baskets II.js
@@ -50,3 +50,23 @@
 // 1 <= fruits[i], baskets[i] <= 1000
 
 // O(n^2), O(1)
+var numOfUnplacedFruits = function(fruits, baskets) {
+    let unplaced = 0;
+   
+    for (let i = 0; i < fruits.length; i++) {
+        let placed = false;
+        for (let j = 0; j < baskets.length; j++) {
+            if (baskets[j] >= fruits[i]) {
+                baskets[j] = -1;  // Mark basket as used
+                placed = true;
+                break;
+            }
+        }
+
+        if (!placed) {
+            unplaced++;
+        }
+    }
+
+    return unplaced;
+};
