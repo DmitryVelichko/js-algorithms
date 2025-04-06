a79860495d9057ad7d19eb7963c71064c9fd4b92
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-06 11:51:32 +0800

    Add algo

--- All/3136. Valid Word.js
+++ All/3136. Valid Word.js
@@ -20,3 +20,13 @@
 // A consonant is an English letter that is not a vowel.
 
 
+// Example 1:
+
+// Input: word = "234Adas"
+
+// Output: true
+
+// Explanation:
+
+// This word satisfies the conditions.
+
