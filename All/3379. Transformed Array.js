e9fa0af01e964f32e743755b65c49fa1d7c52669
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-07 21:52:12 +0800

    Add algo

---
+++ All/3379. Transformed Array.js
@@ -0,0 +1,8 @@
+// 3379. Transformed Array
+// Attempted
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
+
