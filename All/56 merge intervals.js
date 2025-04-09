060acbfc157bfe0ca49b7e7fa1d07fdc96b08a0a
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-09 16:01:28 +0800

    Add algo

--- All/56 merge intervals.js
+++ All/56 merge intervals.js
@@ -17,3 +17,15 @@
 // Input: intervals = [[1,4],[4,5]]
 // Output: [[1,5]]
 // Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
