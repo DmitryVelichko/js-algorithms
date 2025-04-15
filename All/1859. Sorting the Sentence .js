10d89b030a5a15196835b05a89d7407f81792d40
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-15 11:30:07 +0800

    Add algo

---
+++ All/1859. Sorting the Sentence .js
@@ -0,0 +1,7 @@
+// 1859. Sorting the Sentence
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
