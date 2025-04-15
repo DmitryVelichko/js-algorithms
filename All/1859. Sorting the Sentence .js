c3e4fadb10f2c3ed7e112fa813d04213e49c0150
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-15 11:30:33 +0800

    Add algo

--- All/1859. Sorting the Sentence .js
+++ All/1859. Sorting the Sentence .js
@@ -48,3 +48,11 @@ var sortSentence = function (s) {
     }
     return res.join(' ')
 };
+
+// O(n + k log k), O(n)
+var sortSentence = function (s) {
+    return s.split(' ')
+        .sort((a, b) => a[a.length - 1] - b[b.length - 1])
+        .map((word) => word.slice(0, word.length - 1))
+        .join(' ');
+};
