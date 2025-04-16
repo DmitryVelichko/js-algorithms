bc5008e70eb2ffd1196f7a215997984845bda57c
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-16 11:20:59 +0800

    Add algo

---
+++ All/1854. Maximum Population Year.js
@@ -0,0 +1,7 @@
+// 1854. Maximum Population Year
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
