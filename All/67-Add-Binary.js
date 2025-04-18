4cb11c6879dcf262016c289e37a8c3fc7976ac3b
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-18 11:17:08 +0800

    Add algo

--- All/67-Add-Binary.js
+++ All/67-Add-Binary.js
@@ -23,3 +23,14 @@
 // a and b consist only of '0' or '1' characters.
 // Each string does not contain leading zeros except for the zero itself.
 
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+  };
