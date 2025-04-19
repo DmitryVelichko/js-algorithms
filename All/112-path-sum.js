3c61ac74168ad6382653d82961524a0aa895b5a7
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-19 12:42:31 +0800

    Add algo

--- All/112-path-sum.js
+++ All/112-path-sum.js
@@ -43,3 +43,17 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+
+    if (!root.left && !root.right) { // check leaf
+        return sum === root.val;
+    } else { // continue DFS
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+};
