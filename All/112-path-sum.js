4ea1879baa68edf468d9d9fafa3a0d609e396abb
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-19 12:42:26 +0800

    Add algo

--- All/112-path-sum.js
+++ All/112-path-sum.js
@@ -29,3 +29,17 @@
 // Explanation: Since the tree is empty, there are no root-to-leaf paths.
  
 
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5000].
+// -1000 <= Node.val <= 1000
+// -1000 <= targetSum <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
