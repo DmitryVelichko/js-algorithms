ac63a15824bfda160d50ed97c955ea0be6d9fade
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-23 12:50:04 +0800

    Add algo

--- All/1089. Duplicate Zeros.js
+++ All/1089. Duplicate Zeros.js
@@ -31,3 +31,30 @@
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
+// O(n^2), O(1)
+var duplicateZeros = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            arr.pop();
+            i++
+        }
+    }
+};
+// O(n), O(1)
+var duplicateZeros = function (arr) {
+    let zeroes = arr.filter(x => x === 0).length;
+
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (i + zeroes < arr.length) {
+            arr[i + zeroes] = arr[i];
+        }
+        if (arr[i] === 0) {
+            zeroes--;
+            if (i + zeroes < arr.length) {
+                arr[i + zeroes] = 0;
+            }
+        }
+    }
+};
