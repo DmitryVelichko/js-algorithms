ff0f39d56531c19ea4dd9277fc8b3d8f46a52aac
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-26 11:37:55 +0800

    Add algo

---
+++ All/414. Third Maximum Number.js
@@ -0,0 +1,5 @@
+// 414. Third Maximum Number
+// Easy
+
+// Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
