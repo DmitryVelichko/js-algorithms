0db7bf6dd2fcdf3aab9f730c49ad2cc40faf0970
Dmitry Velichko <soleil-2005@mail.ru>
2025-04-28 12:28:22 +0800

    Add algo

--- All/414. Third Maximum Number.js
+++ All/414. Third Maximum Number.js
@@ -67,3 +67,18 @@ var thirdMax = function (nums) {
     return (max3 === -Infinity) ? max1 : max3
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+    if (nums.length < 3) return Math.max(...nums);
+    let u = new Set(nums)
+    if (u.size < 3) return Math.max(...u);
+
+    for (let i = 0; i < 2; i++) {
+        u.delete(Math.max(...u))
+    }
+
+    return Math.max(...u);
+};
