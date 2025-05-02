9fd9d4fcd070f21195864e4a61be359d2fb5c46b
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-02 13:06:11 +0800

    Add algo

--- All/394-decode-string.js
+++ All/394-decode-string.js
@@ -23,3 +23,20 @@
 // Input: s = "2[abc]3[cd]ef"
 // Output: "abcabccdcdcdef"
 
+// Constraints:
+
+// 1 <= s.length <= 30
+// s consists of lowercase English letters, digits, and square brackets '[]'.
+// s is guaranteed to be a valid input.
+// All the integers in s are in the range [1, 300].
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+    let multiply = []
+    let tempMult = ''
+    let repeatStr = []
+    let solution = ''
+
