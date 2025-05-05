7549b31b70d2c34454a878236fa5c26dceaebe36
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-05 19:50:51 +0800

    Add algo

--- All/451. Sort Characters By Frequency.js
+++ All/451. Sort Characters By Frequency.js
@@ -38,3 +38,20 @@
  * @param {string} s
  * @return {string}
  */
+var frequencySort = function(s) {
+    const map = new Map()
+    let res = ''
+    
+    for(const char of s) {
+        map.set(char, (map.get(char) || 0) + 1)
+    }
+
+    const obj = Array.from(map.entries())
+    obj.sort((a,b)=>b[1]-a[1])
+
+    for(const [char, num] of obj) {
+        res+=char.repeat(num)
+    }
+
+    return res
+};
