b6f664275a0b0b849e113af5e8c753ebbe982e3f
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-07 23:23:56 +0800

    Add algo

--- All/13-fizz-buzz.js
+++ All/13-fizz-buzz.js
@@ -8,3 +8,18 @@ answer[i] == "Fizz" if i is divisible by 3.
 answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.
  */
+var fizzBuzz = function (n) {
+  arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i.toString());
+    }
+  }
+  return arr;
+};
