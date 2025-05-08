afa87d7b6ef50fd60fba4f7e81af786b1e8b0493
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-08 12:07:57 +0800

    Add algo

--- All/169. Majority Element .js
+++ All/169. Majority Element .js
@@ -32,3 +32,18 @@
  * @param {number[]} nums
  * @return {number}
  */
+// O(n^2), O(1)
+var majorityElement = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] === nums[i]) {
+                count++;
+            }
+        }
+        if (count > Math.floor(nums.length / 2)) {
+            return nums[i];
+        }
+    }
+};
+
