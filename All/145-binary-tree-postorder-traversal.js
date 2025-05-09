64406e2fd50d84fb5275d1ee243a664988fd2d67
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-09 23:22:56 +0800

    Add algo

--- All/145-binary-tree-postorder-traversal.js
+++ All/145-binary-tree-postorder-traversal.js
@@ -27,3 +27,28 @@
 // The number of the nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if(!root) return []
+    let res = []
+    function traverse(node) {
+        if(node === null) return
+        if(node.left) traverse(node.left)
+        if(node.right) traverse(node.right)
+        res.push(node.val)
+    }
+
+    traverse(root)
+    return res
+};
