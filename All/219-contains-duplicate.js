c8596419ee920ea3c41a2692fbfd54a1f5e897f1
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-10 21:33:10 +0800

    Add algo

--- All/219-contains-duplicate.js
+++ All/219-contains-duplicate.js
@@ -15,3 +15,26 @@
 // Input: nums = [1,2,3,1,2,3], k = 2
 // Output: false
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    const map = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            const j = map.get(nums[i])
+
+            if (Math.abs(j - i) <= k) {
+                return true
+            }
+        }
+        map.set(nums[i], i)
+    }
+
+    return false
+};
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3))
