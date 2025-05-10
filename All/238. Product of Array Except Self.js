f096c4ddb5bd17ece8c34f42013a1d45f59bae16
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-10 21:35:38 +0800

    Add algo

--- All/238. Product of Array Except Self.js
+++ All/238. Product of Array Except Self.js
@@ -30,3 +30,30 @@
 
 // Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+ // Задача на Arrays and Hashing
+ // 
+ // T: O(n), S:O(1) result array не считается по условиям задачи
+ var productExceptSelf = function(nums) {
+    const res = [];
+    let start = 1;
+    let start2 = 1
+    // Перемножаем между собой числа слева направо кроме последнего и пушим в массив
+    for (let i = 0; i < nums.length; i++) {
+        res.push(start)
+        start = start * nums[i]
+    }
+        // Перемножваем между собой числа справо налево кроме первого и 
+        // перемножваем с числами в массиве
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] = start2 * res[i]
+        start2 = start2 * nums[i]
+    }
+    
+    return res;
+};
