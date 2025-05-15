80b4517d1b131a348f65a14d8ba315954bf78cfd
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-15 22:31:37 +0800

    Add algo

--- All/228-summary-ranges.js
+++ All/228-summary-ranges.js
@@ -22,3 +22,21 @@
 // [0,2] --> "0->2"
 // [4,5] --> "4->5"
 // [7,7] --> "7"
+// Example 2:
+
+// Input: nums = [0,2,3,4,6,8,9]
+// Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 20
+// -231 <= nums[i] <= 231 - 1
+// All the values of nums are unique.
+// nums is sorted in ascending order.
+
