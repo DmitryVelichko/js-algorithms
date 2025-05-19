4315accda652ac14e905ef5981d0a95398f311db
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-19 22:08:51 +0800

    Add algo

--- All/202-happy-number.js
+++ All/202-happy-number.js
@@ -25,3 +25,16 @@
 // Input: n = 2
 // Output: false
 
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    const checked = [];
+    while (n !== 1) {
+        if (checked.includes(n)) return false;
+        checked.push(n);
+        n = n.toString().split('').map(i => i**2).reduce((sum, i) => sum + i, 0);
+    }
+    return true;
+};
