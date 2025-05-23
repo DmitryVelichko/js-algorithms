cfceec9cde78bd4bf0f7d6c59a56c1652ab2278e
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-23 18:13:39 +0800

    Add algo

---
+++ All/303-Range-Sum-Query-Immutable.js
@@ -0,0 +1,7 @@
+// 303. Range Sum Query - Immutable
+// Easy
+
+// Given an integer array nums, handle multiple queries of the following type:
+
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
