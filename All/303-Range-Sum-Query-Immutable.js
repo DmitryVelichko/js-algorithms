53bd223f193a9dea0370ee8d52c0d166f2cdfcb6
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-23 18:16:05 +0800

    Add algo

--- All/303-Range-Sum-Query-Immutable.js
+++ All/303-Range-Sum-Query-Immutable.js
@@ -5,3 +5,15 @@
 
 // Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
 // Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+// Example 1:
+
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
