5359f50bf74648c3de7b92f6b4580eb4adc94b16
Dmitry Velichko <soleil-2005@mail.ru>
2025-05-31 12:20:00 +0800

    Add algo

--- All/504-base7.js
+++ All/504-base7.js
@@ -17,3 +17,20 @@
 
 // -107 <= num <= 107
 
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    if(num === 0) return '0';
+    
+    let temp = Math.abs(num);
+    let res = '';
+    
+    while(temp !== 0){
+        res = temp % 7 + res;
+        temp = Math.floor(temp / 7);
+    }
+    
+    return num >= 0 ? res : `-${res}`;
+};
