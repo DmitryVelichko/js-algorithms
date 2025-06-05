f4e93a4a254e2bf18335efabcf8816e30080fc9e
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-05 22:19:16 +0800

    Add algo

--- All/26-remove-duplicates from-sorted-arr.js
+++ All/26-remove-duplicates from-sorted-arr.js
@@ -44,3 +44,24 @@
 // -100 <= nums[i] <= 100
 // nums is sorted in non-decreasing order.
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != nums[i]) 
+            nums[++i] = nums[j];
+    }
+    return ++i;
+};
+// var removeDuplicates = function(nums) {
+//    let index = 1
+//    for(let i=0; i<nums.length-1; i++){
+//        if(nums[i] !== nums[i+1]) nums[index] = nums[i+1]
+//        index++
+//    }
+//    return index
+// };
