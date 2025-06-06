286647c697627a09c71196e8e63beb756dbccc0a
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-06 22:19:16 +0800

    Add algo

--- All/14-longets-common-prefix.js
+++ All/14-longets-common-prefix.js
@@ -18,3 +18,34 @@
 // Input: strs = ["dog","racecar","car"]
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
+
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters if it is non-empty.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+//n = number of strings in the array
+// m = length of the first string (prefix starts as this)
+// O(n*m), O(m)
+
+//Or O(n), O(1)
+var longestCommonPrefix = function (strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+};
