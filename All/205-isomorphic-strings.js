6e9cb7be751adbd30783ce244c4e0353145348b8
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-12 11:14:09 +0800

    Add algo

--- All/205-isomorphic-strings.js
+++ All/205-isomorphic-strings.js
@@ -7,3 +7,12 @@
 
 // All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
