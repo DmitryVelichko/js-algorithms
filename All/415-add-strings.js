3ae3fb8e57dd96b33b064aecaa8781f22b45920d
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-18 22:45:00 +0800

    Add algo

--- All/415-add-strings.js
+++ All/415-add-strings.js
@@ -22,3 +22,35 @@
 // Input: num1 = "0", num2 = "0"
 // Output: "0"
  
+
+// Constraints:
+
+// 1 <= num1.length, num2.length <= 104
+// num1 and num2 consist of only digits.
+// num1 and num2 don't have any leading zeros except for the zero itself.
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let sum = "";
+    let carry = 0;
+    
+    while( i >= 0 || j >= 0 || carry > 0){
+        const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
+        const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
+        const digitSum = digit1 + digit2 + carry;
+        
+        sum = (digitSum >= 10 ? digitSum % 10 : digitSum) + sum;
+        carry = digitSum >= 10 ? 1 : 0;
+        
+        i--;
+        j--;
+    }
+    
+    return sum;
+};
