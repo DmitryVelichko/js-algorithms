f0e6879699ee0a6efe37093c894361fb020af632
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-19 21:54:56 +0800

    Add algo

--- All/347 - top-k-frequent-elements.js
+++ All/347 - top-k-frequent-elements.js
@@ -27,3 +27,26 @@
 
 // Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+/**
+   дан массив чисел, вернуть массив k чисел, которые
+   встречаются чаще всего 
+   
+   ( Bucket sort [[], [], []])
+
+   hashmap
+   массив результатов
+   const arr = Array.from({ length: nums.length + 1 }, () => []); // [_, _,_, [1]]
+
+   for по nums- считаем частотность чисел - {1:3}
+  
+   for по hash -добавляем в arr числа по частоте- [[ ], [2], [3]]
+
+   for в обратно направлении
+       for по массиву
+           пушим в результат
+           длина массива === k ? return res
