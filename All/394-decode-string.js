bc4081c15595e74d1288f9b1586a0608f102b2a1
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-21 13:04:15 +0800

    Add algo

--- All/394-decode-string.js
+++ All/394-decode-string.js
@@ -20,3 +20,17 @@
 // Output: "accaccacc"
 // Example 3:
 
+// Input: s = "2[abc]3[cd]ef"
+// Output: "abcabccdcdcdef"
+
+// Constraints:
+
+// 1 <= s.length <= 30
+// s consists of lowercase English letters, digits, and square brackets '[]'.
+// s is guaranteed to be a valid input.
+// All the integers in s are in the range [1, 300].
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
