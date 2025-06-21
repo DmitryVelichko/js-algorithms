9f67f85be7391ceafcccb73b04180fdef0f0c785
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-21 13:04:19 +0800

    Add algo

--- All/394-decode-string.js
+++ All/394-decode-string.js
@@ -34,3 +34,26 @@
  * @param {string} s
  * @return {string}
  */
+var decodeString = function (s) {
+    let multiply = []
+    let tempMult = ''
+    let repeatStr = []
+    let solution = ''
+
+    for (let char of s) {
+        if (!isNaN(char)) {
+            tempMult = `${tempMult}${char}`
+        } else if (char === '[') {
+            multiply.push(tempMult)
+            tempMult = ''
+
+            repeatStr.push(solution)
+            solution = ''
+        } else if (char === ']') {
+            solution = repeatStr.pop() + solution.repeat(multiply.pop())
+        } else {
+            solution += char
+        }
+    }
+    return solution
+};
