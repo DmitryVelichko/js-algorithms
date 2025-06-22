69348e54c9fd36dc2a6bf25dcfdf10f1d569c217
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-22 22:00:55 +0800

    Add algo

--- All/401-binary-watch.js
+++ All/401-binary-watch.js
@@ -14,7 +14,7 @@
 // The minute must consist of two digits and may contain a leading zero.
 
 // For example, "10:2" is not valid. It should be "10:02".
- 
+
 
 // Example 1:
 
@@ -24,9 +24,22 @@
 
 // Input: turnedOn = 9
 // Output: []
- 
+
 
 // Constraints:
 
 // 0 <= turnedOn <= 10
 
+var readBinaryWatch = function (num) {
+  const times = [];
+  for (let h = 0; h < 12; h++) {
+    for (let m = 0; m < 60; m++) {
+      const hOnes = h ? h.toString(2).match(/1/g).length : 0;
+      const mOnes = m ? m.toString(2).match(/1/g).length : 0;
+      if (hOnes + mOnes === num) {
+        times.push(`${h}:${m < 10 ? `0${m}` : m}`);
+      }
+    }
+  }
+  return times;
+};
