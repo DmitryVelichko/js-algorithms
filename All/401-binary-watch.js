054707508203d7387048452165b66c4c1a53573a
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-22 22:00:38 +0800

    Add algo

---
+++ All/401-binary-watch.js
@@ -0,0 +1,10 @@
+// 401. Binary Watch
+// Easy
+
+// A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+
+// For example, the below binary watch reads "4:51".
+
+
+// Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
+
