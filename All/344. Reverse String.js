127d68da9e81320f82c5f056f3a1cb1dbc913d07
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-23 13:24:08 +0800

    Add algo

--- All/344. Reverse String.js
+++ All/344. Reverse String.js
@@ -29,3 +29,17 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+
+//O(n), O(1)
+var reverseString = function (s) {
+    let l = 0
+    let r = s.length - 1
+    while (l < r) {
+        let temp = s[r]
+        s[r] = s[l]
+        s[l] = temp
+        l++
+        r--
+    }
+};
+
