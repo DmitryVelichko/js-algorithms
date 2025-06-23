d4a4d7a284403b4cb950319d0ccb91ea82e2bd2a
Dmitry Velichko <soleil-2005@mail.ru>
2025-06-23 13:24:02 +0800

    Add algo

--- All/344. Reverse String.js
+++ All/344. Reverse String.js
@@ -9,3 +9,23 @@
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
