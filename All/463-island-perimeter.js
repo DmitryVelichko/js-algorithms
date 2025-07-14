e182d04ce12bba861b7a3bb12be2d0b0773e52bd
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-14 23:54:45 +0800

    Algorithms practice

--- All/463-island-perimeter.js
+++ All/463-island-perimeter.js
@@ -8,3 +8,28 @@
 // The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
  
+
+// Example 1:
+
+
+// Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+// Output: 16
+// Explanation: The perimeter is the 16 yellow stripes in the image above.
+// Example 2:
+
+// Input: grid = [[1]]
+// Output: 4
+// Example 3:
+
+// Input: grid = [[1,0]]
+// Output: 4
+ 
+
+// Constraints:
+
+// row == grid.length
+// col == grid[i].length
+// 1 <= row, col <= 100
+// grid[i][j] is 0 or 1.
+// There is exactly one island in grid.
+
