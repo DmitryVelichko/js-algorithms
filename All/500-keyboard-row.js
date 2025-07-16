9a194d8a42141e0e286a90e8a98de1e0c1e472ee
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-16 22:12:55 +0800

    Algorithms practice

--- All/500-keyboard-row.js
+++ All/500-keyboard-row.js
@@ -11,3 +11,23 @@
 
 
 
+// Example 1:
+
+// Input: words = ["Hello","Alaska","Dad","Peace"]
+// Output: ["Alaska","Dad"]
+// Example 2:
+
+// Input: words = ["omk"]
+// Output: []
+// Example 3:
+
+// Input: words = ["adsdf","sfd"]
+// Output: ["adsdf","sfd"]
+
+
+// Constraints:
+
+// 1 <= words.length <= 20
+// 1 <= words[i].length <= 100
+// words[i] consists of English letters (both lowercase and uppercase).
+
