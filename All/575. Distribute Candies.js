26d5e18cc9f91d06e39805d131ada3702bd24cbe
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-19 21:52:42 +0800

    Algorithms practice

--- All/575. Distribute Candies.js
+++ All/575. Distribute Candies.js
@@ -23,3 +23,23 @@
 // Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
 // Example 3:
 
+// Input: candyType = [6,6,6,6]
+// Output: 1
+// Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 candies, she only has 1 type.
+ 
+
+// Constraints:
+
+// n == candyType.length
+// 2 <= n <= 104
+// n is even.
+// -105 <= candyType[i] <= 105
+
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+   
+    return Math.min(new Set(candyType).size, candyType.length / 2)
+};
