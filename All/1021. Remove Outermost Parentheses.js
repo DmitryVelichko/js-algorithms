1c4d9ef65dd503e75b6a0afdf9067310cd51e4b5
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-22 20:17:11 +0300

    Algorithms practice

--- All/1021. Remove Outermost Parentheses.js
+++ All/1021. Remove Outermost Parentheses.js
@@ -47,3 +47,24 @@
  * @param {string} s
  * @return {string}
  */
+// O(n), O(n)
+var removeOuterParentheses = function (s) {
+    let result = "";
+    let balance = 0;
+
+    for (let char of s) {
+        if (char === '(') {
+            if (balance > 0) {
+                result += char;
+            }
+            balance++;
+        } else {
+            balance--;
+            if (balance > 0) {
+                result += char;
+            }
+        }
+    }
+
+    return result;
+};
