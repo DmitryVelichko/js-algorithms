9509fed985daefef4b8970df4d4205299cc6043e
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-25 14:40:08 +0300

    Algorithms practice

---
+++ All/.42. Trapping Rain Water.js
@@ -0,0 +1,8 @@
+// 42. Trapping Rain Water
+// Solved
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
