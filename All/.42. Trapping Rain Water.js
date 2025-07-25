c61ec8293c3d4ec3995809e1c222e6b04602eaae
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-25 14:40:30 +0300

    Algorithms practice

--- All/.42. Trapping Rain Water.js
+++ All/.42. Trapping Rain Water.js
@@ -31,3 +31,26 @@
  * @return {number}
  */
 //O(n), O(1)
+var trap = function (height) {
+
+    let l = 0
+    let r = height.length - 1
+    let leftMax = height[l]
+    let rightMax = height[r]
+    let res = 0
+
+    if (height.length === 0) return 0
+
+    while (l < r) {
+        if (leftMax < rightMax) {
+            l++
+            leftMax = Math.max(leftMax, height[l])
+            res += leftMax - height[l]
+        } else {
+            r--
+            rightMax = Math.max(rightMax, height[r])
+            res += rightMax - height[r]
+        }
+    }
+    return res
+}
