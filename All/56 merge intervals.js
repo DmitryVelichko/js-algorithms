399a9328c857998057b231110b613f25733a028b
Dmitry Velichko <soleil-2005@mail.ru>
2025-07-28 22:18:00 +0300

    Algorithms practice

--- All/56 merge intervals.js
+++ All/56 merge intervals.js
@@ -25,5 +25,23 @@
 // intervals[i].length == 2
 // 0 <= starti <= endi <= 104
 
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+// O(n log n), O(n)
+var merge = function (intervals) {
+    if (intervals.length < 2) return intervals
+    intervals.sort((a, b) => a[0] - b[0])
+    let result = [intervals[0]]
 
+    for (let interval of intervals) {
+        let recent = result[result.length - 1]
+        if (recent[1] >= interval[0]) {
+            recent[1] = Math.max(recent[1], interval[1])
+        } else {
+            result.push(interval)
+        }
+    }
+    return result
 };
