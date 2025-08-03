771ed7c0e1497749ce8ffbb202d93aac3afd3210
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-03 22:22:14 +0300

    Algorithms practice

--- All/853. Car Fleet.js
+++ All/853. Car Fleet.js
@@ -18,3 +18,20 @@
 
 
 
+// Example 1:
+
+// Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+
+// Output: 3
+
+// Explanation:
+
+// The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12. The fleet forms at target.
+// The car starting at 0 (speed 1) does not catch up to any other car, so it is a fleet by itself.
+// The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+// Example 2:
+
+// Input: target = 10, position = [3], speed = [3]
+
+// Output: 1
+
