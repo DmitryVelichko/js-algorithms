2b5613c410f1fce348499045e9dbf14fdd25ec82
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-04 19:15:28 +0300

    Algorithms practice

--- All/50-pow-x-n.js
+++ All/50-pow-x-n.js
@@ -18,3 +18,20 @@
 // Explanation: 2-2 = 1/22 = 1/4 = 0.25
 
 
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+    if (n === 0) return 1;
+
+    let pow = Math.abs(n);
+
+    let result = pow % 2 === 0 ? myPow(x * x, pow / 2) : myPow(x * x, (pow - 1) / 2) * x;
+    console.log(`Pow: ${pow}, result: ${result}`)
+
+    return n < 0 ? 1 / result : result;
+};
+
+console.log(myPow(2, 3))
