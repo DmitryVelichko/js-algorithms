3e01040b16206e22d0533cfe9b471d61de6c1e18
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-07 21:00:37 +0300

    Algorithms practice

--- All/35. Search Insert Position.js
+++ All/35. Search Insert Position.js
@@ -36,3 +36,15 @@
  * @param {number} target
  * @return {number}
  */
+// O(log n), O(1)
+var searchInsert = function (nums, target) {
+    let l = 0
+    let r = nums.length - 1
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2)
+        if (nums[m] === target) return m
+        else if (nums[m] > target) r = m - 1
+        else l = m + 1
+    }
+    return l
+};
