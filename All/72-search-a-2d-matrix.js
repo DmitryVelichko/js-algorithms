87c33b8fdfb75c067799ff5ed95ba2f2068ac318
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-09 15:17:31 +0300

    Algorithms practice

--- All/72-search-a-2d-matrix.js
+++ All/72-search-a-2d-matrix.js
@@ -14,7 +14,7 @@
 
 // You must write a solution in O(log(m * n)) time complexity.
 
-  
+
 
 // Example 1:
 
@@ -26,7 +26,7 @@
 
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
- 
+
 
 // Constraints:
 
@@ -34,3 +34,28 @@
 // n == matrix[i].length
 // 1 <= m, n <= 100
 // -104 <= matrix[i][j], target <= 104
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// O(log(m*n)), O(1) -  one binary search over a virtual 1D array
+var searchMatrix = function (matrix, target) {
+    let rows = matrix.length
+    let cols = matrix[0].length
+    let l = 0
+    let r = rows * cols - 1
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        let row = Math.floor(m / cols)
+        let col = m % cols
+        if (target > matrix[row][col]) {
+            l = m + 1
+        } else if (target < matrix[row][col]) {
+            r = m - 1
+        } else {
+            return true
+        }
+    }
+    return false
+};
