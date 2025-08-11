803d3f27a0c8e5b515995a7ef26ab301715613c7
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-11 19:22:13 +0300

    Algorithms practice

--- All/875. Koko Eating Bananas.js
+++ All/875. Koko Eating Bananas.js
@@ -41,3 +41,22 @@
  */
 
 // O(piles.length * log(Math.max(piles))) === O(n * log(MaxPile)), O(1)
+var minEatingSpeed = function (piles, h) {
+    let l = 1
+    let r = Math.max(...piles)
+    let res = r
+    while (l <= r) {
+        let k = Math.floor((l + r) / 2)
+        let hours = 0
+        for (let pile of piles) {
+            hours += Math.ceil(pile / k)
+        }
+        if (hours <= h) {
+            res = Math.min(res, k)
+            r = k - 1
+        } else {
+            l = k + 1
+        }
+    }
+    return res
+};
