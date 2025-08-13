22f1d4b855645e2d70f76571982c741c6b99ac73
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-13 18:40:08 +0300

    Algorithms practice

--- All/v0-104-max-depth-of-binary-tree copy.js
+++ All/v0-104-max-depth-of-binary-tree copy.js
@@ -23,3 +23,26 @@
 // The number of nodes in the tree is in the range [0, 104].
 // -100 <= Node.val <= 100
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+const maxDepth = (root) => {
+    if(!root) return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+
+
+
+
+
