a8a450f0e861eb918e67e5f4a2ba80fc39638828
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-15 17:16:31 +0300

    Algorithms practice

--- 17. Math & Geometry/66-plus-one.js
+++ 17. Math & Geometry/66-plus-one.js
@@ -25,3 +25,16 @@
 // Thus, the result should be [4,3,2,2].
 // Example 3:
 
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+ 
+
+// Constraints:
+
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+// digits does not contain any leading 0's.
+
