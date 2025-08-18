55b26672132d62abb794331c2cc6b69ff37a6185
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-18 13:19:09 +0300

    Algorithms practice

--- All/5-longest-palindromic-substring.js
+++ All/5-longest-palindromic-substring.js
@@ -18,3 +18,31 @@
 // 1 <= s.length <= 1000
 // s consist of only digits and English letters.
 
+/**
+ * @param {string} s
+ * @return {string}
+*/
+const longestPalindrome = function (str) {
+    if (str.length < 1 || str === null) return '';
+    let longest = ''
+
+    const expandFromCenter = (str, left, right) => {
+        let i = 0;
+        while (str[left - i] && str[left - i] === str[right + i]) {
+            i++
+        }
+        i--
+
+        return str.slice(left - i, right + i + 1)
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let oddPalindrome = expandFromCenter(str, i, i)
+        let evenPalindrome = expandFromCenter(str, i - 1, i)
+
+        if (oddPalindrome.length > longest.length) longest = oddPalindrome
+        if (evenPalindrome.length > longest.length) longest = evenPalindrome
+
+    }
+    return longest
+};
