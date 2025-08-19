b560beab076e8b36396c0ad82f7f31752be55e72
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-19 11:12:56 +0300

    Algorithms practice

---
+++ All/2629-Function-Composition.js
@@ -0,0 +1,7 @@
+// 2629. Function Composition
+// Easy
+// 373
+// 19
+// Companies
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
