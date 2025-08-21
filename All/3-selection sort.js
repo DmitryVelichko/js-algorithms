00c4b936dea0ecb8ab449da7aa4c764717f0e453
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-21 12:59:09 +0300

    Algorithms practice

---
+++ All/3-selection sort.js
@@ -0,0 +1,22 @@
+// Selecion Sort, Сортировка выбором - O(n^2)
+// Пробегаемся по массиву -> находим минимальный элемент -> меняем с первым элементом -> снова пробегаемся и находим минимальный 
+//элемент -> меняем со вторым элементом
+
+const arr = [-5, 23, 7, 5, 3, -12, -29, 21, 54, 35, 0];
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexMin = i
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[indexMin] > arr[j]) {
+                indexMin = j
+            }
+        }
+        let tmp = arr[i]
+        arr[i] = arr[indexMin]
+        arr[indexMin] = tmp
+    }
+    return arr
+}
+
+console.log(selectionSort(arr))
