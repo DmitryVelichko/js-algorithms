943a974ad8893b613b42d84a6c04625aff633031
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-28 19:00:39 +0300

    Algo practice

--- All/112-path-sum.js
+++ All/112-path-sum.js
@@ -28,3 +28,32 @@
 // Output: false
 // Explanation: Since the tree is empty, there are no root-to-leaf paths.
  
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5000].
+// -1000 <= Node.val <= 1000
+// -1000 <= targetSum <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+
+    if (!root.left && !root.right) { // check leaf
+        return sum === root.val;
+    } else { // continue DFS
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+};
