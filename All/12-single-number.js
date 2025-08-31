34592cb817aeaacc53a4b22ee88a7e86a4408453
Dmitry Velichko <soleil-2005@mail.ru>
2025-08-31 10:49:30 +0300

    Algo practice

--- All/12-single-number.js
+++ All/12-single-number.js
@@ -2,3 +2,20 @@
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+
+let singleNumber2 = function (nums) {
+    let ht = {}
+    for (const num of nums) {
+        ht[num] = (ht[num] || 0) + 1;
+    }
+
+    for (const key in ht) {
+        if (ht[key] === 1) {
+            return key;
+        }
+    }
+};
