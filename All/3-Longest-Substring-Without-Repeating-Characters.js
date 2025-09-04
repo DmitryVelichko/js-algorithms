d0788b2080cffb6cdff899d1419238534d7b644f
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-04 12:59:46 +0300

    Algo practice

--- All/3-Longest-Substring-Without-Repeating-Characters.js
+++ All/3-Longest-Substring-Without-Repeating-Characters.js
@@ -22,3 +22,23 @@
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let max = 0
+    let begin = 0
+    let set = new Set()
+
+    for (let end = 0; end < s.length; end++) {
+        while (set.has(s[end])) {
+            set.delete(s[begin])
+            begin = begin + 1
+        }
+        set.add(s[end])
+        max = Math.max(max, end - begin + 1)
+    }
+    return max
+};
