173aa8ec156ffc49dd3c846d66b149bf9bc61956
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-10 21:49:56 +0300

    Algo practice

--- All/58. Length of Last Word.js
+++ All/58. Length of Last Word.js
@@ -10,3 +10,61 @@
 //  consisting of non-space characters only.
 
 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// O(n), O(1)
+var lengthOfLastWord = function (s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    // Skip trailing spaces
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    // Count the characters of the last word
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+};
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// O(n), O(n)
+var lengthOfLastWord = function (s) {
+    const arr = s.trim().split(' ')
+    return arr[arr.length - 1].length
+};
