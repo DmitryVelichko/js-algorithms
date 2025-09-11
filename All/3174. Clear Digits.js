4403db0c51d5e9ecacd4f11533d64c67c2add317
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-11 22:01:28 +0300

    Algo practice

--- All/3174. Clear Digits.js
+++ All/3174. Clear Digits.js
@@ -42,3 +42,54 @@
 // 1 <= s.length <= 100
 // s consists only of lowercase English letters and digits.
 // The input is generated such that it is possible to delete all digits.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// O(n), O(n)
+var clearDigits = function (s) {
+    const stack = [];
+
+    for (const char of s) {
+        if (!isNaN(char)) {
+            // If it's a digit, remove the top character from the stack (if it exists)
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            // If it's not a digit, add it to the stack
+            stack.push(char);
+        }
+    }
+
+    // The stack contains the resulting characters
+    return stack.join('');
+};
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+//O(n^2), O(n)
+var clearDigits = function (s) {
+    if (!s || s.trim === '') return ''
+
+    const arr = s.split('')
+    let i = 0
+
+    while (i < arr.length) {
+        if (!isNaN(arr[i])) {
+            if (i > 0) {
+                arr.splice(i - 1, 2);
+                i = 0;
+            } else {
+                arr.splice(i, 1)
+            }
+        } else {
+            i++
+        }
+    }
+    return arr.join('')
+};
