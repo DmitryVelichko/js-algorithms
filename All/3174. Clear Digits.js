f6cfedc98feaa786f0e4c632fbc9a1df688bbc91
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-14 11:59:29 +0300

    Algo practice

--- All/3174. Clear Digits.js
+++ All/3174. Clear Digits.js
@@ -12,3 +12,28 @@
 // Return the resulting string after removing all digits.
 
 
+
+// Example 1:
+
+// Input: s = "abc"
+
+// Output: "abc"
+
+// Explanation:
+
+// There is no digit in the string.
+
+// Example 2:
+
+// Input: s = "cb34"
+
+// Output: ""
+
+// Explanation:
+
+// First, we apply the operation on s[2], and s becomes "c4".
+
+// Then we apply the operation on s[1], and s becomes "".
+
+
+
