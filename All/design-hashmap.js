0b02a71465b3de8afe104af1c633e942af809b37
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-16 11:08:34 +0300

    Algo practice

--- All/design-hashmap.js
+++ All/design-hashmap.js
@@ -9,3 +9,25 @@
 // int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
 // void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 
+var MyHashMap = function() {
+    this.hashMap = new Array(1000001)
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.hashMap[key] = value
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+   let value = this.hashMap[key]
+   return value !== undefined ? value : -1
+};
+
