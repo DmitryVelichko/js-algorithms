e9091bcc8e02534fa361897c814c9647a2eb836c
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-18 13:11:14 +0300

    Algo practice

--- All/15-3sum.js
+++ All/15-3sum.js
@@ -44,3 +44,36 @@
   скипаем дубликаты
 
 */
+// Time: O(n^2), Space: O(1) auxiliary + O(k) for output, where k is the number of triplets in the result.
+var threeSum = function (nums) {
+    const res = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        const a = nums[i];
+
+        // Skip duplicates
+        if (i > 0 && a === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r];
+            if (threeSum > 0) {
+                r--;
+            } else if (threeSum < 0) {
+                l++;
+            } else {
+                res.push([a, nums[l], nums[r]]);
+                l++;
+                r--;
+                while (nums[l] === nums[l - 1] && l < r) {
+                    l++;
+                }
+            }
+        }
+    }
+
+    return res;
+}
