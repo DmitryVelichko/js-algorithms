31a7a755ce3b73c0ea259de065c91f7f6eefef74
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-19 10:32:46 +0300

    Algo practice

--- All/2725-interval-cancellation.js
+++ All/2725-interval-cancellation.js
@@ -7,3 +7,53 @@
 
  
 
+// Example 1:
+
+// Input: fn = (x) => x * 2, args = [4], t = 35, cancelT = 190
+// Output: 
+// [
+//    {"time": 0, "returned": 8},
+//    {"time": 35, "returned": 8},
+//    {"time": 70, "returned": 8},
+//    {"time": 105, "returned": 8},
+//    {"time": 140, "returned": 8},
+//    {"time": 175, "returned": 8}
+// ]
+// Explanation: 
+// const cancel = cancellable(x => x * 2, [4], 35);
+// setTimeout(cancel, 190);
+
+// Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
+// 1st fn call is at 0ms. fn(4) returns 8.
+// 2nd fn call is at 35ms. fn(4) returns 8.
+// 3rd fn call is at 70ms. fn(4) returns 8.
+// 4th fn call is at 105ms. fn(4) returns 8.
+// 5th fn call is at 140ms. fn(4) returns 8.
+// 6th fn call is at 175ms. fn(4) returns 8.
+// Cancelled at 190ms
+// Example 2:
+
+// Input: fn = (x1, x2) => (x1 * x2), args = [2, 5], t = 30, cancelT = 165
+// Output: 
+// [
+//    {"time": 0, "returned": 10},
+//    {"time": 30, "returned": 10},
+//    {"time": 60, "returned": 10},
+//    {"time": 90, "returned": 10},
+//    {"time": 120, "returned": 10},
+//    {"time": 150, "returned": 10}
+// ]
+// Explanation: 
+// const cancel = cancellable((x1, x2) => (x1 * x2), [2, 5], 30); 
+// setTimeout(cancel, 165);
+
+// Every 30ms, fn(2, 5) is called. Until t=165ms, then it is cancelled.
+// 1st fn call is at 0ms 
+// 2nd fn call is at 30ms 
+// 3rd fn call is at 60ms 
+// 4th fn call is at 90ms 
+// 5th fn call is at 120ms 
+// 6th fn call is at 150ms
+// Cancelled at 165ms
+// Example 3:
+
