3def07a3edf993c178971857069a562696ded4b9
Dmitry Velichko <soleil-2005@mail.ru>
2025-09-20 11:53:58 +0300

    Algo practice

--- All/167-two-sum-2.js
+++ All/167-two-sum-2.js
@@ -32,3 +32,22 @@
  * @param {number} target
  * @return {number[]}
  */
+var twoSum = function(numbers, target) {
+    let lower = 0;
+    let upper = numbers.length - 1;
+
+    while(upper > lower) {
+        let sum = numbers[lower] + numbers[upper]
+        if(sum < target) {
+            lower +=1
+        } else if(sum > target) {
+            upper -=1
+        } else {
+            return [lower + 1, upper + 1]
+        }
+
+        
+    }
+};
+
+console.log(twoSum([2,7,11,15], 9))
