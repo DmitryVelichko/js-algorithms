5573ecb5487f914a0c5154159f27dac1e9ded7a1
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-09-21 10:41:26 +0300

    Algorithms practice

--- All/739. Daily Temperatures.js
+++ All/739. Daily Temperatures.js
@@ -18,3 +18,30 @@
 
 // Input: temperatures = [30,60,90]
 // Output: [1,1,0]
+
+// Constraints:
+
+// 1 <= temperatures.length <= 105
+// 30 <= temperatures[i] <= 100
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  let stack = [];
+  let result = new Array(temperatures.length).fill(0);
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let index = stack.pop();
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //[1,1,4,2,1,1,0,0]
