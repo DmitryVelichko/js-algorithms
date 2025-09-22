017eed7c0e4985c2fd0931a7dda32771bfecefd7
Dmitrii Velichko <soleil-2005@mail.ru>
2025-09-22 10:16:45 +0300

    Algorithms practice

--- All/236-lowest-common-ancestor-of-Binary-Tree.js
+++ All/236-lowest-common-ancestor-of-Binary-Tree.js
@@ -12,3 +12,29 @@
  *     this.left = this.right = null;
  * }
  */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  let result = null;
+
+  const dfs = (node) => {
+    if (node === null) return false;
+
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+
+    let cur = node === p || node === q;
+
+    if (left + right + cur >= 2) result = node;
+
+    return left || right || cur;
+  };
+
+  dfs(root);
+  return result;
+};
