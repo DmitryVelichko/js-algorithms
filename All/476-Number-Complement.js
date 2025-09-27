e516552b028bca8fe172face109e521662221333
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-09-27 10:14:45 +0300

    Algorithms practice

--- All/476-Number-Complement.js
+++ All/476-Number-Complement.js
@@ -8,3 +8,35 @@
 
  
 
+// Example 1:
+
+// Input: num = 5
+// Output: 2
+// Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+// Example 2:
+
+// Input: num = 1
+// Output: 0
+// Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+ 
+
+// Constraints:
+
+// 1 <= num < 231
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let convertedNum = num.toString(2)
+    let flippedNumArr = []
+    for(let char of convertedNum) {
+        if(char === '1') flippedNumArr.push(0)
+        else if(char === '0') flippedNumArr.push(1)
+    }
+    let flippedNum = flippedNumArr.join('')
+
+    let complementNum = parseInt(flippedNum, 2)
+    return complementNum
+};
