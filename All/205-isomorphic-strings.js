e4bbb4fc806119d0a740a973ebbbcd68e88fb004
Dmitrii Velichko <soleil-2005@mail.ru>
2025-09-28 10:11:49 +0300

    Algorithms practice

--- All/205-isomorphic-strings.js
+++ All/205-isomorphic-strings.js
@@ -28,3 +28,29 @@
 // t.length == s.length
 // s and t consist of any valid ascii character.
 
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length) return false // egg, add, e
+    let sMap = {} //e:a
+    let tMap = {}//a:e
+    for(let i = 0; i<s.length; i++) {
+        sChar = s[i] //e
+        tChar = t[i] //a
+
+        if(sMap[sChar] === undefined) {
+            sMap[sChar] = tChar;
+        }
+
+        if(tMap[tChar] === undefined) {
+            tMap[tChar] = sChar;
+        }
+
+        if(sMap[sChar] !== tChar || tMap[tChar] !== sChar) return false
+    }
+
+    return true
+};
