7b1934b73be950dc11afe718139aa35a7fc2732a
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-09-29 12:16:57 +0300

    Algorithms practice

--- All/69-sqrt(x).js
+++ All/69-sqrt(x).js
@@ -29,3 +29,29 @@
 
 // O(log x), O(1)
 
+var mySqrt = function (x) {
+
+    if (x === 0 || x === 1) return x;
+
+    let start = 1, end = x, mid;
+
+    while (start <= end) {
+        mid = Math.floor(start + (end - start) / 2);
+
+        if (mid * mid > x) {
+            end = mid - 1;
+        } else if (mid * mid === x) {
+            return mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return end;
+}
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
