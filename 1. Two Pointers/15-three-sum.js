25ae16d655b0e97c8b4067930082da4b095e625c
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-04 10:47:43 +0300

    Algorithms practice

--- 1. Two Pointers/15-three-sum.js
+++ 1. Two Pointers/15-three-sum.js
@@ -31,3 +31,45 @@
 // Explanation: The only possible triplet sums up to 0.
 
 
+// Constraints:
+
+// 3 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ // O(n^2), O(1)/ O(n) - зависит от ф-ции сортировки
+ var threeSum = function (nums) {
+    const res = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        const a = nums[i];
+
+        // Skip duplicates
+        if (i > 0 && a === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            const threeSum = a + nums[l] + nums[r];
+            if (threeSum > 0) {
+                r--;
+            } else if (threeSum < 0) {
+                l++;
+            } else {
+                res.push([a, nums[l], nums[r]]);
+                l++;
+                r--;
+                while (nums[l] === nums[l - 1] && l < r) {
+                    l++;
+                }
+            }
+        }
+    }
+
+    return res;
+}
