494cad0eb01446fdbd2ec5ae5ccf8d3a30c89091
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-05 10:04:16 +0300

    Algorithms practice

--- All/202-happy-number.js
+++ All/202-happy-number.js
@@ -11,3 +11,30 @@
 // Return true if n is a happy number, and false if not.
 
 
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    const checked = [];
+    while (n !== 1) {
+        if (checked.includes(n)) return false;
+        checked.push(n);
+        n = n.toString().split('').map(i => i**2).reduce((sum, i) => sum + i, 0);
+    }
+    return true;
+};
