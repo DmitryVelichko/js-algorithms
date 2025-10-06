19587a790f010a720edb6ceb5a3e3c1c06a59046
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-06 22:24:46 +0300

    Algorithms practice

--- All/739. Daily Temperatures.js
+++ All/739. Daily Temperatures.js
@@ -24,3 +24,24 @@
 // 1 <= temperatures.length <= 105
 // 30 <= temperatures[i] <= 100
 
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  let stack = [];
+  let result = new Array(temperatures.length).fill(0);
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let index = stack.pop();
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //[1,1,4,2,1,1,0,0]
