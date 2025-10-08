6f41587975574fa926a16d816e02cc44a2c423a1
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-08 10:23:28 +0300

    Algorithms practice

--- JS Interview Questions/2723. Add Two Promises.js
+++ JS Interview Questions/2723. Add Two Promises.js
@@ -3,7 +3,7 @@
 // Easy
 // Companies
 // Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
- 
+
 
 // Example 1:
 
@@ -19,9 +19,23 @@
 // promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
 // Output: -2
 // Explanation: The two input promises resolve with the values of 10 and -12 respectively. The returned promise should resolve with a value of 10 + -12 = -2.
- 
+
 
 // Constraints:
 
 // promise1 and promise2 are promises that resolve with a number
 
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+    const [val1, val2] = await Promise.all([promise1, promise2])
+    return val1 + val2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
