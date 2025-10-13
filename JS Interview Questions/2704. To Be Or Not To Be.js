7a56e82ec46393aace1dd52134fedf33d1254a71
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-13 09:31:45 +0300

    Algorithms practice

--- JS Interview Questions/2704. To Be Or Not To Be.js
+++ JS Interview Questions/2704. To Be Or Not To Be.js
@@ -6,7 +6,7 @@
 
 // toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
 // notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
- 
+
 
 // Example 1:
 
@@ -24,3 +24,24 @@
 // Output: {"value": true}
 // Explanation: 5 !== null so this expression returns true.
 
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+        toBe(value) {
+            if (value === val) return true
+            else throw new Error("Not Equal")
+        },
+        notToBe(value) {
+            if (value !== val) return true
+            else throw new Error('Equal')
+        }
+    }
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
