12c0c8159edcbd73bb2252c613e4e959e23758c8
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-18 11:23:45 +0300

    Algorithms practice

--- All/y-playground.js
+++ All/y-playground.js
@@ -1,13 +1,18 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
-var strStr = function(haystack, needle) {
-    for(let i=0; i<haystack.length; i++) {
-        if(haystack.substring(i, i+ needle.length) === needle) return i
-    }
-    return -1
-};
 
-console.log(strStr( "sabdsadutsad",  "sad"))
+
+// сумма элементов массива
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function sumArr(arr) {
+    return arr.reduce((acc, val) => acc + val, 0)
+}
+
+function sumArr(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+console.log(sumArr(array))
