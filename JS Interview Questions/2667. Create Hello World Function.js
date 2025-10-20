45da297889828a1c63976019c9bc9d2a56aa14c7
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-20 11:31:43 +0300

    Algorithms practice

--- JS Interview Questions/2667. Create Hello World Function.js
+++ JS Interview Questions/2667. Create Hello World Function.js
@@ -3,7 +3,7 @@
 // Easy
 // Companies
 // Write a function createHelloWorld. It should return a new function that always returns "Hello World".
- 
+
 
 // Example 1:
 
@@ -23,4 +23,23 @@
 // f({}, null, 42); // "Hello World"
 
 // Any arguments could be passed to the function but it should still always return "Hello World".
- 
+
+
+// Constraints:
+
+// 0 <= args.length <= 10
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+
+    return function (...args) {
+        return 'Hello World'
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
