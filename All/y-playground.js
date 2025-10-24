e59efc7a41cd1403b3ff2517fcd6ef1a037c7a68
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-24 10:19:28 +0300

    Algorithms practice

--- All/y-playground.js
+++ All/y-playground.js
@@ -35,3 +35,18 @@ let reverseString = function (s) {
 };
 
 reverseString(s);
+
+// time complexity: O(n^2)
+// space complexity: O(1)
+
+function twoSum(nums, target) {
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
+
