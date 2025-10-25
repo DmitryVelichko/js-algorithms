96aa8e59dcf6af199e7e9c26e22b163e5ab5ee30
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-25 11:45:06 +0300

    Algorithms practice

--- All/y-playground.js
+++ All/y-playground.js
@@ -67,3 +67,7 @@ function twoSum2(nums, target) {
     }
 }
 
+// 448. Find All Numbers Disappeared in an Array
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
