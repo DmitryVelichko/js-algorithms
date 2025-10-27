098d3951f1497a287ecf03b5be8744810a3ff5a8
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-27 10:44:26 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -132,3 +132,13 @@ function reverseString(str) {
 }
 
 console.log(reverseString("GeeksForGeeks"));
+
+// Write a Program to check whether a string is a palindrome string.
+
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("GFG"));
+
