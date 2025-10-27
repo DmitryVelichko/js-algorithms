2ea7ccb89a5ca45a3d3932331599c91f187680a8
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-27 10:45:17 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -142,3 +142,16 @@ function isPalindrome(str) {
 
 console.log(isPalindrome("GFG"));
 
+// 3. Find the largest number in an array in JavaScript.
+
+function findLargest(arr) {
+  let largest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+console.log(findLargest([99, 5, 3, 100, 1]));
