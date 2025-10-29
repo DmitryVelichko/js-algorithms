d1cfbcc30bfcc8a732993a68f5e6dc8b7a0a7b71
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-29 10:23:21 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -181,3 +181,16 @@ function sumArray(arr) {
 }
 
 console.log(sumArray([15, 6, 10, 2]));
+
+// 16. Write a Program to check if a number is prime or not ?
+function isPrime(num) {
+  if (num <= 1)
+    return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0)
+      return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7));
