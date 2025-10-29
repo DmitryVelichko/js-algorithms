e53537aab864105d8a8df3f0698c5d805f0a16c9
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-29 10:25:48 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -195,3 +195,12 @@ function isPrime(num) {
 }
 
 console.log(isPrime(7));
+
+// merge two arrays 
+function mergeArrays(arr1, arr2) {
+  // this method merges all the elements 
+  // of arr2 at the end of arr1.
+  return arr1.concat(arr2);
+}
+
+console.log(mergeArrays([5, 6], [7, 8]));
