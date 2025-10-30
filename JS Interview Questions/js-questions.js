e33383d64aeebb55ffd6620da6f2fbbdd2963129
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-30 11:06:18 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -204,3 +204,20 @@ function mergeArrays(arr1, arr2) {
 }
 
 console.log(mergeArrays([5, 6], [7, 8]));
+
+// sort an array in Ascending Order 
+function sortArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        // swap the elements
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray([5, 3, 8, 1]));
