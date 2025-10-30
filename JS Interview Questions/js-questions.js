2f11730759e4b69507a9a6a42b85b30c49dd3de1
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-10-30 11:10:06 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -221,3 +221,22 @@ function sortArray(arr) {
 }
 
 console.log(sortArray([5, 3, 8, 1]));
+
+
+// sort an array in Descending Order 
+function sortArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) { // 👈 changed '>' to '<'
+        // swap the elements
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+
+console.log(sortArrayDesc([5, 3, 8, 1]));
