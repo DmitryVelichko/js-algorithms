2892370a89cd25007427e80a58593191c992481e
Dmitrii Velichko <soleil-2005@mail.ru>
2025-10-31 09:52:37 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -257,3 +257,18 @@ function power(base, exponent) {
 }
 
 console.log(power(3, 4));
+
+//  frequency of elements in an array
+function frequency(arr) {
+  const freq = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (freq[arr[i]]) {
+      freq[arr[i]] += 1;
+    } else {
+      freq[arr[i]] = 1;
+    }
+  }
+  return freq;
+}
+
+console.log(frequency([1, 1, 2, 3, 3, 4]));
