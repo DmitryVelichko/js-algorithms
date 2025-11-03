085e44ee140551d833b9ded3f9ace0ef934d81ec
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-03 13:46:19 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -346,3 +346,17 @@ function uniqueCharacters(str) {
 }
 
 console.log(uniqueCharacters("geeksforgeeks"));
+
+// Remove Duplicates from an Array
+
+function removeDuplicates(arr) {
+  const uniqueArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArray.includes(arr[i])) {
+      uniqueArray.push(arr[i]);
+    }
+  }
+  return uniqueArray;
+}
+
+console.log(removeDuplicates([5, 2, 5, 6, 6, 7]));
