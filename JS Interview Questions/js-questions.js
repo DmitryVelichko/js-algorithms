b816d0d14d74febed52af57c12a9706db08ede4e
Dmitrii Velichko <soleil-2005@mail.ru>
2025-11-04 12:45:38 +0300

    Algorithms practice

--- JS Interview Questions/js-questions.js
+++ JS Interview Questions/js-questions.js
@@ -394,3 +394,29 @@ console.log(removeDuplicates([5, 2, 5, 6, 6, 7]));
 // // "getCallCount" - total call count: 2
 // Example 2:
 
+// Input:
+// fnName = "factorial"
+// actions = ["call","call","call","getCallCount","call","getCallCount"]
+// values = [[2],[3],[2],[],[3],[]]
+// Output: [2,6,2,2,6,2]
+// Explanation:
+// const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+// const memoFactorial = memoize(factorial);
+// memoFactorial(2); // "call" - returns 2.
+// memoFactorial(3); // "call" - returns 6.
+// memoFactorial(2); // "call" - returns 2. However factorial was not called because 2 was seen before.
+// // "getCallCount" - total call count: 2
+// memoFactorial(3); // "call" - returns 6. However factorial was not called because 3 was seen before.
+// // "getCallCount" - total call count: 2
+// Example 3:
+
+// Input:
+// fnName = "fib"
+// actions = ["call","getCallCount"]
+// values = [[5],[]]
+// Output: [8,1]
+// Explanation:
+// fib(5) = 8 // "call"
+// // "getCallCount" - total call count: 1
+ 
+
