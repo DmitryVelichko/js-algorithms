35a626e5e8cf5a604a8e6b94a0c5935822737595
Dmitrii Velichko <soleil-2005@mail.ru>
2025-11-11 09:54:05 +0300

    Algorithms practice

--- js-interview.js
+++ js-interview.js
@@ -117,3 +117,19 @@ const groupByCity2 = (array) =>
 
 
 console.log(groupByCity2(people))
+
+// Объединение интервалов в массиве
+
+const array1 = [[1, 3], [2, 6], [8, 10], [15, 18]]; // [[1, 6], [8, 10], [15, 18]]
+const array2 = [[1, 4], [4, 5]];  // [[1, 5]]
+const array3 = [[11, 12], [2, 3], [5, 7], [1, 4], [8, 10], [6, 8]]; // [[1, 4], [5, 10], [11, 12]]
+
+function merge(intervals) {
+
+}
+
+console.log(merge(array1));
+console.log(merge(array2));
+console.log(merge(array3));
+
+
