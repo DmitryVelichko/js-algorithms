5f7c30e6750e0264002bd8363eb7ca45f561848c
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-20 19:57:42 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -445,3 +445,9 @@ var reverseBits = function (n) {
 var sortBy = function (arr, fn) {
     return arr.sort((a, b) => fn(a) - fn(b))
 };
+
+// 2723. Add Two Promises
+// Easy
+
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+
