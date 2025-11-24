da5e7ac4ff85965c479a58782e355d6bceff0400
Dmitrii Velichko <soleil-2005@mail.ru>
2025-11-24 16:02:48 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -510,6 +510,14 @@ const binarySearch = (arr, value) => {
 };
 console.log(binarySearch(arr, 11));
 
+// 100. Same Tree
+// Solved
+// Easy
+// Topics
+// Companies
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
 
 
