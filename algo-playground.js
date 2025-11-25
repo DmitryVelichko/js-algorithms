76b26545b32df7f306b2592981017e524c2f1679
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-25 10:51:08 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -578,5 +578,16 @@ var isSameTree = function (p, q) {
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
 
+// 21. Merge Two Sorted Lists
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
 
 
