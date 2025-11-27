f2d57eaa7edea0cd227ea1eee0e5c084a2589577
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-27 13:38:28 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -658,7 +658,7 @@ var mergeTwoLists = function (l1, l2) {
 
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
- 
+
 
 // Example 1:
 
@@ -675,3 +675,45 @@ var mergeTwoLists = function (l1, l2) {
 
 // The number of nodes in the tree is in the range[0, 104].
 // - 100 <= Node.val <= 100
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 3 ways: DFS(recursive/iterative) & BFS
+// O(n), O(h): which is O(n) in worst case of an unbalanced tree
+
+// DFS recursive
+const maxDepth = (root) => {
+    if (root === null) return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+// DFS iterative pre-order
+
+function maxDepth2(root) {
+    let stack = [[root, 1]]; // Stack to store nodes and their corresponding depth
+    let res = 0;
+
+    while (stack.length > 0) {
+        let [node, depth] = stack.pop();
+
+        if (node) {
+            res = Math.max(res, depth); // Update the maximum depth
+            stack.push([node.left, depth + 1]);
+            stack.push([node.right, depth + 1]);
+        }
+    }
+    return res;
+}
+
+
+
+}
