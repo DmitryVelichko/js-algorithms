9fea206a759a01379cdbbfc67ebaa6373dc8bd42
Dmitrii Velichko <soleil-2005@mail.ru>
2025-11-29 11:30:47 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -812,7 +812,7 @@ var twoSum = function (nums, target) {
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
- 
+
 
 // Example 1:
 
@@ -844,9 +844,33 @@ var twoSum = function (nums, target) {
 
 // Output: false
 
- 
+
 
 // Constraints:
 
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
+stack 
+iterate through string, for each opening braket push closing braket to stack,
+closing bracket !== bracket on stack top ? => false
+
+=> stack.len === 0
+ */
+// O(n), O(n)
+var isValid = function (s) {
+    const stack = []
+
+    for (let char of s) {
+        if (char === '(') stack.push(')')
+        else if (char === '[') stack.push(']')
+        else if (char === '{') stack.push('}')
+        else if (stack.pop() !== char) return false
+    }
+    return stack.length === 0
+};
