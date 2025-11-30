3c7965b2d4fb3e933c68ffefa92deff163cbf03c
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-30 11:38:47 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -883,7 +883,7 @@ var isValid = function (s) {
 // Companies
 // Given the head of a singly linked list, reverse the list, and return the reversed list.
 
- 
+
 
 // Example 1:
 
@@ -899,12 +899,67 @@ var isValid = function (s) {
 
 // Input: head = []
 // Output: []
- 
+
 
 // Constraints:
 
 // The number of nodes in the list is the range [0, 5000].
 // -5000 <= Node.val <= 5000
- 
+
 
 // Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+curr = head,
+prev = null
+while(curr)
+   next 
+   prev <- curr
+   смещаем 2 указателя
+return prev
+*/
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// O(n), O(1)
+var reverseList = function (head) {
+    let curr = head
+    let prev = null
+    while (curr) {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+};
+
+/**
+base case: (head || head.next === null) return head
+newHead = recursion(head.next)
+5 -> 4
+4 -> null
+return newHead
+*/
+
+// O(n), O(n)
+var reverseList = function (head) {
+
+    if (head === null || head.next === null) return head;
+
+    const newHead = reverseList(head.next);
+
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
+};
