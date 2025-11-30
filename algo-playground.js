bfce82f21e75304fd28af50cf86290e73187ee6c
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-11-30 11:38:33 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -874,3 +874,12 @@ var isValid = function (s) {
     }
     return stack.length === 0
 };
+
+// 206. Reverse Linked List
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
