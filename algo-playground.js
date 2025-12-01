a2b4cc2c05e2c45b62c532db3825e9ffe7a61809
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-01 11:42:30 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -976,3 +976,11 @@ var reverseList = function (head) {
 
 // Return true if there is a cycle in the linked list. Otherwise, return false.
 
+ 
+
+// Example 1:
+
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
