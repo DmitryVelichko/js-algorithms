f0b6e0848dafa4e8bcd71a7f8c062fcac3369b2e
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-02 10:56:30 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1049,9 +1049,50 @@ var hasCycle = function (head) {
 
 // Input: root = [1,null,2]
 // Output: 2
- 
+
 
 // Constraints:
 
 // The number of nodes in the tree is in the range [0, 104].
 // -100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 3 ways: DFS(recursive/iterative) & BFS
+// O(n), O(h): which is O(n) in worst case of an unbalanced tree
+
+// DFS recursive
+const maxDepth = (root) => {
+    if (root === null) return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+// DFS iterative pre-order: O(n), O(h)
+//Best when tree is deep, avoids recursion limit
+
+function maxDepth2(root) {
+    let stack = [[root, 1]]; // Stack to store nodes and their corresponding depth
+    let res = 0;
+
+    while (stack.length > 0) {
+        let [node, depth] = stack.pop();
+
+        if (node) {
+            res = Math.max(res, depth); // Update the maximum depth
+            stack.push([node.left, depth + 1]);
+            stack.push([node.right, depth + 1]);
+        }
+    }
+    return res;
+}
