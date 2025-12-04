c7ea9fe0d10e7c82e2a60faded50a9067691e1aa
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-04 10:27:47 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1132,3 +1132,16 @@ function maxDepth3(root) {
 
     return maxDepth;
 }
+
+// 100. Same Tree
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+
