2101c2c125dd39e603e8ff219a790ead17c1d352
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-12-09 12:33:00 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1419,3 +1419,63 @@ class Solution {
         return true;
     }
 }
+
+
+// 20. Valid Parentheses
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+
+//     Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+
+
+
+
+
+
+
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
+stack 
+iterate through string, for each opening braket push closing braket to stack,
+closing bracket !== bracket on stack top ? => false
+
+=> stack.len === 0
+ */
+// O(n), O(n)
+var isValid = function (s) {
+    const stack = []
+
+    for (let char of s) {
+        if (char === '(') stack.push(')')
+        else if (char === '[') stack.push(']')
+        else if (char === '{') stack.push('}')
+        else if (char !== stack.pop()) return false
+    }
+    return stack.length === 0
+};
