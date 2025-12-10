4a33fb76b2076562a8d496a3e756e883067b16a7
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-10 17:37:24 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1497,3 +1497,15 @@ var isValid = function (s) {
     }
     return stack.length === 0
 };
+
+// 739. Daily Temperatures
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+
+
