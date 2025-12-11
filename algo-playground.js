7e3358d53ab9e32e00dfaed898f989e7684a33b6
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-11 12:04:34 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1571,3 +1571,31 @@ var dailyTemperatures = function (temperatures) {
 
 
 
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// 3 ways: DFS(recursive/iterative) & BFS
+// O(n), O(h): which is O(n) in worst case of an unbalanced tree
