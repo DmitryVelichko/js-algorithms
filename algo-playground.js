39433adb09a9ad0e76bf968bd4b73508bc761b41
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-11 12:04:44 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1599,3 +1599,29 @@ var dailyTemperatures = function (temperatures) {
  */
 // 3 ways: DFS(recursive/iterative) & BFS
 // O(n), O(h): which is O(n) in worst case of an unbalanced tree
+
+// DFS recursive
+const maxDepth = (root) => {
+    if (root === null) return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+// DFS iterative pre-order: O(n), O(h)
+//Best when tree is deep, avoids recursion limit
+
+function maxDepth2(root) {
+    let stack = [[root, 1]]; // Stack to store nodes and their corresponding depth
+    let res = 0;
+
+    while (stack.length > 0) {
+        let [node, depth] = stack.pop();
+
+        if (node) {
+            res = Math.max(res, depth); // Update the maximum depth
+            stack.push([node.left, depth + 1]);
+            stack.push([node.right, depth + 1]);
+        }
+    }
+    return res;
+}
+
