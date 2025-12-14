a262d1f51c8b94bd6e9f2e04bf8a90bd3c698f60
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-14 11:15:33 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -84,3 +84,48 @@ function maxDepth2(root) {
 
 // Return the weight of the last remaining stone. If there are no stones left, return 0.
 
+
+
+// Example 1:
+
+// Input: stones = [2,7,4,1,8,1]
+// Output: 1
+// Explanation: 
+// We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+// we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+// we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+// we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+// Example 2:
+
+// Input: stones = [1]
+// Output: 1
+
+
+// Constraints:
+
+// 1 <= stones.length <= 30
+// 1 <= stones[i] <= 1000
+
+
+/**
+ * 
+ * 
+ * @param {number[]} stones
+ * @return {number}
+ */
+/**
+ * getMaxHeap(): add all stones to maxHeap
+ shrink(): get the heaviest stone1 and stone2 from heap, add to heap the difference if it's > 0
+ if maxHeap is empty return 0 or top element: .front().element
+ */
+//Time O(N * log(N)) | Space O(N)
+var lastStoneWeight = function (stones) {
+    const maxHeap = getMaxHeap(stones)
+
+    shrink(maxHeap)
+
+    if (maxHeap.isEmpty()) return 0
+    else return maxHeap.front().element
+
+};
+
