f9df512ef7542d1db094fc1be1e4af5f28437648
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-12-16 12:29:33 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -216,3 +216,13 @@ var removeNthFromEnd = function (head, n) {
     l.next = l.next.next // delete the nth node
     return dummy.next
 };
+
+// 739. Daily Temperatures
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
