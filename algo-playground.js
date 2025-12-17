f8edef10d19e38d2bfcc96cd2cd660713abb4bfd
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-17 12:23:07 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -334,3 +334,15 @@ var dailyTemperatures = function (temperatures) {
  * @param {ListNode} head
  * @return {boolean}
  */
+// O(n), O(1)
+var hasCycle = function (head) {
+    let slow = head
+    let fast = head
+
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (fast === slow) return true
+    }
+    return false
+};
