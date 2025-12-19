60ed966c8c058767b63f0d416d7eb2c7919a57da
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-19 12:25:44 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -410,3 +410,30 @@ var search = function (nums, target) {
 
 // The length of a path between two nodes is represented by the number of edges between them.
 
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5]
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+// Example 2:
+
+// Input: root = [1,2]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
