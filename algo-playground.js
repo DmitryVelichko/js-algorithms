d3902190cc6872cfacbc0c9da44730a18cab6f6f
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-12-20 11:25:49 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -514,3 +514,20 @@ var climbStairs = function (n) {
     }
     return one
 };
+
+// O(n), O(1)
+var climbStairs = function (n) {
+    if (n <= 2) return n;
+
+    let prev1 = 1, prev2 = 2;
+
+    for (let i = 3; i <= n; i++) {
+        let curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+
+    return prev2;
+};
+
+
