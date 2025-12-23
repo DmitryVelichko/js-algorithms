82d01fe20c71bc1a70c99af7ac9774710097e338
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2025-12-23 22:04:23 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -12,3 +12,28 @@
 
 
 
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5]
+// Output: 3
+// Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+// Example 2:
+
+// Input: root = [1,2]
+// Output: 1
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
