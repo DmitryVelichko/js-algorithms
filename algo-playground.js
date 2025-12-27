4aed5550fdea9588cde9e055650a0c4e87cd5c96
Dmitrii Velichko <soleil-2005@mail.ru>
2025-12-27 11:41:14 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -174,10 +174,45 @@ var invertTree2 = function (root) {
 // Input: nums = [0,0,0]
 // Output: [[0,0,0]]
 // Explanation: The only possible triplet sums up to 0.
- 
+
 
 // Constraints:
 
 // 3 <= nums.length <= 3000
 // -105 <= nums[i] <= 105
- 
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+/**
+* res = []
+ сортируем массив
+ скипаем дубликаты
+ 2 pointers for 2 numbers + первое число
+ скипаем дубликаты
+*/
+// Time: O(n^2), Space: O(n^2)
+var threeSum = function (nums) {
+    const res = []
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        // skip duplicates
+        if (i > 0 && nums[i - 1] === nums[i]) continue
+
+        let l = i + 1
+        let r = nums.length - 1
+        while (l < r) {
+            let threeSum = nums[i] + nums[l] + nums[r]
+            if (threeSum > 0) r--
+            else if (threeSum < 0) l++
+            else {
+                res.push([nums[i], nums[l], nums[r]])
+                l++
+                r--
+                while (l < r && nums[l] === nums[l - 1]) l++
+            }
+        }
+    }
+    return res
+};
