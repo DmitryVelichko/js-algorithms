08fd57f614f13789f43ad63b7c5afe4534ad624e
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-01 17:04:56 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -216,3 +216,12 @@ var threeSum = function (nums) {
     }
     return res
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
