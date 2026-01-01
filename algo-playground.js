3fd6a1c3ff129a04674f5aba422819604ed72aa9
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-01 17:05:08 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -234,10 +234,27 @@ var threeSum = function (nums) {
 // 2. вернуть массив, туда расспредить рекурсивно функцию с root.left, значение root.val функцию с root.right
 
 
-var inorderTraversal = function (root) {
-
-    if (root === null) return []
+var inorderTraversal = function(root) {
+    
+    if(root === null) return []
     return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
-
+    
 };
 
+// function inorderTraversal(root) {
+//     const stack = [];
+//     const res = [];
+  
+//     while (root || stack.length) {
+//       if (root) {
+//         stack.push(root);
+//         root = root.left;
+//       } else {
+//         root = stack.pop();
+//         res.push(root.val);
+//         root = root.right;
+//       }
+//     }
+  
+//     return res;
+//   }
