f7d564a0c2021a0d5efff234101e03b1a2c51b6f
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-02 12:18:05 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -281,4 +281,49 @@ add right char to set, update max string size
 
 
 
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+// Sliding window: 2 pointers: set, l, max.
+// for loop across the whole string (r = 0): while right char exists in set ? delete left char from set, l++;
+// add right char to set, update max string size
+// */
+// O(n), O(n)
+
+
+var lengthOfLongestSubstring = function (s) {
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, set.size);
+    }
+    return max;
+};
 
