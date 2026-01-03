a68f3844b89a61e45f7df75dcefb2899eaf23a05
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-03 12:14:25 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -21,3 +21,25 @@ function range(arr) {
 }
 console.log(range([1, 4, 2, 5, 3, 9, 8, 11, 0])) // '0-5, 8-9, 11' 
 console.log(range([1, 4, 3, 2])) // '1-4'
+
+function range(arr) {
+    if (!arr.length) return '';
+
+    // sort and deduplicate
+    const nums = [...new Set(arr)].sort((a, b) => a - b);
+
+    const result = [];
+    let start = nums[0];
+    let prev = nums[0];
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i] !== prev + 1) {
+            // end current range
+            result.push(start === prev ? `${start}` : `${start}-${prev}`);
+            start = nums[i];
+        }
+        prev = nums[i];
+    }
+
+    return result.join(', ');
+}
