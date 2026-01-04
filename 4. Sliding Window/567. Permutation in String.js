43d6e3a9ba9456e4490a8217ed1e3098fe9c3d5b
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-04 12:44:36 +0300

    Algorithms practice

--- 4. Sliding Window/567. Permutation in String.js
+++ 4. Sliding Window/567. Permutation in String.js
@@ -9,7 +9,7 @@
 
 // In other words, return true if one of s1's permutations is the substring of s2.
 
- 
+
 
 // Example 1:
 
@@ -20,9 +20,61 @@
 
 // Input: s1 = "ab", s2 = "eidboaoo"
 // Output: false
- 
+
 
 // Constraints:
 
 // 1 <= s1.length, s2.length <= 104
 // s1 and s2 consist of lowercase English letters.
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+// O(n), O(1)
+var checkInclusion = function (s1, s2) {
+    if (s1.length > s2.length) {
+        return false;
+    }
+
+    let s1Count = new Array(26).fill(0);
+    let s2Count = new Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+        s1Count[s1.charCodeAt(i) - 97]++;
+        s2Count[s2.charCodeAt(i) - 97]++;
+    }
+
+    let matches = 0;
+    for (let i = 0; i < 26; i++) {
+        if (s1Count[i] === s2Count[i]) {
+            matches++;
+        }
+    }
+
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+        if (matches === 26) {
+            return true;
+        }
+
+        let index = s2.charCodeAt(r) - 97;
+        s2Count[index]++;
+        if (s1Count[index] === s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] + 1 === s2Count[index]) {
+            matches--;
+        }
+
+        index = s2.charCodeAt(l) - 97;
+        s2Count[index]--;
+        if (s1Count[index] === s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] - 1 === s2Count[index]) {
+            matches--;
+        }
+        l++;
+    }
+    return matches === 26;
+}
+
