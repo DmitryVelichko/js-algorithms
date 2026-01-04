8d22c66efe7474b6ff55afe04d4c509aabd88e18
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-04 12:44:23 +0300

    Algorithms practice

---
+++ 4. Sliding Window/567. Permutation in String.js
@@ -0,0 +1,12 @@
+// 567. Permutation in String
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+// In other words, return true if one of s1's permutations is the substring of s2.
+
+
