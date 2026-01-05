008405816c15dbcb806e0080bb763b7d0bf74cfd
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-05 12:14:33 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -7,7 +7,7 @@
 // Hint
 // Given a string s, find the length of the longest substring without duplicate characters.
 
- 
+
 
 // Example 1:
 
@@ -25,9 +25,36 @@
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
- 
+
 
 // Constraints:
 
 // 0 <= s.length <= 5 * 104
 // s consists of English letters, digits, symbols and spaces.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+/**
+Sliding window: 2 pointers: set, l, max.
+for loop across the whole string (r = 0): while right char exists in set ? delete left char from set, l++;
+add right char to set, update max string size
+*/
+// O(n), O(n)
+
+var lengthOfLongestSubstring = function (s) {
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) {
+            set.delete(s[l]);
+            l++;
+        }
+        set.add(s[r]);
+        max = Math.max(max, set.size);
+    }
+    return max;
+};
