3ff582dbcf443f81b67a84eb0aca35febdd3da05
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-06 14:42:21 +0300

    Algorithms practice

--- 4. Sliding Window/239. sliding window maximum.js
+++ 4. Sliding Window/239. sliding window maximum.js
@@ -24,3 +24,16 @@
 //  1  3  -1 [-3  5  3] 6  7       5
 //  1  3  -1  -3 [5  3  6] 7       6
 //  1  3  -1  -3  5 [3  6  7]      7
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// 1 <= k <= nums.length
+ 
+
