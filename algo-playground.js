9c3e9d7a82c325ae60ed66a99e267c7071948d85
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-07 13:02:58 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -58,3 +58,16 @@ var lengthOfLongestSubstring = function (s) {
     }
     return max;
 };
+
+// 239. Sliding Window Maximum
+// Solved
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+
