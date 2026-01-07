c8e6b41a13703cd593b899927986a26dd624f2eb
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-07 13:03:06 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -71,3 +71,29 @@ var lengthOfLongestSubstring = function (s) {
 // Return the max sliding window.
 
 
+
+// Example 1:
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation: 
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// 1 <= k <= nums.length
+
