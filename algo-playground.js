a645b82fb6da364d2c90dd000da68db648e8956c
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-10 12:58:38 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -305,3 +305,79 @@ var hasCycle2 = function (head) {
     }
     return false
 };
+
+// 424. Longest Repeating Character Replacement
+// Solved
+// Medium
+// Topics
+// Companies
+// You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+// Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+// Example 1:
+
+// Input: s = "ABAB", k = 2
+// Output: 4
+// Explanation: Replace the two 'A's with two 'B's or vice versa.
+// Example 2:
+
+// Input: s = "AABABBA", k = 1
+// Output: 4
+// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+// The substring "BBBB" has the longest repeating letters, which is 4.
+// There may exists other ways to achieve this answer too.
+
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of only uppercase English letters.
+// 0 <= k <= s.length
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+/**
+* 2 pointers: hashmap[char]++ = {a: 3, b: 7, ...}, left, right
+window size > k ? hash[char]--, l++;
+update result maximum
+*
+*/
+// T: O(26*n), S: O(n) Where n is the length of the string and m is the total number of unique characters in the string.
+var characterReplacement = function (s, k) {
+    const hash = {}
+    let res = 0
+    let l = 0
+    for (let r = 0; r < s.length; r++) {
+        hash[s[r]] = (hash[s[r]] || 0) + 1
+        while ((r - l + 1) - Math.max(...Object.values(hash)) > k) {
+            hash[s[l]] -= 1
+            l += 1
+
+        }
+        res = Math.max(res, r - l + 1)
+    }
+    return res
+};
+
+// T: O(n), S: O(n) Where n is the length of the string and m is the total number of unique characters in the string.
+var characterReplacementOptimized = function (s, k) {
+    const hash = {}
+    let res = 0
+    let l = 0
+    let maxFreq = 0
+    for (let r = 0; r < s.length; r++) {
+        hash[s[r]] = (hash[s[r]] || 0) + 1
+        maxFreq = Math.max(maxFreq, hash[s[r]])
+        while ((r - l + 1) - maxFreq > k) {
+            hash[s[l]] -= 1
+            l += 1
+
+        }
+        res = Math.max(res, r - l + 1)
+    }
+    return res
+};
