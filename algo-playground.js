e12bdfcb577a6edc2b6887e861b443c71809440b
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-14 22:22:19 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -574,7 +574,7 @@ var hasCycle = function (head) {
 
 // In other words, return true if one of s1's permutations is the substring of s2.
 
- 
+
 
 // Example 1:
 
@@ -585,9 +585,62 @@ var hasCycle = function (head) {
 
 // Input: s1 = "ab", s2 = "eidboaoo"
 // Output: false
- 
+
 
 // Constraints:
 
 // 1 <= s1.length, s2.length <= 104
 // s1 and s2 consist of lowercase English letters.
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+// O(n), O(1)
+var checkInclusion = function (s1, s2) {
+    if (s1.length > s2.length) {
+        return false;
+    }
+
+    let matches = 0;
+    let s1Count = new Array(26).fill(0); // [0, 0, 0...]
+    let s2Count = new Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        s1Count[s1.charCodeAt(i) - 'a'.charCodeAt(0)]++; // [1, 1, 0...]
+        s2Count[s2.charCodeAt(i) - 97]++;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (s1Count[i] === s2Count[i]) { // matches = 24
+            matches++;
+        }
+    }
+
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+        if (matches === 26) {
+            return true;
+        }
+
+        let index = s2.charCodeAt(r) - 'a'.charCodeAt(0);
+        s2Count[index]++;
+        if (s1Count[index] === s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] + 1 === s2Count[index]) {
+            matches--;
+        }
+
+        index = s2.charCodeAt(l) - 97;
+        s2Count[index]--;
+        if (s1Count[index] === s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] - 1 === s2Count[index]) {
+            matches--;
+        }
+        l++;
+    }
+    return matches === 26;
+}
+
