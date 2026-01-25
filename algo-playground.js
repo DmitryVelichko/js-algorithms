915befab10b36f4309db76ee57c9bff997789f80
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-25 22:08:00 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -981,3 +981,43 @@ var invertTree = function (root) {
 // Companies
 // Given the root of a binary tree, invert the tree, and return its root.
 
+
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+// Example 2:
+
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+// DFS iterative
+// Time: O(n), space: O(h) (best - O(log n) for BST, O(n) for linked list)
+var invertTree2 = function (root) {
+    if (root === null) return null;
+
+    const stack = [root];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+
+        [node.left, node.right] = [node.right, node.left];
+
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+
+    return root;
+};
