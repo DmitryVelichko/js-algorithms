a8657b8df24e56387f6bae9c1a60717b0f8ce96e
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-01-26 21:04:04 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -943,3 +943,43 @@ var inorderTraversal = function (root) {
 
 //     return res;
 //   }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// DFS recursive pre-order: O(n), Space: O(h) h = height of the tree (for 2 nodes h = 2, for 1 node h = 0).
+// Space O(h), for a skewed tree: O(n) linked list, for a balanced binary tree: O(log n)
+var invertTree = function (root) {
+
+    if (!root) return null
+
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+};
+
+// 226. Invert Binary Tree
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the root of a binary tree, invert the tree, and return its root.
+
+
+
