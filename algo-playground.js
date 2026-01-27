aa80c1944d3513631d2eea48098b174c9b2d0b06
Dmitrii Velichko <soleil-2005@mail.ru>
2026-01-27 22:09:04 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -1032,7 +1032,7 @@ var invertTree2 = function (root) {
 
 // Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
- 
+
 
 // Example 1:
 
@@ -1049,9 +1049,34 @@ var invertTree2 = function (root) {
 
 // Input: p = [1,2,1], q = [1,1,2]
 // Output: false
- 
+
 
 // Constraints:
 
 // The number of nodes in both trees is in the range [0, 100].
 // -104 <= Node.val <= 104
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+// if both nodes are null => true
+// if one of the nodes in null || values are different => false
+// => (l,l) && (r,r)
+// Time: O(n), space: O(h) (h can be n in unbalanced tree like linked list and log n in balanced tree)
+var isSameTree = function (p, q) {
+
+    if (!p && !q) return true
+    if ((!p || !q) || (p.val !== q.val)) return false
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
