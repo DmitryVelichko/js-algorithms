dd71b40c1a87ec9d455155c120c1e6a3afa3e8ef
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-02-02 22:58:41 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -13,3 +13,36 @@
  */
 // DFS recursive pre-order: O(n), Space: O(h) h = height of the tree (for 2 nodes h = 2, for 1 node h = 0).
 // Space O(h), for a skewed tree: O(n) linked list, for a balanced binary tree: O(log n)
+var invertTree = function (root) {
+
+    if (!root) return null
+
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+};
+
+
+// DFS iterative
+// Time: O(n), space: O(h) (best - O(log n) for BST, O(n) for linked list)
+var invertTree2 = function (root) {
+    if (root === null) return null;
+
+    const stack = [root];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+
+        [node.left, node.right] = [node.right, node.left];
+
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+
+    return root;
+};
