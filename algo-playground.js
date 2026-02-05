ab01d0339974c8e826e901c296177b1949e569c2
Dmitrii Velichko <dmitrii.velichko@fingular.com>
2026-02-05 22:32:50 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -114,3 +114,18 @@ var isSubtree = function (root, subRoot) {
         return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right)
     }
 };
+
+// 141. Linked List Cycle
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+
