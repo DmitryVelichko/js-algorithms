9c4377c9c3f954351ca4cb9264a89a1240573cce
Di <soleil-2005@mail.ru>
2026-02-10 23:45:59 +0800

    Algorithms practice

---
+++ algo-playground.js
@@ -0,0 +1,11 @@
+
+// 206. Reverse Linked List
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
