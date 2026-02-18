047ea73978f4385cc9f0336459acb5ed9d6338da
Dmitry Velichko <soleil-2005@mail.ru>
2026-02-18 20:08:35 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -324,3 +324,14 @@ function maxDepth3(root) {
 
     return maxDepth;
 }
+
+// \206. Reverse Linked List
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
