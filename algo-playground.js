7a1c194e1d120ae74c5b318b8208bef51e4ec820
Dmitry Velichko <soleil-2005@mail.ru>
2026-02-21 21:22:39 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -612,3 +612,29 @@ var threeSum = function (nums) {
  
 
 // Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+// slow, fast. В цикле смещаем их на 1, 2 шага, затем проверяем: Если равны, то true
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// O(n), O(1)
+var hasCycle = function (head) {
+    let slow = head
+    let fast = head
+
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (fast === slow) return true
+    }
+    return false
+};
