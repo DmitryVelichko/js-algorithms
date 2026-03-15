adca9f5716ec0dc2e30f03a6bc717601d04125ef
Dmitry Velichko <soleil-2005@mail.ru>
2026-03-15 23:48:08 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -488,3 +488,52 @@ var isSameTree = function (p, q) {
 
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// DFS recursive pre-order: O(n), Space: O(h) h = height of the tree (for 2 nodes h = 2, for 1 node h = 0).
+// Space O(h), for a skewed tree: O(n) linked list, for a balanced binary tree: O(log n)
+var invertTree = function (root) {
+
+    if (!root) return null
+
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+};
+
+
+// DFS iterative
+// Time: O(n), space: O(h) (best - O(log n) for BST, O(n) for linked list)
+var invertTree2 = function (root) {
+    if (root === null) return null; 
+
+    const stack = [root]; 
+
+    while (stack.length > 0) {
+        const node = stack.pop(); 
+
+        [node.left, node.right] = [node.right, node.left];
+
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+
+    return root; 
+};
