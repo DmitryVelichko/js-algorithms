b4dd03fd65530ae82b29df9a91a9618e2175c93c
Dmitry Velichko <soleil-2005@mail.ru>
2026-03-20 23:49:29 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -641,3 +641,14 @@ function maxDepth3(root) {
 
     return maxDepth;
 }
+
+// 226. Invert Binary Tree
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
