a1fc368ec08f330c7bf66de0edad0a21c1cfb2b4
Dmitry Velichko <soleil-2005@mail.ru>
2026-04-16 08:42:07 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -87,3 +87,24 @@ var reverseList = function (head) {
     return newHead;
 };
 
+// /**
+//  * @param {number[]} nums
+//  */
+// var Solution = function(nums) {
+//     this.nums = nums;
+// };
+
+// /**
+//  * @return {number[]}
+//  */
+// Solution.prototype.reset = function() {
+//     return this.nums
+// };
+
+// Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+// Implement the Solution class:
+
+// Solution(int[] nums) Initializes the object with the integer array nums.
+// int[] reset() Resets the array to its original configuration and returns it.
+// int[] shuffle() Returns a random shuffling of the array.
