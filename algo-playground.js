e2245f6100e985601f4d7cf371db4ce4f7e0c8c1
Dmitry Velichko <soleil-2005@mail.ru>
2026-04-18 21:36:01 +0800

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -234,3 +234,48 @@ var MinStack = function() {
     this.min = []
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if(this.stack.length === 0) {
+        this.min.push(val)
+    } else {
+        let minimum = Math.min(val, this.min[this.min.length - 1])
+        this.min.push(minimum)
+    }
+    
+    this.stack.push(val)
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+    this.min.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min[this.min.length - 1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
