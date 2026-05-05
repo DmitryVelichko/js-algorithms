aa524bfd2635e4571d7acc0d334d96baf4b6eb45
Dmitry Velichko <soleil-2005@mail.ru>
2026-05-05 14:48:45 +0300

    Algorithms practice

---
+++ algo-playground.js
@@ -0,0 +1,11 @@
+// 704. Binary Search
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
