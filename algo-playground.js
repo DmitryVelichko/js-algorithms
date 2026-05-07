5546504d8c91ae4fdcaaa5f3d45060a34347cc62
Dmitry Velichko <soleil-2005@mail.ru>
2026-05-07 09:41:16 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -152,4 +152,50 @@ var hasCycle = function (head) {
 
 // 1 <= s.length <= 2 * 105
 // s consists only of printable ASCII characters.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
+2 pointers,
+char is not alphaNumeric() ? l++ or r--
+left char !== right char ? => false
+=> true
+}
+*/
+// O(n), O(1)
+var isPalindrome = function (s) {
+
+    function isAlphaNumeric(char) {
+        const charCode = char.charCodeAt(0);
+        return (
+            '0'.charCodeAt(0) <= charCode && charCode <= '9'.charCodeAt(0) ||
+            'a'.charCodeAt(0) <= charCode && charCode <= 'z'.charCodeAt(0) ||
+            'A'.charCodeAt(0) <= charCode && charCode <= 'Z'.charCodeAt(0)
+        );
+    }
+
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+
+        while (l < r && !isAlphaNumeric(s[l])) l++
+        while (l < r && !isAlphaNumeric(s[r])) r--
+
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+
+        l++;
+        r--;
+    }
+    return true;
+}
+
+// str = str.replace
+
+// var isPalindrome = function(s) {
+//     let newStr = s.replace(/[^a-z0-9]/gi,"").toLowerCase();
+//     return newStr.split("").reverse().join("") === newStr ? true : false;
+// };
  
