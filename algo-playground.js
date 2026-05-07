10740d39266d10a8138af43af76ec4c677d019e1
Dmitry Velichko <soleil-2005@mail.ru>
2026-05-07 09:41:04 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -117,3 +117,18 @@ var hasCycle = function (head) {
     }
     return false
 };
+
+// 125. Valid Palindrome
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+
+ 
