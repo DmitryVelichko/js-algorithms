4432bc4ab5aa7f54776c31e5b4189358c77c8de0
Dmitry Velichko <soleil-2005@mail.ru>
2026-05-17 10:11:58 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -626,3 +626,14 @@ var dailyTemperatures = function(temperatures) {
     }
     return result;
 };
+
+// 206. Reverse Linked List
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
