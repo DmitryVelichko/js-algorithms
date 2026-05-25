96939a647b3debff3732ded00ea5da410dc5e1e9
Dmitry Velichko <soleil-2005@mail.ru>
2026-05-25 12:02:51 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -213,3 +213,13 @@ var threeSum = function (nums) {
     }
     return res
 };
+
+// 347. Top K Frequent Elements
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
