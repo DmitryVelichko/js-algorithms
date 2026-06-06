078b22d95cf314db0e02eeb5bad7ad158b603959
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-06 10:44:31 +0300

    Algorithms practice

--- js-interview.js
+++ js-interview.js
@@ -503,3 +503,18 @@ var isSameTree = function (p, q) {
 
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
+
+// 238. Product of Array Except Self
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
