49f1f84407725b5475b33514ae069e5a589d7608
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-09 10:56:37 +0300

    Algorithms practice

--- js-interview.js
+++ js-interview.js
@@ -664,3 +664,17 @@ function maxSlidingWindow(nums, k) {
 
     return output;
 }
+
+// 121. Best Time to Buy and Sell Stock
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
