1365ad2e6dd6f7e03e889b5a01557289639db224
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-10 09:52:39 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -87,3 +87,15 @@ function topKFrequent(nums, k) {
         }
     }
 }
+
+// 3. Longest Substring Without Repeating Characters
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
