2ad361496340404e542d731b2fa00e04922ac699
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-11 09:49:43 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -148,3 +148,12 @@ var lengthOfLongestSubstring = function (s) {
     }
     return max;
 };
+
+// 258. Add Digits
+// Easy
+// 4.5K
+// 1.9K
+// Companies
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
