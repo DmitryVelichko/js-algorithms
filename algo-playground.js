ca84453cf446cfaf80a25ed06ba5a0c7dc10be73
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-12 10:19:27 +0300

    Algorithms practice

--- algo-playground.js
+++ algo-playground.js
@@ -241,3 +241,12 @@ function addDigits(num) {
 // -100 <= Node.val <= 100
  
 // Seen this question in a real interview before?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
