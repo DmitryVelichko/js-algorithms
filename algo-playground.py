55278c0f873d2e77c910639d86f4c6969f0eae1e
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-18 17:08:46 +0300

    Algorithms practice

--- algo-playground.py
+++ algo-playground.py
@@ -18,5 +18,25 @@
 
 # The element 1 occurs at the indices 0 and 3.
 
+# Example 2:
 
+# Input: nums = [1,2,3,4]
+
+# Output: false
+
+# Explanation:
+
+# All elements are distinct.
+
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+# Output: true
+
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
 # -109 <= nums[i] <= 109
