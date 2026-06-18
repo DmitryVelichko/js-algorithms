d744efc6188336aa3b4028f1bfff5e8955ec5d99
Dmitry Velichko <soleil-2005@mail.ru>
2026-06-18 17:08:54 +0300

    Algorithms practice

--- algo-playground.py
+++ algo-playground.py
@@ -40,3 +40,13 @@
 
 # 1 <= nums.length <= 105
 # -109 <= nums[i] <= 109
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
